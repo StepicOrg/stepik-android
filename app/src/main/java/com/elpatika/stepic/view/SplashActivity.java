f8740ab8dd97ed2cbde3a7e5bcc4635f435f82a5
Kirill Makarov <kir-maka@yandex.ru>
2015-08-23 12:40:58 +0300

    splash screen is added

---
+++ app/src/main/java/com/elpatika/stepic/view/SplashActivity.java
@@ -0,0 +1,15 @@
+package com.elpatika.stepic.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.elpatika.stepic.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
