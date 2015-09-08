480812bdbdf08072af0c9d090d6753656ec1cd49
Kirill Makarov <kir-maka@yandex.ru>
2015-09-08 11:53:02 +0300

    Add working progress bar to base stepic task

--- app/src/main/java/com/elpatika/stepic/concurrency/StepicTask.java
+++ app/src/main/java/com/elpatika/stepic/concurrency/StepicTask.java
@@ -2,6 +2,7 @@ package com.elpatika.stepic.concurrency;
 
 import android.content.Context;
 import android.os.Handler;
+import android.view.View;
 import android.widget.ProgressBar;
 
 import com.elpatika.stepic.core.IShell;
@@ -9,7 +10,7 @@ import com.google.inject.Inject;
 
 import roboguice.util.RoboAsyncTask;
 
-public abstract class StepicTask<T>  extends RoboAsyncTask<T> {
+public abstract class StepicTask<T> extends RoboAsyncTask<T> {
 
 
     @Inject
@@ -20,7 +21,6 @@ public abstract class StepicTask<T>  extends RoboAsyncTask<T> {
     protected StepicTask(Context context) {
         super(context);
     }
-
 
 
     protected void handle(final Exception ex) {
@@ -34,5 +34,21 @@ public abstract class StepicTask<T>  extends RoboAsyncTask<T> {
 
     public void setProgressBar(ProgressBar progressBar) {
         this.mProgressBar = progressBar;
+    }
+
+    @Override
+    protected void onPreExecute() throws Exception {
+        super.onPreExecute();
+        if (mProgressBar != null) {
+            mProgressBar.setVisibility(View.VISIBLE);
+        }
+    }
+
+    @Override
+    protected void onFinally() throws RuntimeException {
+        super.onFinally();
+        if (mProgressBar != null) {
+            mProgressBar.setVisibility(View.GONE);
+        }
     }
 }
