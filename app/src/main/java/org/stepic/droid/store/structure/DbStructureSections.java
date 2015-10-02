fefc52573f1e3d8b50195f320743fd82f04f680f
Kirill Makarov <kir-maka@yandex.ru>
2015-10-02 14:06:11 +0300

    Create DbStructure for section

---
+++ app/src/main/java/org/stepic/droid/store/structure/DbStructureSections.java
@@ -0,0 +1,49 @@
+package org.stepic.droid.store.structure;
+
+public class DbStructureSections extends DBStructureBase {
+
+    private static String[] mUsedColumns = null;
+
+    public static final String SECTIONS = "sections";
+
+    public static final class Column {
+        public static final String ID = "_id";
+        public static final String SECTION_ID = "section_id";
+        public static final String COURSE = "course";
+        public static final String UNITS = "units";
+        public static final String POSITION = "position";
+        public static final String PROGRESS = "progress";
+        public static final String TITLE = "title";
+        public static final String SLUG = "slug";
+        public static final String BEGIN_DATE = "begin_date";
+        public static final String END_DATE = "end_date";
+        public static final String SOFT_DEADLINE = "soft_deadline";
+        public static final String HARD_DEADLINE = "hard_deadline";
+        public static final String GRADING_POLICY = "grading_policy";
+        public static final String BEGIN_DATA_SOURCE = "begin_data_source";
+        public static final String END_DATE_SOURCE = "end_data_source";
+        public static final String SOFT_DEADLINE_SOURCE = "soft_deadline_source";
+        public static final String HARD_DEADLINE_SOURCE = "hard_deadline_source";
+        public static final String GRADING_POLICY_SOURCE = "grading_policy_source";
+        public static final String IS_ACTIVE = "is_active";
+        public static final String CREATE_DATE = "create_date";
+        public static final String UPDATE_DATE = "update_date";
+
+    }
+
+    public static String[] getUsedColumns() {
+        if (mUsedColumns == null) {
+            mUsedColumns = new String[]{
+                    Column.ID,
+                    Column.SECTION_ID,
+                    Column.TITLE,
+                    Column.SLUG,
+                    Column.IS_ACTIVE,
+                    Column.BEGIN_DATE,
+                    Column.SOFT_DEADLINE,
+                    Column.HARD_DEADLINE,
+            };
+        }
+        return mUsedColumns;
+    }
+}
