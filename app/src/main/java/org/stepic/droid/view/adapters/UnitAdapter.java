1c5a69ea77ae704e5a52575d37db4e7a388c7db6
Kirill Makarov <kir-maka@yandex.ru>
2015-10-08 00:28:26 +0300

    Update all lists to tiles

--- app/src/main/java/org/stepic/droid/view/adapters/UnitAdapter.java
+++ app/src/main/java/org/stepic/droid/view/adapters/UnitAdapter.java
@@ -1,7 +1,6 @@
 package org.stepic.droid.view.adapters;
 
 import android.content.Context;
-import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -84,7 +83,7 @@ public class UnitAdapter extends RecyclerView.Adapter<UnitAdapter.UnitViewHolder
     public static class UnitViewHolder extends RecyclerView.ViewHolder {
 
         @Bind(R.id.cv)
-        CardView cv;
+        View cv;
 
         @Bind(R.id.unit_layout)
         View layout;
