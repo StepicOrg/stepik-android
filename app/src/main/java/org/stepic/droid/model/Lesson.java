5b167ce35ae46970aab243b9109e8e0e5bea78ba
Kirill Makarov <kir-maka@yandex.ru>
2015-10-08 17:21:43 +0300

    Add Lesson to model

---
+++ app/src/main/java/org/stepic/droid/model/Lesson.java
@@ -0,0 +1,26 @@
+package org.stepic.droid.model;
+
+public class Lesson {
+    private int id;
+    private int [] steps;
+    private String actions;
+    private int [] tags;
+    private String playlists[];
+    private boolean is_featured;
+    private boolean is_prime;
+    private String progress;
+    private int owner;
+    private String [] subscriptions;
+    private int viewed_by;
+    private int passed_by;
+    private String[] dependencies;
+    private String [] followers;
+    private String language;
+    private boolean is_public;
+    private String title;
+    private String slug;
+    private String create_date;
+    private String update_date;
+    private String learners_group;
+    private String teacher_group;
+}
