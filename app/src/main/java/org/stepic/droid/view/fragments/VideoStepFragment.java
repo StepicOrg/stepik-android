87cb0433a114ac3b2e128a1a20e91acec3429069
Kirill Makarov <kir-maka@yandex.ru>
2015-10-13 00:15:51 +0300

    Video with baseline codec

--- app/src/main/java/org/stepic/droid/view/fragments/VideoStepFragment.java
+++ app/src/main/java/org/stepic/droid/view/fragments/VideoStepFragment.java
@@ -1,12 +1,15 @@
 package org.stepic.droid.view.fragments;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.MediaController;
 import android.widget.TextView;
+import android.widget.VideoView;
 
 import org.stepic.droid.R;
 import org.stepic.droid.base.FragmentStepBase;
@@ -18,6 +21,9 @@ public class VideoStepFragment extends FragmentStepBase {
 
     @Bind(R.id.test_tv)
     TextView testTv;
+
+    @Bind(R.id.video_view)
+    VideoView mVideoView;
 
     @Nullable
     @Override
@@ -32,5 +38,37 @@ public class VideoStepFragment extends FragmentStepBase {
         super.onActivityCreated(savedInstanceState);
         testTv.setText(getArguments().getString("test"));
         Log.i("newFragment", "new");
+
+        String url = "https://03-lvl3-pdl.vimeocdn.com/01/2389/4/111946354/307895202.mp4?expires=1444698357&token=0dabeb9e8375e26668ac7";
+//        String url = "https://player.vimeo.com/video/137358105";
+//        mVideoView.setVideoURI(Uri.parse(url));
+//        mVideoView.start();
+
+        Uri uri = Uri.parse(url);
+
+        MediaController mc = new MediaController(getActivity());
+        mVideoView.setMediaController(mc);
+        mVideoView.setVideoURI(uri);
+
+
+        mVideoView.requestFocus();
+        mVideoView.start();
     }
+
+//MediaController media_Controller;
+//    DisplayMetrics dm;
+//    public void getInit(String url) {
+//
+//        media_Controller = new MediaController(getContext());
+//        dm = new DisplayMetrics();
+//        this.getActivity().getWindowManager().getDefaultDisplay().getMetrics(dm);
+//        int height = dm.heightPixels;
+//        int width = dm.widthPixels;
+//        mVideoView.setMinimumWidth(width);
+//        mVideoView.setMinimumHeight(height);
+//        mVideoView.setMediaController(media_Controller);
+//        mVideoView.setVideoPath(url);
+//        mVideoView.start();
+//    }
+
 }
