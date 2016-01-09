fc8433fe480261dcee13a2f7a3f737af9b46ee82
Kirill Makarov <kir-maka@yandex.ru>
2016-01-09 19:19:42 +0300

    add search from search activity

--- app/src/main/java/org/stepic/droid/view/activities/CourseSearchResultActivity.java
+++ app/src/main/java/org/stepic/droid/view/activities/CourseSearchResultActivity.java
@@ -10,6 +10,7 @@ import android.support.v4.app.Fragment;
 import android.support.v4.view.MenuItemCompat;
 import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -22,6 +23,8 @@ import butterknife.Bind;
 import butterknife.ButterKnife;
 
 public class CourseSearchResultActivity extends FragmentActivityBase {
+
+    private final static String TAG = "SearchActivity";
 
 
     @Bind(R.id.toolbar)
@@ -37,19 +40,22 @@ public class CourseSearchResultActivity extends FragmentActivityBase {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.i(TAG, "onCreate");
         setContentView(R.layout.activity_search_courses);
         ButterKnife.bind(this);
 
         setSupportActionBar(mToolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-
         // Get the intent, verify the action and get the query
         Intent intent = getIntent();
-        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
-            query = intent.getStringExtra(SearchManager.QUERY);
-            initActivity(query);
-        }
+        handleIntent(intent);
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        setIntent(intent);
+        handleIntent(intent);
     }
 
     private void initActivity(String query) {
@@ -71,9 +77,7 @@ public class CourseSearchResultActivity extends FragmentActivityBase {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.action_search:
-
                 return true;
-
             case android.R.id.home:
                 // Respond to the action bar's Up/Home button
                 finish();
@@ -84,6 +88,7 @@ public class CourseSearchResultActivity extends FragmentActivityBase {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
+        Log.i(TAG, "onCreateOptionsMenu");
         getMenuInflater().inflate(R.menu.search_menu, menu);
         SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
         mMenuItem = menu.findItem(R.id.action_search);
@@ -117,5 +122,16 @@ public class CourseSearchResultActivity extends FragmentActivityBase {
     public void finish() {
         super.finish();
         overridePendingTransition(R.anim.slide_in_from_start, R.anim.slide_out_to_end);
+    }
+
+    private void handleIntent(Intent intent) {
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            query = intent.getStringExtra(SearchManager.QUERY);
+            initActivity(query);
+            if (mSearchView != null) {
+                //// TODO: 09.01.16 optimize it: need just clear focus
+                invalidateOptionsMenu();
+            }
+        }
     }
 }
