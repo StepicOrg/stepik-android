c9fac831a6c30d105653686a31870b09d3f532e2
Kirill Makarov <kir-maka@yandex.ru>
2016-01-20 16:06:01 +0300

    add attachment to free response reply

---
+++ app/src/main/java/org/stepic/droid/model/Attachment.java
@@ -0,0 +1,10 @@
+package org.stepic.droid.model;
+
+public class Attachment {
+    String name;
+    long size;
+    String url;
+    String content;
+    String type;
+
+}
--- app/src/main/java/org/stepic/droid/model/Reply.java
+++ app/src/main/java/org/stepic/droid/model/Reply.java
@@ -1,13 +1,16 @@
 package org.stepic.droid.model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Reply {
     List<Boolean> choices;
     String text;
+    List<Attachment> attachments;
 
     public Reply(String text) {
         this.text = text;
+        attachments = new ArrayList<>();
     }
 
     public Reply(List<Boolean> choices) {
