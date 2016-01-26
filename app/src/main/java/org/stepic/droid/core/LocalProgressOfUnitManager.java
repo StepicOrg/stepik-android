05094b337be8809602c587aa46757afa2bab7663
Kirill Makarov <kir-maka@yandex.ru>
2016-01-26 21:07:09 +0300

    remake LocalProgressManager (expect that it will be run not at UI thread)

--- app/src/main/java/org/stepic/droid/core/LocalProgressOfUnitManager.java
+++ app/src/main/java/org/stepic/droid/core/LocalProgressOfUnitManager.java
@@ -1,6 +1,7 @@
 package org.stepic.droid.core;
 
 import android.os.Handler;
+import android.util.Log;
 
 import com.squareup.otto.Bus;
 
@@ -24,37 +25,34 @@ public class LocalProgressOfUnitManager implements ILocalProgressManager {
         mBus = bus;
     }
 
+
     @Override
     public void checkUnitAsPassed(final long stepId) {
-        Handler handler = new Handler();
-        handler.post(new Runnable() {
+        Step step = mDatabaseManager.getStepById(stepId);
+        if (step == null) return;
+        Log.d("prmanager", "in");
+        List<Step> stepList = mDatabaseManager.getStepsOfLesson(step.getLesson());
+        for (Step stepItem : stepList) {
+            if (!stepItem.is_custom_passed()) return;
+        }
+
+        Unit unit = mDatabaseManager.getUnitByLessonId(step.getLesson());
+        if (unit == null) return;
+
+        unit.setIs_viewed_custom(true);
+        mDatabaseManager.addUnit(unit);
+
+        final long unitId = unit.getId();
+        Handler mainHandler = new Handler(MainApplication.getAppContext().getMainLooper());
+        //Say to ui that ui is cached now
+        Log.d("prmanager", "post");
+        Runnable myRunnable = new Runnable() {
             @Override
             public void run() {
-                Step step = mDatabaseManager.getStepById(stepId);
-                if (step == null) return;
-                List<Step> stepList = mDatabaseManager.getStepsOfLesson(step.getLesson());
-                for (Step stepItem : stepList) {
-                    if (!stepItem.is_custom_passed()) return;
-                }
-
-                Unit unit = mDatabaseManager.getUnitByLessonId(step.getLesson());
-                if (unit == null) return;
-
-                unit.setIs_viewed_custom(true);
-                mDatabaseManager.addUnit(unit);
-
-                final long unitId = unit.getId();
-                Handler mainHandler = new Handler(MainApplication.getAppContext().getMainLooper());
-                //Say to ui that ui is cached now
-                Runnable myRunnable = new Runnable() {
-                    @Override
-                    public void run() {
-                        mBus.post(new UnitProgressUpdateEvent(unitId));
-                    }
-                };
-                mainHandler.post(myRunnable);
-
+                mBus.post(new UnitProgressUpdateEvent(unitId));
             }
-        });
+        };
+        mainHandler.post(myRunnable);
+
     }
 }
