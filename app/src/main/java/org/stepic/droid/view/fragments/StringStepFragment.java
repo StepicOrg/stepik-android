d4f364fdb988f4dcaec6a31699b3f1a7df2327da
Kirill Makarov <kir-maka@yandex.ru>
2016-02-15 09:52:36 +0300

    fix number class for landscape, and make string multiline

--- app/src/main/java/org/stepic/droid/view/fragments/StringStepFragment.java
+++ app/src/main/java/org/stepic/droid/view/fragments/StringStepFragment.java
@@ -1,7 +1,16 @@
 package org.stepic.droid.view.fragments;
 
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+
 import com.squareup.otto.Subscribe;
 
+import org.stepic.droid.R;
 import org.stepic.droid.events.InternetIsEnabledEvent;
 import org.stepic.droid.events.attempts.FailAttemptEvent;
 import org.stepic.droid.events.attempts.SuccessAttemptEvent;
@@ -9,10 +18,34 @@ import org.stepic.droid.events.submissions.FailGettingLastSubmissionEvent;
 import org.stepic.droid.events.submissions.FailSubmissionCreatedEvent;
 import org.stepic.droid.events.submissions.SubmissionCreatedEvent;
 import org.stepic.droid.events.submissions.SuccessGettingLastSubmissionEvent;
+import org.stepic.droid.model.Attempt;
 import org.stepic.droid.model.Reply;
 import org.stepic.droid.util.HtmlHelper;
 
-public class StringStepFragment extends SingleLineSendStepFragment {
+public class StringStepFragment extends StepWithAttemptsFragment {
+
+    protected EditText mAnswerField;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = super.onCreateView(inflater, container, savedInstanceState);
+        mAnswerField = (EditText) ((LayoutInflater) this.getActivity().getSystemService(Context.LAYOUT_INFLATER_SERVICE)).inflate(R.layout.view_free_answer_attempt, mAttemptContainer, false);
+        mAttemptContainer.addView(mAnswerField);
+        return v;
+    }
+
+
+    @Override
+    protected final void showAttempt(Attempt attempt) {
+        mAnswerField.getText().clear();
+    }
+
+    @Override
+    protected final void blockUIBeforeSubmit(boolean needBlock) {
+        mAnswerField.setEnabled(!needBlock);
+    }
+
     @Override
     protected Reply generateReply() {
         return new Reply.Builder()
