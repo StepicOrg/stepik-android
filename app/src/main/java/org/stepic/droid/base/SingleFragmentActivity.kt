3ffc3d27fc76e7a6a76d0f69fe9f6e381233f581
Kirill Makarov <kir-maka@yandex.ru>
2016-02-19 10:54:29 +0300

    add kotlin and rewrite to kotlin single fragment activity (tested on 4.1.1)

---
+++ app/src/main/java/org/stepic/droid/base/SingleFragmentActivity.kt
@@ -0,0 +1,26 @@
+package org.stepic.droid.base
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+
+import org.stepic.droid.R
+
+abstract class SingleFragmentActivity : FragmentActivityBase() {
+    protected abstract fun createFragment(): Fragment
+    open fun getLayoutResId() = R.layout.activity_fragment
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(getLayoutResId())
+
+        val fm = supportFragmentManager
+        var fragment: Fragment? = fm.findFragmentById(R.id.fragmentContainer)
+
+        if (fragment == null) {
+            fragment = createFragment()
+            fm.beginTransaction().add(R.id.fragmentContainer, fragment).commit()
+        }
+
+    }
+
+}
--- app/src/main/java/org/stepic/droid/view/activities/CourseDetailActivity.java
+++ app/src/main/java/org/stepic/droid/view/activities/CourseDetailActivity.java
@@ -1,5 +1,6 @@
 package org.stepic.droid.view.activities;
 
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 
 import org.stepic.droid.base.SingleFragmentActivity;
@@ -8,6 +9,7 @@ import org.stepic.droid.util.AppConstants;
 import org.stepic.droid.view.fragments.CourseDetailFragment;
 
 public class CourseDetailActivity extends SingleFragmentActivity {
+    @NonNull
     @Override
     protected Fragment createFragment() {
         Course course = (Course) (getIntent().getExtras().get(AppConstants.KEY_COURSE_BUNDLE));
