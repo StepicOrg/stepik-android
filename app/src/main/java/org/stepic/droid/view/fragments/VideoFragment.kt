5239ba52bb926e64a186948d947856ca20e5fc9e
Kirill Makarov <kir-maka@yandex.ru>
2016-03-09 10:56:17 +0300

    make correct acceleration of video

--- app/src/main/java/org/stepic/droid/view/fragments/VideoFragment.kt
+++ app/src/main/java/org/stepic/droid/view/fragments/VideoFragment.kt
@@ -83,8 +83,10 @@ class VideoFragment : FragmentBase(), LibVLC.HardwareAccelerationError, IVLCVout
         try {
             val options = ArrayList<String>()
             //options.add("--subsdec-encoding <encoding>");
-            options.add("--aout=opensles")
+//            options.add("--aout=opensles")
+
             options.add("--audio-time-stretch") // time stretching
+            options.add("--no-drop-late-frames") //help when user accelerates video
             options.add("-vvv") // verbosity
             libvlc = LibVLC(options)
             libvlc?.setOnHardwareAccelerationError(this)
