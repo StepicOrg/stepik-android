aba82acc8c7321fa03de18cb88fb8a0ee8657bea
Kirill Makarov <kir-maka@yandex.ru>
2016-03-13 21:02:36 +0300

    add vlc utils

---
+++ app/src/main/java/org/stepic/droid/util/Util.java
@@ -0,0 +1,90 @@
+/*****************************************************************************
+ * UiTools.java
+ *****************************************************************************
+ * Copyright © 2011-2014 VLC authors and VideoLAN
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston MA 02110-1301, USA.
+ *****************************************************************************/
+
+package org.stepic.droid.util;
+
+import android.annotation.TargetApi;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+
+import org.stepic.droid.base.MainApplication;
+import org.videolan.libvlc.util.AndroidUtil;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.List;
+
+public class Util {
+    public final static String TAG = "VLC/Util";
+
+    public static String readAsset(String assetName, String defaultS) {
+        InputStream is = null;
+        BufferedReader r = null;
+        try {
+            is = MainApplication.getAppContext().getResources().getAssets().open(assetName);
+            r = new BufferedReader(new InputStreamReader(is, "UTF8"));
+            StringBuilder sb = new StringBuilder();
+            String line = r.readLine();
+            if(line != null) {
+                sb.append(line);
+                line = r.readLine();
+                while(line != null) {
+                    sb.append('\n');
+                    sb.append(line);
+                    line = r.readLine();
+                }
+            }
+            return sb.toString();
+        } catch (IOException e) {
+            return defaultS;
+        } finally {
+            close(is);
+            close(r);
+        }
+    }
+
+    @TargetApi(android.os.Build.VERSION_CODES.GINGERBREAD)
+    public static void commitPreferences(SharedPreferences.Editor editor){
+        if (AndroidUtil.isGingerbreadOrLater())
+            editor.apply();
+        else
+            editor.commit();
+    }
+
+    public static boolean close(Closeable closeable) {
+        if (closeable != null)
+            try {
+                closeable.close();
+                return true;
+            } catch (IOException e) {}
+        return false;
+    }
+
+    public static boolean isCallable(Intent intent) {
+        List<ResolveInfo> list = MainApplication.getAppContext().getPackageManager().queryIntentActivities(intent,
+                PackageManager.MATCH_DEFAULT_ONLY);
+        return list.size() > 0;
+    }
+}
