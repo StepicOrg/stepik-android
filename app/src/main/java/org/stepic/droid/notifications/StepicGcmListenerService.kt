cea25b89ad93da89f77211cef9d530bfb9a177d3
Kirill Makarov <kir-maka@yandex.ru>
2016-03-31 21:33:54 +0300

    add big style (collapse problem)

--- app/src/main/java/org/stepic/droid/notifications/StepicGcmListenerService.kt
+++ app/src/main/java/org/stepic/droid/notifications/StepicGcmListenerService.kt
@@ -4,34 +4,29 @@ import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
+import android.graphics.BitmapFactory
 import android.media.RingtoneManager
 import android.os.Bundle
 import android.support.v4.app.NotificationCompat
 import com.google.android.gms.gcm.GcmListenerService
 import com.google.gson.Gson
-import com.yandex.metrica.YandexMetrica
 import org.stepic.droid.R
 import org.stepic.droid.notifications.model.Notification
+import org.stepic.droid.util.ColorUtil
+import org.stepic.droid.util.HtmlHelper
 import org.stepic.droid.view.activities.MainFeedActivity
 
 class StepicGcmListenerService : GcmListenerService() {
 
     override fun onMessageReceived(from: String?, data: Bundle?) {
-        val emptyBundle = data!!.getBundle("notification")
-        val notificationRawString : String = data!!.getString("object")
-        val stepicNotification =  Gson().fromJson(notificationRawString, Notification::class.java)
+        val notificationRawString: String? = data?.getString("object")
+        val stepicNotification = Gson().fromJson(notificationRawString, Notification::class.java)
 
-        val message = data!!.getString("gcm.notification.title")
-        YandexMetrica.reportEvent("gcm.notification.title is " + message);
-        sendNotification("ETO CUSTOM " + (message ?: "null"))
+        stepicNotification?.htmlText?.let {
+            sendNotification(it)
+        }
     }
 
-
-    /**
-     * Create and show a simple notification containing the received GCM message.
-
-     * @param message GCM message received.
-     */
     private fun sendNotification(message: String) {
         val intent = Intent(this, MainFeedActivity::class.java)
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
@@ -39,18 +34,25 @@ class StepicGcmListenerService : GcmListenerService() {
                 PendingIntent.FLAG_ONE_SHOT)
 
         val defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+        val largeIcon = BitmapFactory.decodeResource(getResources(), R.drawable.ic_code);
+        val colorArgb = ColorUtil.getColorArgb(R.color.stepic_brand_primary)
         val notification = NotificationCompat.Builder(this)
-                .setSmallIcon(R.drawable.stepic_logo)
-                .setContentTitle("GCM Message")
-                .setContentText(message)
+                .setLargeIcon(largeIcon)
+                .setSmallIcon(R.drawable.ic_matching)
+                .setContentTitle("I handle this notification")
+                .setContentText(HtmlHelper.fromHtml(message).toString())
+
+                .setStyle(NotificationCompat.BigTextStyle()
+                        .bigText(HtmlHelper.fromHtml(message).toString()))
+                .setColor(colorArgb)
                 .setAutoCancel(true)
                 .setSound(defaultSoundUri)
                 .setContentIntent(pendingIntent)
                 .setVisibility(NotificationCompat.VISIBILITY_PUBLIC)
-                .build()
+
 
         val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 
-        notificationManager.notify(0 /* ID of notification */, notification)
+        notificationManager.notify(0 /* ID of notification */, notification.build())
     }
 }
