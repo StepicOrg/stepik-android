69b41ef403657ac422d4aee1c988023562cfe6c4
Kirill Makarov <kir-maka@yandex.ru>
2016-04-02 00:02:50 +0300

    improve branded launch screen (like Google Hangouts, Google+, etc)

--- app/src/main/java/org/stepic/droid/view/activities/SplashActivity.java
+++ app/src/main/java/org/stepic/droid/view/activities/SplashActivity.java
@@ -6,13 +6,11 @@ import android.os.Handler;
 
 import org.stepic.droid.preferences.SharedPreferenceHelper;
 
-import butterknife.ButterKnife;
-
 
 public class SplashActivity extends BackToExitActivityBase {
 
     // Splash screen wait time
-    private static final int SPLASH_TIME_OUT = 1500;
+    private static final int SPLASH_TIME_OUT = 1000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,9 +21,6 @@ public class SplashActivity extends BackToExitActivityBase {
             finish();
             return;
         }
-
-        setContentView(org.stepic.droid.R.layout.activity_splash);
-        ButterKnife.bind(this);
 
         new Handler().postDelayed(new Runnable() {
             @Override
@@ -38,25 +33,10 @@ public class SplashActivity extends BackToExitActivityBase {
                     } else {
                         mShell.getScreenProvider().showLaunchScreen(SplashActivity.this, false);
                     }
-
-
-                    finish();//it is obsolete todo: is it clear when use clear top
+                    finish();
                 }
             }
         }, SPLASH_TIME_OUT);
 
     }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        bus.register(this);
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        bus.unregister(this);
-    }
-
 }
