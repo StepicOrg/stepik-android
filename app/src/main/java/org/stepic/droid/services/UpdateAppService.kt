e9f2538fb118e38fa679291e0b7aec1059adcac7
Kirill Makarov <kir-maka@yandex.ru>
2016-04-25 16:59:51 +0300

    add update fetching

--- app/src/main/java/org/stepic/droid/services/UpdateAppService.kt
+++ app/src/main/java/org/stepic/droid/services/UpdateAppService.kt
@@ -3,9 +3,11 @@ package org.stepic.droid.services
 import android.app.IntentService
 import android.app.Service
 import android.content.Intent
+import com.squareup.otto.Bus
 import org.stepic.droid.base.MainApplication
 import org.stepic.droid.concurrency.IMainHandler
 import org.stepic.droid.configuration.IConfig
+import org.stepic.droid.model.AppInfo
 import org.stepic.droid.web.IApi
 import javax.inject.Inject
 
@@ -20,6 +22,9 @@ class UpdateAppService : IntentService("update_stepic") {
     @Inject
     lateinit var api: IApi;
 
+    @Inject
+    lateinit var bus: Bus;
+
     override fun onHandleIntent(intent: Intent?) {
         checkUpdateAndPushMessageOnMainFeed()
     }
@@ -31,5 +36,33 @@ class UpdateAppService : IntentService("update_stepic") {
     }
 
     private fun checkUpdateAndPushMessageOnMainFeed() {
+        if (configs.isCustomUpdateEnable) {
+            val appInfo = api.infoForUpdating.execute()?.body()?.appInfo
+            val currentCustomVersion = configs.oldUpdatingVersion
+
+            if (appInfo?.custom_version ?: 0 > currentCustomVersion) {
+                //need update
+                val linkForUpdate = getLinkForUpdating(appInfo)
+                val jjj = 0
+
+            }
+        } else {
+            return;
+        }
+    }
+
+    private fun getLinkForUpdating(appInfo: AppInfo?): String? {
+        val links = appInfo?.download_links
+
+        //should take from system arch, and update using this info
+        if (links != null) {
+            for (linkItem in links) {
+                if (linkItem.architecture ?: "" == "all") {
+                    return linkItem.link
+                }
+            }
+        }
+
+        return null
     }
 }
