bad39342419d165e54594abee94fb592e7327372
Kirill Makarov <kir-maka@yandex.ru>
2016-05-13 15:08:54 +0300

    add possibility for canceling section and unit

--- app/src/main/java/org/stepic/droid/store/SynchronizedCancelSniffer.java
+++ app/src/main/java/org/stepic/droid/store/SynchronizedCancelSniffer.java
@@ -10,25 +10,77 @@ import javax.inject.Singleton;
 public class SynchronizedCancelSniffer implements ICancelSniffer {
 
     private final Set<Long> mCanceledStepIdsSet;
+    private final Set<Long> mCanceledSectionIdsSet;
+    private final Set<Long> mCanceledUnitIdsSet;
 
     @Inject
     public SynchronizedCancelSniffer() {
         mCanceledStepIdsSet = new HashSet<>();
+        mCanceledSectionIdsSet = new HashSet<>();
+        mCanceledUnitIdsSet = new HashSet<>();
     }
 
     @Override
-    public synchronized void addStepIdCancel(long stepId) {
-        mCanceledStepIdsSet.add(stepId);
+    public void addStepIdCancel(long stepId) {
+        synchronized (mCanceledStepIdsSet) {
+            mCanceledStepIdsSet.add(stepId);
+        }
     }
 
     @Override
-    public synchronized void removeStepIdCancel(long stepId) {
-        mCanceledStepIdsSet.remove(stepId);
+    public void removeStepIdCancel(long stepId) {
+        synchronized (mCanceledStepIdsSet) {
+            mCanceledStepIdsSet.remove(stepId);
+        }
     }
 
     @Override
-    public synchronized boolean isStepIdCanceled(long stepId) {
-        return mCanceledStepIdsSet.contains(stepId);
+    public boolean isStepIdCanceled(long stepId) {
+        synchronized (mCanceledStepIdsSet) {
+            return mCanceledStepIdsSet.contains(stepId);
+        }
+    }
+
+    @Override
+    public void addSectionIdCancel(long sectionId) {
+        synchronized (mCanceledSectionIdsSet) {
+            mCanceledSectionIdsSet.add(sectionId);
+        }
+    }
+
+    @Override
+    public void removeSectionIdCancel(long sectionId) {
+        synchronized (mCanceledSectionIdsSet) {
+            mCanceledSectionIdsSet.remove(sectionId);
+        }
+    }
+
+    @Override
+    public boolean isSectionIdIsCanceled(long sectionId) {
+        synchronized (mCanceledSectionIdsSet) {
+            return mCanceledSectionIdsSet.contains(sectionId);
+        }
+    }
+
+    @Override
+    public void addUnitIdCancel(long unitId) {
+        synchronized (mCanceledUnitIdsSet) {
+            mCanceledUnitIdsSet.add(unitId);
+        }
+    }
+
+    @Override
+    public void removeUnitIdCancel(long unitId) {
+        synchronized (mCanceledUnitIdsSet) {
+            mCanceledUnitIdsSet.remove(unitId);
+        }
+    }
+
+    @Override
+    public boolean isUnitIdIsCanceled(long unitId) {
+        synchronized (mCanceledUnitIdsSet) {
+            return mCanceledUnitIdsSet.contains(unitId);
+        }
     }
 
 }
