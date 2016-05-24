f7f925acf5bff7f1275ad6144e55d580a3855323
Kirill Makarov <kir-maka@yandex.ru>
2016-05-24 23:25:10 +0300

    add order of comments, load discussion proxy

--- app/src/main/java/org/stepic/droid/view/fragments/CommentsFragment.kt
+++ app/src/main/java/org/stepic/droid/view/fragments/CommentsFragment.kt
@@ -7,9 +7,21 @@ import android.support.v7.widget.Toolbar
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ProgressBar
 import android.widget.TextView
+import com.squareup.otto.Subscribe
 import org.stepic.droid.R
 import org.stepic.droid.base.FragmentBase
+import org.stepic.droid.events.comments.CommentsLoadedSuccessfullyEvent
+import org.stepic.droid.events.comments.DiscussionProxyLoadedSuccessfullyEvent
+import org.stepic.droid.model.comments.Comment
+import org.stepic.droid.model.comments.DiscussionProxy
+import org.stepic.droid.util.ProgressHelper
+import org.stepic.droid.web.DiscussionProxyResponse
+import retrofit.Callback
+import retrofit.Response
+import retrofit.Retrofit
+import java.util.*
 
 class CommentsFragment : FragmentBase() {
     companion object {
@@ -24,11 +36,20 @@ class CommentsFragment : FragmentBase() {
         }
     }
 
-    lateinit var mToolbar: Toolbar
-
     lateinit var discussionId: String
 
-    lateinit var discussionTv : TextView
+    lateinit var mToolbar: Toolbar
+    lateinit var discussionTv: TextView
+    lateinit var loadProgressBarOnCenter: ProgressBar
+
+    val commentsList: MutableList<Comment> = ArrayList()
+    val commentsIdSet: MutableSet<Long> = HashSet() //it is set of comments, which are already on screen
+    var discussionProxy: DiscussionProxy? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        retainInstance = true
+    }
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val v = inflater?.inflate(R.layout.fragment_comments, container, false)
@@ -37,8 +58,15 @@ class CommentsFragment : FragmentBase() {
         v?.let {
             initToolbar(v)
             initDiscussionTv(v)
+            initEmptyProgressOnCenter(v)
         }
         return v
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        showEmptyProgressOnCenter()
+        loadDiscussionProxyById()
     }
 
     private fun initDiscussionTv(v: View) {
@@ -46,10 +74,80 @@ class CommentsFragment : FragmentBase() {
         discussionTv.text = discussionId
     }
 
+    private fun initEmptyProgressOnCenter(v: View) {
+        loadProgressBarOnCenter = v.findViewById(R.id.load_progressbar) as ProgressBar
+    }
+
     fun initToolbar(v: View) {
         mToolbar = v.findViewById(R.id.toolbar) as Toolbar
         (activity as AppCompatActivity).setSupportActionBar(mToolbar)
         (activity as AppCompatActivity).supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    private fun loadDiscussionProxyById(id: String = discussionId) {
+        mShell.api.getDiscussionProxies(id).enqueue(object : Callback<DiscussionProxyResponse> {
+            override fun onResponse(response: Response<DiscussionProxyResponse>?, retrofit: Retrofit?) {
+                if (response != null && response.isSuccess) {
+                    val discussionProxy = response.body().discussionProxies.firstOrNull()
+                    if (discussionProxy != null) {
+                        bus.post(DiscussionProxyLoadedSuccessfullyEvent(discussionProxy))
+                    } else {
+                        //TODO IMPLEMENT ON FAIL.
+                    }
+                } else {
+                    //TODO IMPLEMENT ON FAIL
+                }
+            }
+
+            override fun onFailure(t: Throwable?) {
+                //TODO IMPLEMENT ON FAIL
+            }
+
+        })
+    }
+
+    private fun loadComments(ids : LongArray) {
+//if order is changed --> clear list and set, where it was changed
+
+    }
+
+    @Subscribe
+    fun onDiscussionProxyLoadedSuccessfully(successfullyEvent: DiscussionProxyLoadedSuccessfullyEvent) {
+        discussionProxy = successfullyEvent.discussionProxy
+//        loadComments()
+        val e = 0
+    }
+
+
+    @Subscribe
+    fun onCommentsLoadedSuccessfully(successfullyEvent: CommentsLoadedSuccessfullyEvent) {
+
+        successfullyEvent.comments
+                .forEach {
+                    if (it.id != null && !commentsIdSet.contains(it.id))
+                        commentsIdSet.add(it.id)
+                    commentsList.add(it)
+                }
+        //now in list we have new comments, which we should show
+    }
+
+    override fun onStart() {
+        super.onStart()
+        bus.register(this)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        bus.unregister(this)
+    }
+
+
+    private fun showEmptyProgressOnCenter(needShow: Boolean = true) {
+        if (needShow) {
+            ProgressHelper.activate(loadProgressBarOnCenter)
+        } else {
+            ProgressHelper.dismiss(loadProgressBarOnCenter)
+        }
     }
 
 }
