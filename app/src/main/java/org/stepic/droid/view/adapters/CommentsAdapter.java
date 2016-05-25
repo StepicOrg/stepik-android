c7019714fe344e17b702b834a554a6ff4c107224
Kirill Makarov <kir-maka@yandex.ru>
2016-05-25 18:23:04 +0300

    show avatar of user

--- app/src/main/java/org/stepic/droid/view/adapters/CommentsAdapter.java
+++ app/src/main/java/org/stepic/droid/view/adapters/CommentsAdapter.java
@@ -1,14 +1,21 @@
 package org.stepic.droid.view.adapters;
 
 import android.content.Context;
+import android.net.Uri;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.facebook.common.util.UriUtil;
+import com.facebook.drawee.backends.pipeline.Fresco;
+import com.facebook.drawee.interfaces.DraweeController;
+import com.facebook.drawee.view.DraweeView;
+
 import org.stepic.droid.R;
 import org.stepic.droid.core.CommentManager;
+import org.stepic.droid.model.User;
 import org.stepic.droid.model.comments.Comment;
 import org.stepic.droid.util.HtmlHelper;
 
@@ -38,11 +45,44 @@ public class CommentsAdapter extends RecyclerView.Adapter<CommentsAdapter.Commen
         Comment comment = needUpdateAndComment.component2();
 
         boolean isParent = false;
-        if (comment.getParent() == null){
+        if (comment.getParent() == null) {
             isParent = true;
         }
 
         holder.commentText.setText(HtmlHelper.fromHtml(comment.getText()));
+
+        DraweeController controller = getControllerForUserAvatar(comment);
+        holder.userIcon.setController(controller);
+    }
+
+    private DraweeController getControllerForUserAvatar(Comment comment) {
+        String userAvatar = null;
+        if (comment.getUser() != null) {
+            User user = commentManager.getUserById(comment.getUser());
+            if (user != null) {
+                userAvatar = user.getAvatar();
+            }
+        }
+        DraweeController controller = null;
+        if (userAvatar != null) {
+            controller = Fresco.newDraweeControllerBuilder()
+                    .setUri(userAvatar)
+                    .setAutoPlayAnimations(true)
+                    .build();
+
+        } else {
+            //for empty cover:
+            Uri uri = new Uri.Builder()
+                    .scheme(UriUtil.LOCAL_RESOURCE_SCHEME) // "res"
+                    .path(String.valueOf(R.drawable.placeholder_icon))
+                    .build();
+
+            controller = Fresco.newDraweeControllerBuilder()
+                    .setUri(uri)
+                    .setAutoPlayAnimations(true)
+                    .build();
+        }
+        return controller;
     }
 
     @Override
@@ -53,6 +93,9 @@ public class CommentsAdapter extends RecyclerView.Adapter<CommentsAdapter.Commen
     static class CommentsViewHolder extends RecyclerView.ViewHolder {
         @Bind(R.id.text_comment)
         TextView commentText;
+
+        @Bind(R.id.user_icon)
+        DraweeView userIcon;
 
         public CommentsViewHolder(View itemView) {
             super(itemView);
