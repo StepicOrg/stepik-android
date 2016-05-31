142bebc08f83782e3eb964f5db8e7d7faa1e8eef
Kirill Makarov <kir-maka@yandex.ru>
2016-05-31 14:56:57 +0300

    show keyboard in onResume of new comment

--- app/src/main/java/org/stepic/droid/view/fragments/NewCommentFragment.kt
+++ app/src/main/java/org/stepic/droid/view/fragments/NewCommentFragment.kt
@@ -1,5 +1,6 @@
 package org.stepic.droid.view.fragments
 
+import android.content.Context
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v7.app.AppCompatActivity
@@ -7,6 +8,8 @@ import android.support.v7.widget.Toolbar
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
 import org.stepic.droid.R
 import org.stepic.droid.base.FragmentBase
 
@@ -25,6 +28,7 @@ class NewCommentFragment : FragmentBase() {
     }
 
     lateinit var mToolbar: Toolbar
+    lateinit var mTextBody: EditText
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val v = inflater?.inflate(R.layout.new_comment_fragment, container, false)
@@ -32,8 +36,23 @@ class NewCommentFragment : FragmentBase() {
         setHasOptionsMenu(true)
         v?.let {
             initToolbar(v)
+            initEditBody(v)
         }
         return v
+    }
+
+    private fun initEditBody(v: View) {
+        mTextBody = v.findViewById(R.id.input_comment_form) as EditText
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (!mTextBody.isFocused) {
+            mTextBody.requestFocus()
+        }
+        mTextBody.postDelayed({
+            showSoftKeypad(mTextBody)
+        }, 300)
     }
 
     private fun initToolbar(v: View) {
@@ -41,5 +60,11 @@ class NewCommentFragment : FragmentBase() {
         (activity as AppCompatActivity).setSupportActionBar(mToolbar)
         (activity as AppCompatActivity).supportActionBar?.setDisplayHomeAsUpEnabled(true)
         (activity as AppCompatActivity).supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_close_white_24dp)
+    }
+
+    private fun showSoftKeypad(editTextView: View) {
+        val imm = activity.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.showSoftInput(editTextView, InputMethodManager.SHOW_IMPLICIT)
+
     }
 }
