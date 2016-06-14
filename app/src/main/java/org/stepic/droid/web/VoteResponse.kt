ed621713bf4535eef3ea97c73fe8bd3a4e2e59c2
Kirill Makarov <kir-maka@yandex.ru>
2016-06-14 15:28:14 +0300

    add detail for likes

--- app/src/main/java/org/stepic/droid/web/VoteResponse.kt
+++ app/src/main/java/org/stepic/droid/web/VoteResponse.kt
@@ -6,4 +6,5 @@ import org.stepic.droid.model.comments.Vote
 class VoteResponse {
     val meta: Meta? = null
     val votes: List<Vote>? = null
+    val detail : String? = null //"You do not have permission to perform this action."
 }
