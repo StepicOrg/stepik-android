7b15d3c5e94527c196fc4cdbea1b828ab7d21cab
Kirill Makarov <kir-maka@yandex.ru>
2016-07-01 17:44:05 +0300

    add string resources for user role

--- app/src/main/java/org/stepic/droid/model/UserRole.kt
+++ app/src/main/java/org/stepic/droid/model/UserRole.kt
@@ -1,12 +1,14 @@
 package org.stepic.droid.model
 
+import android.support.annotation.StringRes
 import com.google.gson.annotations.SerializedName
+import org.stepic.droid.R
 
-enum class UserRole private constructor(val value: String?) {
+enum class UserRole private constructor(val value: String?,@StringRes  val resource : Int? ) {
     @SerializedName("student")
-    student("student"),
+    student("student", null),
     @SerializedName("stuff")
-    stuff("stuff"),
+    stuff("stuff", R.string.stuff_label),
     @SerializedName("teacher")
-    teacher("teacher")
+    teacher("teacher", R.string.teacher_label)
 }
