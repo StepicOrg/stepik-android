10e0d389a64d0d48f3e4d5b0d04ea6de888e93a6
Kirill Makarov <kir-maka@yandex.ru>
2016-07-01 18:03:11 +0300

    fix spelling in staff

--- app/src/main/java/org/stepic/droid/model/UserRole.kt
+++ app/src/main/java/org/stepic/droid/model/UserRole.kt
@@ -7,8 +7,8 @@ import org.stepic.droid.R
 enum class UserRole private constructor(val value: String?,@StringRes  val resource : Int? ) {
     @SerializedName("student")
     student("student", null),
-    @SerializedName("stuff")
-    stuff("stuff", R.string.stuff_label),
+    @SerializedName("staff")
+    stuff("staff", R.string.staff_label),
     @SerializedName("teacher")
     teacher("teacher", R.string.teacher_label)
 }
