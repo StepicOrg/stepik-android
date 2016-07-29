155b7ae57f2cdb9e2c1da0e7c25fe1c5418799f0
Kirill Makarov <kir-maka@yandex.ru>
2016-07-29 22:10:38 +0300

    make base design of certificate item

--- app/src/main/java/org/stepic/droid/view/adapters/CertificateAdapter.java
+++ app/src/main/java/org/stepic/droid/view/adapters/CertificateAdapter.java
@@ -1,11 +1,17 @@
 package org.stepic.droid.view.adapters;
 
 import android.content.Context;
+import android.net.Uri;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+
+import com.facebook.common.util.UriUtil;
+import com.facebook.drawee.backends.pipeline.Fresco;
+import com.facebook.drawee.interfaces.DraweeController;
+import com.facebook.drawee.view.DraweeView;
 
 import org.jetbrains.annotations.NotNull;
 import org.stepic.droid.R;
@@ -54,6 +60,12 @@ public class CertificateAdapter extends RecyclerView.Adapter<CertificateAdapter.
         @BindView(R.id.certificate_title)
         TextView certificateTitleView;
 
+        @BindView(R.id.certificate_icon)
+        DraweeView certificateIcon;
+
+        @BindView(R.id.certificate_grade)
+        TextView certificateGradeView;
+
         CertificateViewHolder(View itemView) {
             super(itemView);
             ButterKnife.bind(this, itemView);
@@ -66,7 +78,13 @@ public class CertificateAdapter extends RecyclerView.Adapter<CertificateAdapter.
         }
 
         public void setData(CertificateViewItem certificate) {
+            String prefix = "Сертификат с отличием за курс"; // FIXME: 29.07.16 FROM RESOURCES, resolve distinct
+
+            String certificateName = prefix + " " + certificate.getTitle();
             certificateTitleView.setText(certificate.getTitle());
+            certificateGradeView.setText(certificateName + "\n\n Result: 146%"); // FIXME: 29.07.16 throw in certificateviewitem result and show here
+//            certificateGradeView.setText("Result: 146%"); // FIXME: 29.07.16 throw in certificateviewitem result and show here
+            certificateIcon.setController(getControllerForCertificateCover(certificate.getCoverFullPath()));
         }
 
 
@@ -75,6 +93,26 @@ public class CertificateAdapter extends RecyclerView.Adapter<CertificateAdapter.
             // TODO: 26.07.16 open certificate
         }
 
+    }
+
+    private DraweeController getControllerForCertificateCover(String coverFullPath) {
+        if (coverFullPath != null) {
+            return Fresco.newDraweeControllerBuilder()
+                    .setUri(coverFullPath)
+                    .setAutoPlayAnimations(true)
+                    .build();
+        } else {
+            //for empty cover:
+            Uri uri = new Uri.Builder()
+                    .scheme(UriUtil.LOCAL_RESOURCE_SCHEME) // "res"
+                    .path(String.valueOf(R.drawable.ic_course_placeholder))
+                    .build();
+
+            return Fresco.newDraweeControllerBuilder()
+                    .setUri(uri)
+                    .setAutoPlayAnimations(true)
+                    .build();
+        }
     }
 
     private interface CertificateClickListener {
