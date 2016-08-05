3976a89c5f9550060e446371b9b0764e7734102e
Kirill Makarov <kir-maka@yandex.ru>
2016-08-05 20:35:56 +0300

    change icon google plus -> google (but look at https://developers.google.com/identity/branding-guidelines)

--- app/src/main/java/org/stepic/droid/social/SocialManager.java
+++ app/src/main/java/org/stepic/droid/social/SocialManager.java
@@ -19,7 +19,7 @@ public class SocialManager {
     private static final String VK_SOCIAL_IDENTIFIER = "vk";
 
     public enum SocialType implements ISocialType {
-        google(GOOGLE_SOCIAL_IDENTIFIER, ContextCompat.getDrawable(MainApplication.getAppContext(), R.drawable.ic_google_plus_new_icon_logo)),
+        google(GOOGLE_SOCIAL_IDENTIFIER, ContextCompat.getDrawable(MainApplication.getAppContext(), R.drawable.ic_new_google_favicon_logo)),
         facebook(FACEBOOK_SOCIAL_IDENTIFIER, ContextCompat.getDrawable(MainApplication.getAppContext(), R.drawable.ic_facebook)),
         mailru(MAILRU_SOCIAL_IDENTIFIER, ContextCompat.getDrawable(MainApplication.getAppContext(), R.drawable.ic_it)),
         twitter(TWITTER_SOCIAL_IDENTIFIER, ContextCompat.getDrawable(MainApplication.getAppContext(), R.drawable.ic_twitter)),
