aa4d6f9a0c3f4630b7d79bc14c629cac8aa8560f
Kirill Makarov <kir-maka@yandex.ru>
2016-08-16 17:58:35 +0300

    improve logic of showing "next step" (stub)

--- app/src/main/java/org/stepic/droid/core/StepModule.java
+++ app/src/main/java/org/stepic/droid/core/StepModule.java
@@ -1,6 +1,10 @@
 package org.stepic.droid.core;
 
+import org.stepic.droid.concurrency.IMainHandler;
 import org.stepic.droid.core.presenters.NextStepPresenter;
+import org.stepic.droid.store.operations.DatabaseFacade;
+
+import java.util.concurrent.ThreadPoolExecutor;
 
 import dagger.Module;
 import dagger.Provides;
@@ -8,8 +12,11 @@ import dagger.Provides;
 @Module
 public class StepModule {
     @Provides
-    public NextStepPresenter provideNextStepPresenter() {
-        return new NextStepPresenter();
+    public NextStepPresenter provideNextStepPresenter(
+            ThreadPoolExecutor threadPoolExecutor,
+            IMainHandler mainHandler,
+            DatabaseFacade databaseFacade) {
+        return new NextStepPresenter(threadPoolExecutor, mainHandler, databaseFacade);
     }
 
 }
--- app/src/main/java/org/stepic/droid/core/presenters/NextStepPresenter.kt
+++ app/src/main/java/org/stepic/droid/core/presenters/NextStepPresenter.kt
@@ -1,24 +1,35 @@
 package org.stepic.droid.core.presenters
 
+import org.stepic.droid.concurrency.IMainHandler
 import org.stepic.droid.core.presenters.contracts.NextStepView
 import org.stepic.droid.model.Lesson
+import org.stepic.droid.store.operations.DatabaseFacade
+import java.util.concurrent.ThreadPoolExecutor
 
-class NextStepPresenter : PresenterBase<NextStepView>() {
+class NextStepPresenter(
+        val threadPoolExecutor: ThreadPoolExecutor,
+        val mainHandler: IMainHandler,
+        val databaseFacade: DatabaseFacade) : PresenterBase<NextStepView>() {
 
     /**
      * Last step in lesson can be shown differently
      */
     fun checkStepForLast(stepId: Long, lesson: Lesson) {
         val stepIds = lesson.steps
+        var isCandidateForShowing = false
         if (stepIds != null && stepIds.size != 0) {
             val lastStepId = stepIds[stepIds.size - 1]
             if (lastStepId == stepId) {
-                view?.showNextLessonView()
-                return
+                //YEAH, it is candidate for showing -> check in db
+                threadPoolExecutor.execute {
+                    //todo get section of unit
+                    mainHandler.post {
+                        //if not last lesson in section -> show button
+                    }
+                }
+
             }
         }
-
-        //if not shown -> check in db
     }
 
     fun clickNextLesson(currentLesson: Lesson) {
