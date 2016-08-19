afde344a081db912e63d01dd24f6d8d48a7b448e
Kirill Makarov <kir-maka@yandex.ru>
2016-08-19 09:51:39 +0300

    change c->k in tests

--- app/src/test/java/org/stepic/droid/util/StringUtilTest.kt
+++ app/src/test/java/org/stepic/droid/util/StringUtilTest.kt
@@ -7,16 +7,9 @@ class StringUtilTest {
     @Test
     fun testUriForCourse_correct (){
         //todo: provide IConfig
-        val  expected ="https://stepic.org/course/Школьная-физика-Тепловые-и-электромагнитные-явления-432/"
-        assertEquals("only for stepic instance (production): ", expected, StringUtil.getUriForCourse("https://stepic.org","Школьная-физика-Тепловые-и-электромагнитные-явления-432"))
+        val  expected ="https://stepik.org/course/Школьная-физика-Тепловые-и-электромагнитные-явления-432/"
+        assertEquals("only for stepik instance (production): ", expected, StringUtil.getUriForCourse("https://stepik.org","Школьная-физика-Тепловые-и-электромагнитные-явления-432"))
     }
 
-//    @Test
-//    fun testGetAppUriForAppIndexing_correct(){
-//        val actual = StringUtil.getAppUriForCourse("https://stepic.org", "Программирование-на-Python-67").toString()
-//        val expected = "android-app://org.stepic.droid/https/stepic.org/course/Программирование-на-Python-67/"
-//        assertEquals(expected, actual)
-//    }
-
 }
-//https://stepic.org/course/Школьная-физика-Тепловые-и-электромагнитные-явления-432/
+//https://stepik.org/course/Школьная-физика-Тепловые-и-электромагнитные-явления-432/
