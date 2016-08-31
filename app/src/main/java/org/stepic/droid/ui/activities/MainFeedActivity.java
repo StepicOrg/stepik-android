d4ef6471db723102593195bb16feb6a2edad0cff
Kirill Makarov <kir-maka@yandex.ru>
2016-09-01 00:46:29 +0300

    make My courses root: back go to My Courses

--- app/src/main/java/org/stepic/droid/ui/activities/MainFeedActivity.java
+++ app/src/main/java/org/stepic/droid/ui/activities/MainFeedActivity.java
@@ -10,6 +10,7 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -80,7 +81,6 @@ public class MainFeedActivity extends BackToExitActivityBase
     ImageView mProfileImage;
 
     TextView mUserNameTextView;
-
 
     @BindString(R.string.my_courses_title)
     String mCoursesTitle;
@@ -214,7 +214,15 @@ public class MainFeedActivity extends BackToExitActivityBase
         if (mDrawerLayout.isDrawerOpen(GravityCompat.START)) {
             mDrawerLayout.closeDrawer(GravityCompat.START);
         } else {
-            super.onBackPressed();
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            fragmentManager.popBackStackImmediate();
+            if (mCurrentIndex == 0) {
+                finish();
+            } else {
+                mCurrentIndex = 0;
+                mNavigationView.setCheckedItem(R.id.my_courses);
+                setTitle(R.string.my_courses_title);
+            }
         }
     }
 
