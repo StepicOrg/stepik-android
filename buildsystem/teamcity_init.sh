9fd79b1c6737cc1b56ba4d47872d0b4807eb58e4
Pavel Sviderski <pavel@psviderski.name>
2016-09-05 22:13:57 +0300

    fix teamcity init script

--- buildsystem/teamcity_init.sh
+++ buildsystem/teamcity_init.sh
@@ -3,7 +3,7 @@
 TOOLS_VERSION=`grep buildToolsVersion app/build.gradle | cut -d \" -f 2`
 TOOLS_DIR=$ANDROID_HOME/build-tools/$TOOLS_VERSION
 
-if [ ! -d "TOOLS_DIR" ]; then
+if [ ! -d "$TOOLS_DIR" ]; then
     echo "Installing Android build-tools version $TOOLS_VERSION"
     echo y | $ANDROID_HOME/tools/android update sdk --no-ui --all --filter \
     build-tools-$TOOLS_VERSION
