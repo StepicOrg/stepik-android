3479bb3b18de80e974dd65a5a1fbfa3587cf3f43
Kirill Makarov <kir-maka@yandex.ru>
2016-09-12 20:03:38 +0300

    remake ugly shaking to color animation

--- app/src/main/java/org/stepic/droid/ui/adapters/SectionAdapter.java
+++ app/src/main/java/org/stepic/droid/ui/adapters/SectionAdapter.java
@@ -4,6 +4,9 @@ import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.graphics.drawable.Drawable;
+import android.graphics.drawable.TransitionDrawable;
+import android.os.Build;
+import android.support.annotation.ColorInt;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.content.ContextCompat;
@@ -12,8 +15,6 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
 import android.widget.TextView;
 
 import org.stepic.droid.R;
@@ -75,6 +76,9 @@ public class SectionAdapter extends RecyclerView.Adapter<SectionAdapter.GenericV
     private Course course;
     private boolean needShowCalendarWidget;
     private Drawable highlightDrawable;
+    @ColorInt
+    private int defaultColor;
+    private final int durationMillis = 3000;
 
     public void setDefaultHighlightPosition(int defaultHighlightPosition) {
         this.defaultHighlightPosition = defaultHighlightPosition;
@@ -86,6 +90,7 @@ public class SectionAdapter extends RecyclerView.Adapter<SectionAdapter.GenericV
         mActivity = activity;
         this.calendarPresenter = calendarPresenter;
         highlightDrawable = ContextCompat.getDrawable(mContext, R.drawable.section_background);
+        defaultColor = ColorUtil.INSTANCE.getColorArgb(R.color.stepic_white, mContext);
         MainApplication.component().inject(this);
     }
 
@@ -350,6 +355,8 @@ public class SectionAdapter extends RecyclerView.Adapter<SectionAdapter.GenericV
 
             if (defaultHighlightPosition >= 0 && defaultHighlightPosition == position) {
                 setAnimation(cv);
+            } else {
+                cv.setBackgroundColor(defaultColor);
             }
         }
 
@@ -359,8 +366,13 @@ public class SectionAdapter extends RecyclerView.Adapter<SectionAdapter.GenericV
         }
 
         private void setAnimation(View viewToAnimate) {
-            Animation animation = AnimationUtils.loadAnimation(mContext, R.anim.shake_animation);
-            viewToAnimate.startAnimation(animation);
+            TransitionDrawable drawable = (TransitionDrawable) highlightDrawable;
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+                viewToAnimate.setBackground(highlightDrawable);
+            } else {
+                viewToAnimate.setBackgroundDrawable(highlightDrawable);
+            }
+            drawable.startTransition(durationMillis);
             defaultHighlightPosition = -1;
         }
     }
@@ -434,6 +446,6 @@ public class SectionAdapter extends RecyclerView.Adapter<SectionAdapter.GenericV
     @Override
     public void onViewDetachedFromWindow(GenericViewHolder holder) {
         super.onViewDetachedFromWindow(holder);
-        ((GenericViewHolder)holder).clearAnimation();
+        ((GenericViewHolder) holder).clearAnimation();
     }
 }
