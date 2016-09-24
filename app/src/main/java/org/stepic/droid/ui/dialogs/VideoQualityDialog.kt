f95d6721a0faa623ded484b14e86f205217b73bd
Kirill Makarov <kir-maka@yandex.ru>
2016-09-24 16:45:42 +0300

    extract initialization of video quality dialog to parent class

--- app/src/main/java/org/stepic/droid/ui/dialogs/VideoQualityDialog.kt
+++ app/src/main/java/org/stepic/droid/ui/dialogs/VideoQualityDialog.kt
@@ -2,18 +2,15 @@ package org.stepic.droid.ui.dialogs
 
 import android.app.Dialog
 import android.os.Bundle
-import android.support.v4.app.DialogFragment
 import android.support.v7.app.AlertDialog
 import org.stepic.droid.R
 import org.stepic.droid.analytic.Analytic
 import org.stepic.droid.base.MainApplication
 import org.stepic.droid.preferences.UserPreferences
-import java.util.*
 import java.util.concurrent.ThreadPoolExecutor
 import javax.inject.Inject
 
-class VideoQualityDialog : DialogFragment() {
-
+class VideoQualityDialog : VideoQualityDialogBase() {
     @Inject
     lateinit var userPreferences: UserPreferences
 
@@ -23,14 +20,8 @@ class VideoQualityDialog : DialogFragment() {
     @Inject
     lateinit var threadPoolExecutor: ThreadPoolExecutor
 
-    private val qualityToPositionMap: MutableMap<String, Int> = HashMap()
-    private val positionToQualityMap: MutableMap<Int, String> = HashMap()
-
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-        MainApplication.component().inject(this)
-        if (qualityToPositionMap.isEmpty() || positionToQualityMap.isEmpty()) {
-            initMaps()
-        }
+        init()
 
         val builder = AlertDialog.Builder(activity)
         builder
@@ -54,15 +45,7 @@ class VideoQualityDialog : DialogFragment() {
         return builder.create()
     }
 
-    private fun initMaps() {
-        qualityToPositionMap.put("270", 0)
-        qualityToPositionMap.put("360", 1)
-        qualityToPositionMap.put("720", 2)
-        qualityToPositionMap.put("1080", 3)
-
-        positionToQualityMap.put(0, "270")
-        positionToQualityMap.put(1, "360")
-        positionToQualityMap.put(2, "720")
-        positionToQualityMap.put(3, "1080")
+    override fun injectDependencies() {
+        MainApplication.component().inject(this)
     }
 }
