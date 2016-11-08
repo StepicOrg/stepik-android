d63fbacb9bbb0ffe23cebdfee82aff5d3942e06c
Kirill Makarov <kir-maka@yandex.ru>
2016-11-08 15:25:07 +0300

    post feedback to freshdesk (add converter for Strings)

--- app/src/main/java/org/stepic/droid/web/RetrofitRESTApi.java
+++ app/src/main/java/org/stepic/droid/web/RetrofitRESTApi.java
@@ -48,8 +48,9 @@ import org.stepic.droid.store.operations.Table;
 import org.stepic.droid.ui.NotificationCategory;
 import org.stepic.droid.util.AppConstants;
 import org.stepic.droid.util.DeviceInfoUtil;
-import org.stepic.droid.util.HtmlHelper;
 import org.stepic.droid.util.RWLocks;
+import org.stepic.droid.util.resolvers.text.TextResolver;
+import org.stepic.droid.web.util.StringConverterFactory;
 
 import java.io.IOException;
 import java.net.CookieManager;
@@ -85,6 +86,8 @@ public class RetrofitRESTApi implements IApi {
     UserPreferences userPreferences;
     @Inject
     Analytic analytic;
+    @Inject
+    TextResolver textResolver;
 
     private StepicRestLoggedService loggedService;
     private StepicRestOAuthService oAuthService;
@@ -116,7 +119,7 @@ public class RetrofitRESTApi implements IApi {
 //                .baseUrl(mConfig.getZendeskHost())
 //                .client(okHttpClient)
 //                .build();
-//        mZendeskAuthService = retrofit.create(StepicZendeskEmptyAuthService.class);
+//        mZendeskAuthService = retrofit.create(StepikDeskEmptyAuthService.class);
 //    }
 
 
@@ -480,79 +483,18 @@ public class RetrofitRESTApi implements IApi {
     @Override
     public Call<Void> sendFeedback(String email, String rawDescription) {
 
-        Interceptor interceptor = new Interceptor() {
-            @Override
-            public Response intercept(Chain chain) throws IOException {
-                Request newRequest = chain.request();
-
-                String csrftoken = null;
-                String zendesk_shared_session = null;
-                String help_center_session = null;
-                try {
-                    Response response = getZendeskResponse();
-                    String htmlText = response.body().string();
-                    csrftoken = HtmlHelper.getValueOfMetaOrNull(htmlText, "csrf-token");
-
-                    Headers headers = response.headers();
-                    CookieManager cookieManager = new CookieManager();
-                    URI myUri = null;
-                    try {
-                        myUri = new URI(config.getZendeskHost());
-                    } catch (URISyntaxException e) {
-                        return null;
-                    }
-                    cookieManager.put(myUri, headers.toMultimap());
-                    List<HttpCookie> cookies = cookieManager.getCookieStore().get(myUri);
-
-                    for (HttpCookie item : cookies) {
-                        if (item.getName() != null && item.getName().equals("_zendesk_shared_session")) {
-                            zendesk_shared_session = item.getValue();
-                            continue;
-                        }
-                        if (item.getName() != null && item.getName().equals("_help_center_session")) {
-                            help_center_session = item.getValue();
-                        }
-                    }
-
-                } catch (Exception e) {
-                    return chain.proceed(newRequest);
-                }
-                if (csrftoken == null && zendesk_shared_session == null && help_center_session == null) {
-                    return chain.proceed(newRequest);
-                }
-
-
-                String cookieResult = "_zendesk_shared_session=" + zendesk_shared_session + "; " + "_help_center_session=" + help_center_session;
-
-                HttpUrl url = newRequest
-                        .httpUrl()
-                        .newBuilder()
-                        .addQueryParameter("authenticity_token", csrftoken)
-                        .build();
-                newRequest = newRequest.newBuilder()
-                        .addHeader("Cookie", cookieResult)
-                        .url(url)
-                        .build();
-                return chain.proceed(newRequest);
-            }
-        };
 
         OkHttpClient okHttpClient = new OkHttpClient();
-        okHttpClient.networkInterceptors().add(interceptor);
         Retrofit notLogged = new Retrofit.Builder()
                 .baseUrl(config.getZendeskHost())
-                .addConverterFactory(generateGsonFactory())
+                .addConverterFactory(StringConverterFactory.create())
                 .client(okHttpClient)
                 .build();
-        StepicZendeskEmptyAuthService tempService = notLogged.create(StepicZendeskEmptyAuthService.class);
+        StepikDeskEmptyAuthService tempService = notLogged.create(StepikDeskEmptyAuthService.class);
 
-        String encodedEmail = URLEncoder.encode(email);
-        String encodedDescription = URLEncoder.encode(rawDescription);
         String subject = MainApplication.getAppContext().getString(R.string.feedback_subject);
-        String encodedSubject = URLEncoder.encode(subject);
         String aboutSystem = DeviceInfoUtil.getInfosAboutDevice(MainApplication.getAppContext());
-        String encodedSystem = URLEncoder.encode(aboutSystem);
-        return tempService.sendFeedback(encodedSubject, encodedEmail, encodedSystem, encodedDescription, config.getBaseUrl());
+        return tempService.sendFeedback(subject, email, aboutSystem, rawDescription);
     }
 
     @Override
@@ -668,19 +610,6 @@ public class RetrofitRESTApi implements IApi {
             categoryType = notificationCategory.name();
         }
         return categoryType;
-    }
-
-    @Nullable
-    private Response getZendeskResponse() throws IOException {
-        OkHttpClient client = new OkHttpClient();
-
-        String url = config.getZendeskHost() + "/hc/en-us/requests/new";
-
-        Request request = new Request.Builder()
-                .url(url)
-                .build();
-
-        return client.newCall(request).execute();
     }
 
     @Nullable
