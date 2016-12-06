4aedb8b8b3cdd4747313e08dd5a22fd5927d22ad
Kirill Makarov <kir-maka@yandex.ru>
2016-12-06 17:43:12 +0300

    remove obsolete code from launch activity

--- app/src/classic/java/org.stepic.droid/ui/activities/LaunchActivity.java
+++ app/src/classic/java/org.stepic.droid/ui/activities/LaunchActivity.java
@@ -1,15 +1,10 @@
 package org.stepic.droid.ui.activities;
 
-import android.content.BroadcastReceiver;
-import android.content.Context;
-import android.content.Intent;
-import android.content.IntentFilter;
 import android.os.Bundle;
 import android.view.View;
 
 import org.stepic.droid.R;
 import org.stepic.droid.analytic.Analytic;
-import org.stepic.droid.util.AppConstants;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -20,10 +15,10 @@ public class LaunchActivity extends BackToExitActivityBase {
     public static final String OVERRIDE_ANIMATION_FLAG = "override_animation_flag";
 
     @BindView(R.id.sign_up_btn_activity_launch)
-    View mSignUpButton;
+    View signUpButton;
 
     @BindView(R.id.sign_in_tv)
-    View mSignInTextView;
+    View signInTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,15 +26,13 @@ public class LaunchActivity extends BackToExitActivityBase {
         setContentView(R.layout.activity_launch);
         unbinder = ButterKnife.bind(this);
 
-        enableLoginCallback();
-
         //Activity override animation has to be handled if the Launch Activity
         //is called after user logs out and closes the Sign-in screen.
         if (getIntent().getBooleanExtra(OVERRIDE_ANIMATION_FLAG, false)) {
             overridePendingTransition(R.anim.no_transition, R.anim.slide_out_to_bottom);
         }
 
-        mSignUpButton.setOnClickListener((new View.OnClickListener() {
+        signUpButton.setOnClickListener((new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 analytic.reportEvent(Analytic.Interaction.CLICK_SIGN_UP);
@@ -47,7 +40,7 @@ public class LaunchActivity extends BackToExitActivityBase {
             }
         }));
 
-        mSignInTextView.setOnClickListener(new View.OnClickListener() {
+        signInTextView.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
@@ -55,44 +48,5 @@ public class LaunchActivity extends BackToExitActivityBase {
                 shell.getScreenProvider().showLogin(LaunchActivity.this);
             }
         });
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        bus.register(this);
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        bus.unregister(this);
-    }
-
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        disableLoginCallback();
-    }
-
-    //Broadcast Receiver to notify all activities to onFinish if user logs out
-    private BroadcastReceiver loginReceiver = new BroadcastReceiver() {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            finish();
-        }
-    };
-
-    protected void enableLoginCallback() {
-        // register for login listener
-        IntentFilter filter = new IntentFilter();
-        filter.addAction(AppConstants.USER_LOG_IN);
-        registerReceiver(loginReceiver, filter);
-    }
-
-    protected void disableLoginCallback() {
-        // un-register loginReceiver
-        unregisterReceiver(loginReceiver);
     }
 }
