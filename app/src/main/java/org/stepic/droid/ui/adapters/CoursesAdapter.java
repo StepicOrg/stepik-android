5f9a1d87dcb3a5cb16c257afa9a9b437049b020c
Kirill Makarov <kir-maka@yandex.ru>
2016-12-09 21:50:10 +0300

    remake course list to recycler

--- app/src/main/java/org/stepic/droid/ui/adapters/CoursesAdapter.java
+++ app/src/main/java/org/stepic/droid/ui/adapters/CoursesAdapter.java
@@ -1,16 +1,17 @@
 package org.stepic.droid.ui.adapters;
 
+import android.app.Activity;
 import android.content.Context;
 import android.graphics.drawable.Drawable;
-import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.support.v4.content.ContextCompat;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.target.GlideDrawableImageViewTarget;
@@ -19,6 +20,7 @@ import org.jetbrains.annotations.Nullable;
 import org.stepic.droid.R;
 import org.stepic.droid.base.MainApplication;
 import org.stepic.droid.configuration.IConfig;
+import org.stepic.droid.core.IShell;
 import org.stepic.droid.model.Course;
 import org.stepic.droid.store.operations.Table;
 import org.stepic.droid.util.StepicLogicHelper;
@@ -31,7 +33,8 @@ import javax.inject.Inject;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class CoursesAdapter extends ArrayAdapter<Course> {
+public class CoursesAdapter extends RecyclerView.Adapter<CoursesAdapter.CourseViewHolderBase> {
+
 
     @Inject
     IConfig config;
@@ -39,46 +42,87 @@ public class CoursesAdapter extends ArrayAdapter<Course> {
     @Inject
     TextResolver textResolver;
 
+    @Inject
+    IShell shell;
+
     private Drawable coursePlaceholder;
 
     private LayoutInflater inflater;
 
-    public CoursesAdapter(Fragment fragment, List<Course> courses, @Nullable Table type) {
-        super(fragment.getActivity(), 0, courses);
-        inflater = (LayoutInflater) fragment.getActivity().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    private Activity contextActivity;
+    private final List<Course> courses;
+    private int footerViewType = 1;
+    private int itemViewType = 2;
+    private int NUMBER_OF_EXTRA_ITEMS = 1;
+    private boolean isNeedShowFooter;
 
+    public CoursesAdapter(Fragment fragment, List<Course> courses, @Nullable Table type) {
+        contextActivity = fragment.getActivity();
+        this.courses = courses;
+        inflater = (LayoutInflater) contextActivity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         MainApplication.component().inject(this);
         coursePlaceholder = ContextCompat.getDrawable(fragment.getContext(), R.drawable.ic_course_placeholder);
     }
 
-    @NonNull
     @Override
-    public View getView(final int position, View convertView, @NonNull ViewGroup parent) {
-        final Course course = getItem(position);
-
-        View view = convertView;
-        // Check if an existing view is being reused, otherwise inflate the view
-        ViewHolderItem viewHolderItem = null;
-        if (view == null) {
-            view = inflater.inflate(R.layout.course_item, null);
-            viewHolderItem = new ViewHolderItem(view);
-            view.setTag(viewHolderItem);
+    public CourseViewHolderBase onCreateViewHolder(ViewGroup parent, int viewType) {
+        if (viewType == footerViewType) {
+            View view = inflater.inflate(R.layout.loading_view, parent, false);
+            return new FooterViewHolderItem(view);
+        } else if (itemViewType == viewType) {
+            View view = inflater.inflate(R.layout.course_item, parent, false);
+            return new CourseViewHolderItem(view);
         } else {
-            viewHolderItem = (ViewHolderItem) convertView.getTag();
+            throw new IllegalStateException("Not valid item type");
         }
-        viewHolderItem.courseName.setText(course.getTitle());
-        viewHolderItem.courseSummary.setText(textResolver.fromHtml(course.getSummary()).toString());
-
-        Glide
-                .with(getContext())
-                .load(StepicLogicHelper.getPathForCourseOrEmpty(course, config))
-                .placeholder(coursePlaceholder)
-                .centerCrop()
-                .into(viewHolderItem.imageViewTarget);
-        return view;
     }
 
-    static class ViewHolderItem {
+    @Override
+    public void onBindViewHolder(CourseViewHolderBase holder, int position) {
+        holder.setDataOnView(position);
+    }
+
+
+    @Override
+    public int getItemViewType(int position) {
+        if (position == getItemCount() - 1) {
+            return footerViewType;
+        } else {
+            return itemViewType;
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return courses.size() + NUMBER_OF_EXTRA_ITEMS;
+    }
+
+    private void onClickContinue(int position) {
+        Toast.makeText(contextActivity, "you click continue at " + position, Toast.LENGTH_SHORT).show(); //// FIXME: 09.12.16 open smth
+    }
+
+    private void onClickCourse(int position) {
+        if (position >= courses.size() || position < 0) return;
+        Course course = courses.get(position);
+        if (course.getEnrollment() != 0) {
+            shell.getScreenProvider().showSections(contextActivity, course);
+        } else {
+            shell.getScreenProvider().showCourseDescription(contextActivity, course);
+        }
+    }
+
+
+    abstract static class CourseViewHolderBase extends RecyclerView.ViewHolder {
+
+        public CourseViewHolderBase(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+        }
+
+        abstract void setDataOnView(int position);
+    }
+
+    class CourseViewHolderItem extends CourseViewHolderBase {
 
         @BindView(R.id.course_name)
         TextView courseName;
@@ -94,19 +138,55 @@ public class CoursesAdapter extends ArrayAdapter<Course> {
 
         GlideDrawableImageViewTarget imageViewTarget;
 
-        public ViewHolderItem(View view) {
-            ButterKnife.bind(this, view);
+        CourseViewHolderItem(View itemView) {
+            super(itemView);
             imageViewTarget = new GlideDrawableImageViewTarget(courseIcon);
             continueButton.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    CoursesAdapter.this.onClickContinue(getAdapterPosition());
+                }
+            });
+            itemView.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
+                    CoursesAdapter.this.onClickCourse(getAdapterPosition());
                 }
             });
         }
+
+        @Override
+        void setDataOnView(int position) {
+            final Course course = courses.get(position);
+
+            courseName.setText(course.getTitle());
+            courseSummary.setText(textResolver.fromHtml(course.getSummary()).toString());
+            Glide
+                    .with(contextActivity)
+                    .load(StepicLogicHelper.getPathForCourseOrEmpty(course, config))
+                    .placeholder(coursePlaceholder)
+                    .centerCrop()
+                    .into(imageViewTarget);
+        }
     }
 
-    private interface OnClickContinueListener {
-        void onClickContinue(int position);
+    class FooterViewHolderItem extends CourseViewHolderBase {
+
+        @BindView(R.id.loading_root)
+        View loadingRoot;
+
+        FooterViewHolderItem(View itemView) {
+            super(itemView);
+        }
+
+        @Override
+        void setDataOnView(int position) {
+            loadingRoot.setVisibility(isNeedShowFooter ? View.VISIBLE : View.GONE);
+        }
+    }
+
+    public void showLoadingFooter(boolean isNeedShow) {
+        isNeedShowFooter = isNeedShow;
+        notifyItemChanged(getItemCount() - 1);
     }
 
 }
--- app/src/main/java/org/stepic/droid/ui/fragments/CourseListFragmentBase.java
+++ app/src/main/java/org/stepic/droid/ui/fragments/CourseListFragmentBase.java
@@ -1,17 +1,15 @@
 package org.stepic.droid.ui.fragments;
 
 import android.app.Activity;
-import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AbsListView;
-import android.widget.AdapterView;
 import android.widget.Button;
-import android.widget.ListView;
 import android.widget.ProgressBar;
 
 import com.squareup.otto.Subscribe;
@@ -34,6 +32,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import butterknife.BindView;
+import timber.log.Timber;
 
 public abstract class CourseListFragmentBase extends FragmentBase implements SwipeRefreshLayout.OnRefreshListener, CoursesView {
 
@@ -41,7 +40,7 @@ public abstract class CourseListFragmentBase extends FragmentBase implements Swi
     protected SwipeRefreshLayout swipeRefreshLayout;
 
     @BindView(R.id.list_of_courses)
-    protected ListView listOfCoursesView;
+    protected RecyclerView listOfCoursesView;
 
     @BindView(R.id.report_problem)
     protected View reportConnectionProblem;
@@ -65,8 +64,8 @@ public abstract class CourseListFragmentBase extends FragmentBase implements Swi
     protected CoursesAdapter coursesAdapter;
 
     protected boolean userScrolled;
-
-    protected View footerDownloadingView;
+    private RecyclerView.OnScrollListener listOfCoursesViewListener;
+    private LinearLayoutManager layoutManager;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -94,45 +93,32 @@ public abstract class CourseListFragmentBase extends FragmentBase implements Swi
 
         if (courses == null) courses = new ArrayList<>();
 
-        footerDownloadingView = ((LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE)).inflate(R.layout.loading_view, null, false);
-        footerDownloadingView.setVisibility(View.GONE);
-        listOfCoursesView.addFooterView(footerDownloadingView);
-
         registerForContextMenu(listOfCoursesView);
 
         coursesAdapter = new CoursesAdapter(this, courses, getCourseType());
         listOfCoursesView.setAdapter(coursesAdapter);
+        layoutManager = new LinearLayoutManager(getContext());
+        listOfCoursesView.setLayoutManager(layoutManager);
 
-        listOfCoursesView.setOnScrollListener(new AbsListView.OnScrollListener() {
-
+        listOfCoursesViewListener = new RecyclerView.OnScrollListener() {
             @Override
-            public void onScrollStateChanged(AbsListView view, int scrollState) {
-                if (scrollState == AbsListView.OnScrollListener.SCROLL_STATE_TOUCH_SCROLL) {
-                    userScrolled = true; // just for 1st creation
-                }
-            }
+            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+                if (dy > 0) //check for scroll down
+                {
+                    int visibleItemCount = layoutManager.getChildCount();
+                    int totalItemCount = layoutManager.getItemCount();
+                    int pastVisibleItems = layoutManager.findFirstVisibleItemPosition();
+                    Timber.d("visibleItemCount = %d, totalItemCount = %d, pastVisibleItems=%d", visibleItemCount, totalItemCount, pastVisibleItems);
 
-            @Override
-            public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
-                if (firstVisibleItem + visibleItemCount >= totalItemCount && userScrolled && StepikUtil.INSTANCE.isInternetAvailable()) {
-                    //check inside more expensive condition:
-                    onNeedDownloadNextPage();
+                    if ((visibleItemCount + pastVisibleItems) >= totalItemCount && StepikUtil.INSTANCE.isInternetAvailable()) {
+                        onNeedDownloadNextPage();
+                    }
                 }
-            }
-        });
 
-        listOfCoursesView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                if (position >= courses.size() || position < 0) return;
-                Course course = courses.get(position);
-                if (course.getEnrollment() != 0) {
-                    shell.getScreenProvider().showSections(getActivity(), course);
-                } else {
-                    shell.getScreenProvider().showCourseDescription(CourseListFragmentBase.this, course);
-                }
             }
-        });
+        };
+        listOfCoursesView.addOnScrollListener(listOfCoursesViewListener);
+
 
         findCourseButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -150,8 +136,6 @@ public abstract class CourseListFragmentBase extends FragmentBase implements Swi
     public void onDestroyView() {
         if (listOfCoursesView != null) {
             listOfCoursesView.setAdapter(null);
-            listOfCoursesView.setOnScrollListener(null);
-            listOfCoursesView.setOnItemClickListener(null);
         }
         super.onDestroyView();
     }
@@ -184,13 +168,13 @@ public abstract class CourseListFragmentBase extends FragmentBase implements Swi
     public void showLoading() {
         ProgressHelper.dismiss(progressBarOnEmptyScreen);
         ProgressHelper.dismiss(swipeRefreshLayout);
-        footerDownloadingView.setVisibility(View.GONE);
+        coursesAdapter.showLoadingFooter(false);
         reportConnectionProblem.setVisibility(View.GONE);
 
         if (courses.isEmpty()) {
             ProgressHelper.activate(swipeRefreshLayout);
         } else if (swipeRefreshLayout != null && !swipeRefreshLayout.isRefreshing()) {
-            footerDownloadingView.setVisibility(View.VISIBLE);
+            coursesAdapter.showLoadingFooter(true);
         }
     }
 
@@ -198,7 +182,6 @@ public abstract class CourseListFragmentBase extends FragmentBase implements Swi
     public void showEmptyCourses() {
         ProgressHelper.dismiss(progressBarOnEmptyScreen);
         ProgressHelper.dismiss(swipeRefreshLayout);
-        footerDownloadingView.setVisibility(View.GONE);
         reportConnectionProblem.setVisibility(View.GONE);
         if (courses.isEmpty()) {
             showEmptyScreen(true);
@@ -210,7 +193,7 @@ public abstract class CourseListFragmentBase extends FragmentBase implements Swi
     public void showConnectionProblem() {
         ProgressHelper.dismiss(progressBarOnEmptyScreen);
         ProgressHelper.dismiss(swipeRefreshLayout);
-        footerDownloadingView.setVisibility(View.GONE);
+        coursesAdapter.showLoadingFooter(true);
 
         if (courses == null || courses.isEmpty()) {
             //screen is clear due to error connection
@@ -223,7 +206,7 @@ public abstract class CourseListFragmentBase extends FragmentBase implements Swi
     public void showCourses(List<Course> courses) {
         ProgressHelper.dismiss(progressBarOnEmptyScreen);
         ProgressHelper.dismiss(swipeRefreshLayout);
-        footerDownloadingView.setVisibility(View.GONE);
+        coursesAdapter.showLoadingFooter(false);
         reportConnectionProblem.setVisibility(View.GONE);
         showEmptyScreen(false);
         List<Course> finalCourses = KotlinUtil.INSTANCE.getListOldPlusUpdated(this.courses, courses);
