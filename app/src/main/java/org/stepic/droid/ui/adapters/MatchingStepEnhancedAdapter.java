7a5ecd0e25c039ec78b8c67c05c698ef25085a86
Kirill Makarov <kir-maka@yandex.ru>
2017-01-17 19:03:47 +0300

    draw only once matching quiz

--- app/src/main/java/org/stepic/droid/ui/adapters/MatchingStepEnhancedAdapter.java
+++ app/src/main/java/org/stepic/droid/ui/adapters/MatchingStepEnhancedAdapter.java
@@ -1,9 +1,14 @@
 package org.stepic.droid.ui.adapters;
 
+import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
+
 import org.stepic.droid.model.Option;
 
 import java.util.Collections;
 import java.util.List;
+
+import timber.log.Timber;
 
 public class MatchingStepEnhancedAdapter extends SortingStepEnhancedAdapter {
     int draggableUpperBound;
@@ -18,6 +23,27 @@ public class MatchingStepEnhancedAdapter extends SortingStepEnhancedAdapter {
 //        return new ItemDraggableRange(0, draggableUpperBound);
 //    }
 
+
+    @Override
+    public OptionViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        final OptionViewHolder optionViewHolder = super.onCreateViewHolder(parent, viewType);
+        optionViewHolder.container.getViewTreeObserver().addOnPreDrawListener(new ViewTreeObserver.OnPreDrawListener() {
+            @Override
+            public boolean onPreDraw() {
+                int localHeight = optionViewHolder.container.getMeasuredHeight();
+                int localWidth = optionViewHolder.container.getMeasuredWidth();
+                Timber.d("MeasuredHeight = %s", localHeight);
+                if (localHeight != 0) {
+                    optionViewHolder.container.getLayoutParams().height = localHeight;
+                    optionViewHolder.container.getViewTreeObserver().removeOnPreDrawListener(this);
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        });
+        return optionViewHolder;
+    }
 
     @Override
     public boolean onCheckCanStartDrag(OptionViewHolder holder, int position, int x, int y) {
