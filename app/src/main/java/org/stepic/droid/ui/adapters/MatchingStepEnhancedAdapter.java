11cb6fbf6275725e227605de5edac0a750731eca
Kirill Makarov <kir-maka@yandex.ru>
2017-01-17 19:10:10 +0300

    refactor code in Matching Adapter

--- app/src/main/java/org/stepic/droid/ui/adapters/MatchingStepEnhancedAdapter.java
+++ app/src/main/java/org/stepic/droid/ui/adapters/MatchingStepEnhancedAdapter.java
@@ -11,18 +11,10 @@ import java.util.List;
 import timber.log.Timber;
 
 public class MatchingStepEnhancedAdapter extends SortingStepEnhancedAdapter {
-    int draggableUpperBound;
 
     public MatchingStepEnhancedAdapter(List<Option> data) {
         super(data);
-        draggableUpperBound = (data.size() / 2) - 1;
     }
-
-//    @Override
-//    public ItemDraggableRange onGetItemDraggableRange(OptionViewHolder holder, int position) {
-//        return new ItemDraggableRange(0, draggableUpperBound);
-//    }
-
 
     @Override
     public OptionViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
@@ -31,15 +23,12 @@ public class MatchingStepEnhancedAdapter extends SortingStepEnhancedAdapter {
             @Override
             public boolean onPreDraw() {
                 int localHeight = optionViewHolder.container.getMeasuredHeight();
-                int localWidth = optionViewHolder.container.getMeasuredWidth();
                 Timber.d("MeasuredHeight = %s", localHeight);
                 if (localHeight != 0) {
                     optionViewHolder.container.getLayoutParams().height = localHeight;
                     optionViewHolder.container.getViewTreeObserver().removeOnPreDrawListener(this);
-                    return true;
-                } else {
-                    return false;
                 }
+                return true;
             }
         });
         return optionViewHolder;
@@ -47,11 +36,7 @@ public class MatchingStepEnhancedAdapter extends SortingStepEnhancedAdapter {
 
     @Override
     public boolean onCheckCanStartDrag(OptionViewHolder holder, int position, int x, int y) {
-        if (position % 2 == 0) {
-            return false;
-        } else {
-            return super.onCheckCanStartDrag(holder, position, x, y);
-        }
+        return position % 2 != 0 && super.onCheckCanStartDrag(holder, position, x, y);
     }
 
     @Override
@@ -65,7 +50,6 @@ public class MatchingStepEnhancedAdapter extends SortingStepEnhancedAdapter {
 
     @Override
     public boolean onCheckCanDrop(int draggingPosition, int dropPosition) {
-        boolean canDrop = dropPosition % 2 != 0;
-        return canDrop;
+        return dropPosition % 2 != 0;
     }
 }
