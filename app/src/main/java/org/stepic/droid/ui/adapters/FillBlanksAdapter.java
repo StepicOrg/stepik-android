149b6be515d6c17183dcc213739117785f120bde
Kirill Makarov <kir-maka@yandex.ru>
2017-01-23 11:20:15 +0300

    make base implementation of fill the blanks view (need tune styles up)

---
+++ app/src/main/java/org/stepic/droid/ui/adapters/FillBlanksAdapter.java
@@ -0,0 +1,143 @@
+package org.stepic.droid.ui.adapters;
+
+import android.support.v7.widget.AppCompatSpinner;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+
+import org.jetbrains.annotations.NotNull;
+import org.stepic.droid.R;
+import org.stepic.droid.model.FillBlankComponent;
+import org.stepic.droid.ui.custom.ProgressLatexView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class FillBlanksAdapter extends RecyclerView.Adapter<FillBlanksAdapter.FillBlankViewHolderBase> {
+    final static int TEXT_TYPE = 0;
+    final static int INPUT_TYPE = 1;
+    final static int SELECT_TYPE = 2;
+
+    private List<FillBlankComponent> componentList;
+
+    public FillBlanksAdapter(@NotNull List<FillBlankComponent> componentList) {
+        this.componentList = componentList;
+    }
+
+    @Override
+    public FillBlankViewHolderBase onCreateViewHolder(ViewGroup parent, int viewType) {
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        if (viewType == TEXT_TYPE) {
+            View view = layoutInflater.inflate(R.layout.fill_blanks_text_option, parent, false);
+            return new TextViewHolder(view);
+        } else if (viewType == INPUT_TYPE) {
+            View view = layoutInflater.inflate(R.layout.fill_blanks_input_option, parent, false);
+            return new InputViewHolder(view);
+        } else if (viewType == SELECT_TYPE) {
+            View view = layoutInflater.inflate(R.layout.fill_blanks_select_option, parent, false);
+            return new SelectViewHolder(view);
+        } else {
+            throw new IllegalStateException("view type of fill blanks was illegal");
+        }
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        FillBlankComponent fillBlankComponent = componentList.get(position);
+        switch (fillBlankComponent.getType()) {
+            case text:
+                return TEXT_TYPE;
+            case input:
+                return INPUT_TYPE;
+            case select:
+                return SELECT_TYPE;
+            default:
+                throw new IllegalStateException("Illegal type of fill blank component");
+        }
+    }
+
+    @Override
+    public void onBindViewHolder(FillBlankViewHolderBase holder, int position) {
+        FillBlankComponent fillBlankComponent = componentList.get(position);
+        holder.bindData(fillBlankComponent);
+    }
+
+    @Override
+    public int getItemCount() {
+        return componentList.size();
+    }
+
+    static abstract class FillBlankViewHolderBase extends RecyclerView.ViewHolder {
+
+        public FillBlankViewHolderBase(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+        }
+
+        public abstract void bindData(FillBlankComponent fillBlankComponent);
+    }
+
+
+    static class TextViewHolder extends FillBlankViewHolderBase {
+
+        @BindView(R.id.latex_text_fill_blanks)
+        ProgressLatexView progressLatexView;
+
+
+        public TextViewHolder(View itemView) {
+            super(itemView);
+        }
+
+        @Override
+        public void bindData(FillBlankComponent fillBlankComponent) {
+            progressLatexView.setPlainOrLaTeXText(fillBlankComponent.getText()); //// FIXME: 23.01.17 HTML is allowed here
+        }
+    }
+
+    static class SelectViewHolder extends FillBlankViewHolderBase {
+
+        @BindView(R.id.spinner_fill_blanks)
+        AppCompatSpinner spinner;
+
+        ArrayAdapter<String> dataAdapter;
+
+        public SelectViewHolder(View itemView) {
+            super(itemView);
+            dataAdapter = new ArrayAdapter<>(itemView.getContext(), android.R.layout.simple_spinner_item, new ArrayList<String>());
+            spinner.setAdapter(dataAdapter);
+        }
+
+        @Override
+        public void bindData(FillBlankComponent fillBlankComponent) {
+            List<String> options = fillBlankComponent.getOptions();
+            if (options == null) {
+                throw new IllegalArgumentException("options in select of fill blank cannot be null");
+            }
+            dataAdapter.clear();
+            dataAdapter.addAll(options);
+            dataAdapter.notifyDataSetChanged();
+        }
+    }
+
+    static class InputViewHolder extends FillBlankViewHolderBase {
+
+        @BindView(R.id.input_view_fill_blanks)
+        EditText editText;
+
+        public InputViewHolder(View itemView) {
+            super(itemView);
+        }
+
+        @Override
+        public void bindData(FillBlankComponent fillBlankComponent) {
+            //// TODO: 23.01.17 we should restore data
+        }
+    }
+
+}
--- app/src/main/java/org/stepic/droid/ui/fragments/FillBlanksFragment.java
+++ app/src/main/java/org/stepic/droid/ui/fragments/FillBlanksFragment.java
@@ -1,19 +1,12 @@
 package org.stepic.droid.ui.fragments;
 
 import android.content.Context;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
-import android.graphics.RectF;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.text.SpannableString;
-import android.text.Spanned;
-import android.text.style.BackgroundColorSpan;
-import android.text.style.ReplacementSpan;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.TextView;
 
 import com.squareup.otto.Subscribe;
 
@@ -24,7 +17,7 @@ import org.stepic.droid.events.steps.StepWasUpdatedEvent;
 import org.stepic.droid.model.Attempt;
 import org.stepic.droid.model.FillBlankComponent;
 import org.stepic.droid.model.Reply;
-import org.stepic.droid.util.ColorUtil;
+import org.stepic.droid.ui.adapters.FillBlanksAdapter;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -33,30 +26,26 @@ import butterknife.ButterKnife;
 
 public class FillBlanksFragment extends StepAttemptFragment {
 
-    TextView textViewFillBlanks;
-    private int flags = Spanned.SPAN_EXCLUSIVE_EXCLUSIVE;
-
+    private RecyclerView recyclerContainer;
+    private final List<FillBlankComponent> componentList = new ArrayList<>();
 
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         View fillBlanksView = ((LayoutInflater) this.getActivity().getSystemService(Context.LAYOUT_INFLATER_SERVICE)).inflate(R.layout.view_fill_blanks, attemptContainer, false);
-        textViewFillBlanks = ButterKnife.findById(fillBlanksView, R.id.textViewFillBlanks);
+        recyclerContainer = ButterKnife.findById(fillBlanksView, R.id.recycler);
         attemptContainer.addView(fillBlanksView);
+
+        recyclerContainer.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false));
     }
 
     @Override
     protected void showAttempt(Attempt attempt) {
-        List<FillBlankComponent> list = attempt.getDataset().getFillBlankComponents();
-
-        String source = "Hello it is spannable text ";
-        SpannableString text = new SpannableString(source + source + source + source);
-        text.setSpan(new BackgroundColorSpan(ColorUtil.INSTANCE.getColorArgb(R.color.stepic_orange_carrot, getContext())), 6, source.length() * 2 + 10, flags);
-        textViewFillBlanks.setText(text);
-
-
-        //// TODO: 20.01.17   show components from reply
-
+        componentList.clear();
+        componentList.addAll(attempt.getDataset().getFillBlankComponents());
+        FillBlanksAdapter fillBlanksAdapter = new FillBlanksAdapter(componentList);
+        recyclerContainer.setAdapter(fillBlanksAdapter);
+        recyclerContainer.getAdapter().notifyDataSetChanged();
     }
 
     @Override
@@ -97,77 +86,6 @@ public class FillBlanksFragment extends StepAttemptFragment {
     @Subscribe
     public void onStepWasUpdated(StepWasUpdatedEvent event) {
         super.onStepWasUpdated(event);
-    }
-
-    public static class LOLLL extends ReplacementSpan {
-        private static final float PADDING = 150.0f;
-        private int backgroundColor = 0;
-        private int textColor = 0;
-        private float margin = 16f;
-
-        public LOLLL(Context context) {
-            super();
-            backgroundColor = context.getResources().getColor(R.color.stepic_orange_carrot);
-            textColor = context.getResources().getColor(R.color.black);
-        }
-
-        @Override
-        public void draw(Canvas canvas, CharSequence text, int start, int end, float x, int top, int y, int bottom, Paint paint) {
-            canvas.drawColor(Color.RED);
-            RectF rect = new RectF(x + margin, top, x + measureText(paint, text, start, end) + PADDING - margin, bottom);
-            paint.setColor(backgroundColor);
-            canvas.drawRect(rect, paint);
-            paint.setColor(textColor);
-//            canvas.drawText(text, start, end, x, y, paint);
-            int xPos = Math.round(x + (PADDING / 2));
-//            int yPos = (int) ((canvas.getHeight() / 2) - ((paint.descent() + paint.ascent()) / 2));
-            canvas.drawText(text, start, end, xPos, y, paint);
-
-            paint.setStrokeWidth(10f);
-            paint.setStyle(Paint.Style.STROKE);
-            paint.setColor(Color.BLACK);
-            canvas.drawRect(rect, paint);
-        }
-
-        @Override
-        public int getSize(Paint paint, CharSequence text, int start, int end, Paint.FontMetricsInt fm) {
-            return Math.round(paint.measureText(text, start, end) + PADDING);
-        }
-
-        private float measureText(Paint paint, CharSequence text, int start, int end) {
-            return paint.measureText(text, start, end);
-        }
-    }
-
-    public static class RoundedBackgroundSpan extends ReplacementSpan {
-
-        private static int CORNER_RADIUS = 8;
-        private int backgroundColor = 0;
-        private int textColor = 0;
-
-        public RoundedBackgroundSpan(Context context) {
-            super();
-            backgroundColor = context.getResources().getColor(R.color.stepic_orange_carrot);
-            textColor = context.getResources().getColor(R.color.white);
-        }
-
-        @Override
-        public void draw(Canvas canvas, CharSequence text, int start, int end, float x, int top, int y, int bottom, Paint paint) {
-            RectF rect = new RectF(x, top, x + measureText(paint, text, start, end), bottom);
-            paint.setColor(backgroundColor);
-            canvas.drawRoundRect(rect, CORNER_RADIUS, CORNER_RADIUS, paint);
-            paint.setColor(textColor);
-            canvas.drawText(text, start, end, x, y, paint);
-        }
-
-        @Override
-        public int getSize(Paint paint, CharSequence text, int start, int end, Paint.FontMetricsInt fm) {
-            return Math.round(paint.measureText(text, start, end));
-        }
-
-        private float measureText(Paint paint, CharSequence text, int start, int end) {
-            return paint.measureText(text, start, end);
-        }
     }
 
 }
