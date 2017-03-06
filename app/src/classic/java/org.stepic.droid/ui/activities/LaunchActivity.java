fe07660a7249287a7398daac46e871d765a46f58
Kirlill Makarov <kir-maka@yandex.ru>
2017-03-06 21:26:43 +0300

    make the 1st dumb version of google sign in lock

--- app/src/classic/java/org.stepic.droid/ui/activities/LaunchActivity.java
+++ app/src/classic/java/org.stepic.droid/ui/activities/LaunchActivity.java
@@ -3,6 +3,7 @@ package org.stepic.droid.ui.activities;
 import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.content.IntentSender;
 import android.graphics.Point;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -22,13 +23,20 @@ import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 import com.google.android.gms.appindexing.AppIndex;
 import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.credentials.Credential;
+import com.google.android.gms.auth.api.credentials.CredentialRequest;
+import com.google.android.gms.auth.api.credentials.CredentialRequestResult;
+import com.google.android.gms.auth.api.credentials.IdentityProviders;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.auth.api.signin.GoogleSignInResult;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.Scopes;
+import com.google.android.gms.common.api.CommonStatusCodes;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
 import com.google.android.gms.common.api.Scope;
+import com.google.android.gms.common.api.Status;
 import com.vk.sdk.VKAccessToken;
 import com.vk.sdk.VKCallback;
 import com.vk.sdk.VKSdk;
@@ -50,6 +58,7 @@ import org.stepic.droid.util.ProgressHelper;
 import butterknife.BindString;
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import timber.log.Timber;
 
 
 public class LaunchActivity extends BackToExitActivityBase {
@@ -126,6 +135,7 @@ public class LaunchActivity extends BackToExitActivityBase {
                             Toast.makeText(LaunchActivity.this, R.string.connectionProblems, Toast.LENGTH_SHORT).show();
                         }
                     } /* OnConnectionFailedListener */)
+                    .addApi(Auth.CREDENTIALS_API)
                     .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
                     .addApi(AppIndex.API).build();
         }
@@ -243,9 +253,131 @@ public class LaunchActivity extends BackToExitActivityBase {
         overridePendingTransition(R.anim.no_transition, R.anim.slide_out_to_bottom);
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (checkPlayServices()) {
+            final CredentialRequest mCredentialRequest = new CredentialRequest.Builder()
+                    .setPasswordLoginSupported(true)
+                    .setAccountTypes(IdentityProviders.GOOGLE, IdentityProviders.TWITTER, IdentityProviders.FACEBOOK, SocialManager.SocialType.github.getIdentifier(), SocialManager.SocialType.vk.getIdentifier())
+                    .build();
+            final Credential credential = new Credential.Builder("user@mail.com")
+                    .setPassword("password")
+                    .build();
+
+            googleApiClient.registerConnectionCallbacks(new GoogleApiClient.ConnectionCallbacks() {
+                @Override
+                public void onConnected(@Nullable Bundle bundle) {
+                    Auth.CredentialsApi.save(googleApiClient, credential).setResultCallback(new ResultCallback<Status>() {
+                        @Override
+                        public void onResult(@NonNull Status status) {
+                            if (status.isSuccess()) {
+                                Timber.d("SAVE: OK");
+                                Toast.makeText(LaunchActivity.this, "Credentials saved", Toast.LENGTH_SHORT).show();
+                            } else {
+                                if (status.hasResolution()) {
+                                    // Try to resolve the save request. This will prompt the user if
+                                    // the credential is new.
+                                    try {
+                                        status.startResolutionForResult(LaunchActivity.this, RC_SAVE);
+                                    } catch (IntentSender.SendIntentException e) {
+                                        // Could not resolve the request
+                                        Toast.makeText(LaunchActivity.this, "Save failed", Toast.LENGTH_SHORT).show();
+                                    }
+                                } else {
+                                    // Request has no resolution
+                                    Toast.makeText(LaunchActivity.this, "Save failed", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        }
+                    });
+
+                    Auth.CredentialsApi.request(googleApiClient, mCredentialRequest).setResultCallback(
+                            new ResultCallback<CredentialRequestResult>() {
+
+                                @Override
+                                public void onResult(CredentialRequestResult credentialRequestResult) {
+                                    if (credentialRequestResult.getStatus().isSuccess()) {
+                                        // See "Handle successful credential requests"
+                                        onCredentialRetrieved(credentialRequestResult.getCredential());
+                                    } else {
+                                        // See "Handle unsuccessful and incomplete credential requests"
+                                        resolveResult(credentialRequestResult.getStatus());
+                                    }
+                                }
+                            });
+                }
+
+                @Override
+                public void onConnectionSuspended(int i) {
+
+                }
+            });
+
+        }
+    }
+
+    int RC_READ = 314;
+    int RC_SAVE = 316;
+
+    private void resolveResult(Status status) {
+        Timber.d(status.toString());
+        if (status.getStatusCode() == CommonStatusCodes.RESOLUTION_REQUIRED) {
+            // Prompt the user to choose a saved credential; do not show the hint
+            // selector.
+            try {
+                status.startResolutionForResult(this, RC_READ);
+            } catch (IntentSender.SendIntentException e) {
+                Timber.e(e, "STATUS: Failed to send resolution.");
+            }
+        } else {
+            Timber.d("STATUS: Failed to send resolution.");
+            // The user must create an account or sign in manually.
+        }
+    }
+
+    private void onCredentialRetrieved(Credential credential) {
+        String accountType = credential.getAccountType();
+        if (accountType == null) {
+            // Sign the user in with information from the Credential.
+            loginManager.login(credential.getId(), credential.getPassword(),
+                    progressHandler,
+                    new ActivityFinisher() {
+                        @Override
+                        public void onFinish() {
+                            finish();
+                        }
+                    }, getCourseFromExtra());
+        } else if (accountType.equals(IdentityProviders.GOOGLE)) {
+            // The user has previously signed in with Google Sign-In. Silently
+            // sign in the user with the same ID.
+            // See https://developers.google.com/identity/sign-in/android/
+            Timber.d(credential.getAccountType());
+        }
+    }
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == RC_SAVE) {
+            if (resultCode == RESULT_OK) {
+                Timber.d("SAVE: OK");
+                Toast.makeText(this, "Credentials saved", Toast.LENGTH_SHORT).show();
+            } else {
+                Timber.d("SAVE: Canceled by user");
+            }
+        }
+
+        if (requestCode == RC_READ) {
+            if (resultCode == RESULT_OK) {
+                Credential credential = data.getParcelableExtra(Credential.EXTRA_KEY);
+                onCredentialRetrieved(credential);
+            } else {
+                Timber.d("Credential Read: NOT OK");
+                Toast.makeText(this, "Credential Read Failed", Toast.LENGTH_SHORT).show();
+            }
+        }
+
+
         if (VKSdk.onActivityResult(requestCode, resultCode, data, new VKCallback<VKAccessToken>() {
             @Override
             public void onResult(VKAccessToken res) {
