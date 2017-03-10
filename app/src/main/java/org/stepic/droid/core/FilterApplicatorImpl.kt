0a1a9130f64b1ddd49d22b0e342aa91e8b70cfb4
Kirlill Makarov <kir-maka@yandex.ru>
2017-03-10 15:11:21 +0300

    fix bug, when courses were discarded due to filters

--- app/src/main/java/org/stepic/droid/core/FilterApplicatorImpl.kt
+++ app/src/main/java/org/stepic/droid/core/FilterApplicatorImpl.kt
@@ -5,7 +5,6 @@ import org.joda.time.DateTimeZone
 import org.stepic.droid.model.Course
 import org.stepic.droid.model.StepikFilter
 import org.stepic.droid.preferences.SharedPreferenceHelper
-import org.stepic.droid.store.operations.DatabaseFacade
 import org.stepic.droid.store.operations.Table
 
 class FilterApplicatorImpl(
@@ -14,46 +13,23 @@ class FilterApplicatorImpl(
 
     override fun getFilteredFromSharedPrefs(sourceCourses: List<Course>, courseType: Table): List<Course> {
         val filters = sharedPreferenceHelper.getFilter(courseType)
-        val now: Long = DateTime.now(DateTimeZone.getDefault()).millis
-
-        val filteredList = sourceCourses.filterNotNull().filter { course ->
-            var beginDate: Long? = null
-            course.begin_date?.let {
-                beginDate = DateTime(it).millis
-            }
-
-            var endDate: Long? = null
-            course.end_date?.let {
-                endDate = DateTime(it).millis
-            }
-
-            var isEnded: Boolean = false
-            course.last_deadline?.let {
-                val lastDeadlineMillis = DateTime(it).millis
-                if (now > lastDeadlineMillis) {
-                    isEnded = true
-                }
-            }
-
-            val isBeginDateInFuture: Boolean = beginDate?.compareTo(now) ?: -1 > 0
-            val isEndDateInFuture: Boolean = endDate?.compareTo(now) ?: -1 > 0
-
-            (filters.contains(StepikFilter.RUSSIAN) && course.language?.equals("ru") ?: false
-                    || filters.contains(StepikFilter.ENGLISH) && course.language?.equals("en") ?: false)
-                    &&
-                    (filters.contains(StepikFilter.UPCOMING) && isBeginDateInFuture
-                            || filters.contains(StepikFilter.ACTIVE) && !isEnded && (beginDate != null && !isBeginDateInFuture || beginDate == null && endDate == null)
-                            || filters.contains(StepikFilter.PAST) && (endDate == null && isEnded || endDate != null && !isEndDateInFuture))
-
-        }
-        return filteredList
-
+        return resolveFiltersForList(sourceCourses, filters)
     }
 
-    override fun getFilteredFromDefault(sourceCourses: List<Course>, courseType: Table): List<Course>? {
-        val now: Long = DateTime.now(DateTimeZone.getDefault()).millis
 
-        val filteredList = sourceCourses.filterNotNull().filter { course ->
+    override fun getFilteredFromDefault(sourceCourses: List<Course>, courseType: Table): List<Course>? {
+        val filters = if (courseType == Table.enrolled) {
+            StepikFilter.values().filter { defaultFilter.getDefaultEnrolled(it) }.toSet()
+        } else {
+            StepikFilter.values().filter { defaultFilter.getDefaultFeatured(it) }.toSet()
+        }
+
+        return resolveFiltersForList(sourceCourses, filters)
+    }
+
+    private fun resolveFiltersForList(sourceCourses: List<Course>, filters: Set<StepikFilter>): List<Course> {
+        //local helper functions:
+        fun resolveFilters(course: Course, now: Long, applyFilters: (Course, endDate: Long?, isAfterBeginOrNotStartable: Boolean, isBeginDateInFuture: Boolean, isEndDateInFuture: Boolean, isEnded: Boolean, filterSet: Set<StepikFilter>) -> Boolean, filterSet: Set<StepikFilter>): Boolean {
             var beginDate: Long? = null
             course.begin_date?.let {
                 beginDate = DateTime(it).millis
@@ -75,23 +51,24 @@ class FilterApplicatorImpl(
             val isBeginDateInFuture: Boolean = beginDate?.compareTo(now) ?: -1 > 0
             val isEndDateInFuture: Boolean = endDate?.compareTo(now) ?: -1 > 0
 
-            //todo refactor: the 1st key is default for enrolled, the 2nd is default for featured
-            if (courseType == Table.enrolled) {
-                (defaultFilter.getDefaultEnrolled(StepikFilter.RUSSIAN) && course.language?.equals("ru") ?: false
-                        || defaultFilter.getDefaultEnrolled(StepikFilter.ENGLISH) && course.language?.equals("en") ?: false)
-                        &&
-                        (defaultFilter.getDefaultEnrolled(StepikFilter.UPCOMING) && isBeginDateInFuture
-                                || defaultFilter.getDefaultEnrolled(StepikFilter.ACTIVE) && !isEnded && (beginDate != null && !isBeginDateInFuture || beginDate == null && endDate == null)
-                                || defaultFilter.getDefaultEnrolled(StepikFilter.PAST) && (endDate == null && isEnded || endDate != null && !isEndDateInFuture))
-            } else {
-                (defaultFilter.getDefaultFeatured(StepikFilter.RUSSIAN) && course.language?.equals("ru") ?: false
-                        || defaultFilter.getDefaultFeatured(StepikFilter.ENGLISH) && course.language?.equals("en") ?: false)
-                        &&
-                        (defaultFilter.getDefaultFeatured(StepikFilter.UPCOMING) && isBeginDateInFuture
-                                || defaultFilter.getDefaultFeatured(StepikFilter.ACTIVE) && !isEnded && (beginDate != null && !isBeginDateInFuture || beginDate == null && endDate == null)
-                                || defaultFilter.getDefaultFeatured(StepikFilter.PAST) && (endDate == null && isEnded || endDate != null && !isEndDateInFuture))
+            val isAfterBeginOrNotStartable = (beginDate != null && !isBeginDateInFuture || beginDate == null)
+            return applyFilters.invoke(course, endDate, isAfterBeginOrNotStartable, isBeginDateInFuture, isEndDateInFuture, isEnded, filterSet)
 
-            }
+        }
+
+        fun applyFiltersForSet(course: Course, endDate: Long?, isAfterBeginOrNotStartable: Boolean, isBeginDateInFuture: Boolean, isEndDateInFuture: Boolean, isEnded: Boolean, filters: Set<StepikFilter>): Boolean {
+            return (filters.contains(StepikFilter.RUSSIAN) && course.language?.equals("ru") ?: false
+                    || filters.contains(StepikFilter.ENGLISH) && course.language?.equals("en") ?: false) || (filters.contains(StepikFilter.ENGLISH) && (filters.contains((StepikFilter.ENGLISH))))
+                    &&
+                    (filters.contains(StepikFilter.UPCOMING) && isBeginDateInFuture
+                            || filters.contains(StepikFilter.ACTIVE) && !isEnded && isAfterBeginOrNotStartable
+                            || filters.contains(StepikFilter.PAST) && (endDate == null && isEnded || endDate != null && !isEndDateInFuture))
+        }
+
+        //logic
+        val now: Long = DateTime.now(DateTimeZone.getDefault()).millis
+        val filteredList = sourceCourses.filterNotNull().filter { course ->
+            resolveFilters(course, now, ::applyFiltersForSet, filters)
         }
         return filteredList
     }
