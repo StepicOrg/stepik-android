85b091c002f77eef81143d4785a3bf83fb77d108
Kirlill Makarov <kir-maka@yandex.ru>
2017-03-13 14:25:32 +0300

    refactor time presentation

--- app/src/main/java/org/stepic/droid/core/presenters/VideoLengthPresenter.kt
+++ app/src/main/java/org/stepic/droid/core/presenters/VideoLengthPresenter.kt
@@ -5,6 +5,7 @@ import org.stepic.droid.core.VideoLengthResolver
 import org.stepic.droid.core.presenters.contracts.VideoLengthView
 import org.stepic.droid.model.Step
 import org.stepic.droid.model.Video
+import org.stepic.droid.util.TimeUtil
 import org.stepic.droid.util.resolvers.VideoResolver
 import java.util.concurrent.ThreadPoolExecutor
 
@@ -12,11 +13,6 @@ class VideoLengthPresenter(private val threadPoolExecutor: ThreadPoolExecutor,
                            private val mainHandler: MainHandler,
                            private val videoResolver: VideoResolver,
                            private val videoLengthResolver: VideoLengthResolver) : PresenterBase<VideoLengthView>() {
-
-    private companion object {
-        val colon = ":"
-    }
-
     var cachedFormat: String? = null
 
     fun fetchLength(video: Video, step: Step) {
@@ -29,36 +25,11 @@ class VideoLengthPresenter(private val threadPoolExecutor: ThreadPoolExecutor,
             val millis = videoLengthResolver.determineLengthInMillis(path) ?: return@execute
             // if not determine millis -> do not form printable string
 
-            val printable = getPrintableFormat(millis)
+            val printable = TimeUtil.getFormattedVideoTime(millis)
             mainHandler.post {
                 cachedFormat = printable
                 view?.onVideoLengthDetermined(printable)
             }
         }
-    }
-
-    private fun getPrintableFormat(millis: Long): String {
-        val durationInSeconds = millis / 1000
-        val hours = durationInSeconds / 3600
-        val minutes = (durationInSeconds - hours * 3600) / 60
-        val seconds = durationInSeconds - (hours * 3600 + minutes * 60)
-
-        val stringBuilder = StringBuilder()
-        stringBuilder.apply {
-            if (hours > 0) {
-                append(hours)
-                append(colon)
-                append(String.format("%02d", minutes)) // 2 digits always
-                append(colon)
-
-            } else {
-                // no hours -> 1 or 2 digits minutes
-                append(minutes)
-                append(colon)
-            }
-
-            append(String.format("%02d", seconds)) // 2 digits always for seconds
-        }
-        return stringBuilder.toString()
     }
 }
