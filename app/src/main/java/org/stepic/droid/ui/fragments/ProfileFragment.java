b9ba94e81bbcab539e618565cd9ab5aba0e8a293
Kirlill Makarov <kir-maka@yandex.ru>
2017-03-24 17:08:56 +0300

    make profile shareable, fix padding for course sharing

--- app/src/main/java/org/stepic/droid/ui/fragments/ProfileFragment.java
+++ app/src/main/java/org/stepic/droid/ui/fragments/ProfileFragment.java
@@ -12,6 +12,9 @@ import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CompoundButton;
@@ -150,6 +153,7 @@ public class ProfileFragment extends FragmentBase implements ProfileView, Notifi
     TextView notificationTimeZoneInfo;
 
     long userId;
+    private UserViewModel localUserViewModel = null;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -157,6 +161,7 @@ public class ProfileFragment extends FragmentBase implements ProfileView, Notifi
         setRetainInstance(true);
         userId = getArguments().getLong(USER_ID_KEY);
         analytic.reportEvent(Analytic.Profile.OPEN_SCREEN_OVERALL);
+        setHasOptionsMenu(true);
     }
 
     @Override
@@ -290,6 +295,9 @@ public class ProfileFragment extends FragmentBase implements ProfileView, Notifi
         loadingView.setVisibility(View.GONE);
         contentRoot.setVisibility(View.VISIBLE);
 
+        localUserViewModel = userViewModel;
+        getActivity().supportInvalidateOptionsMenu();
+
         if (userViewModel.isMyProfile()) {
             shortBioTitle.setText(aboutMeTitle);
             notificationTimePresenter.tryShowNotificationSetting();
@@ -400,4 +408,38 @@ public class ProfileFragment extends FragmentBase implements ProfileView, Notifi
             }
         }
     }
+
+
+    @Override
+    public void onPrepareOptionsMenu(Menu menu) {
+
+
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        if (localUserViewModel != null) {
+            inflater.inflate(R.menu.share_menu, menu);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_item_share:
+                shareProfile();
+                return true;
+        }
+        return false;
+    }
+
+    private void shareProfile() {
+        if (localUserViewModel != null) {
+            Intent intent = shareHelper.getIntentForProfileSharing(localUserViewModel);
+            startActivity(intent);
+        }
+    }
+
+
 }
