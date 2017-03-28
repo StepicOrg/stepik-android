2ccff29b736e333ed53435ff6783dc4c54d43a45
Kirlill Makarov <kir-maka@yandex.ru>
2017-03-28 17:57:48 +0300

    add MockWebServer to dependencies, add one more test case

--- app/src/test/java/org/stepic/droid/core/presenters/ProfilePresenterTest.java
+++ app/src/test/java/org/stepic/droid/core/presenters/ProfilePresenterTest.java
@@ -1,5 +1,6 @@
 package org.stepic.droid.core.presenters;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.InOrder;
@@ -17,11 +18,14 @@ import org.stepic.droid.test_utils.FakeProfileGenerator;
 import org.stepic.droid.util.ProfileExtensionKt;
 import org.stepic.droid.web.Api;
 
+import java.io.IOException;
 import java.util.concurrent.ThreadPoolExecutor;
 
+import okhttp3.mockwebserver.MockWebServer;
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.inOrder;
 import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -29,7 +33,6 @@ import static org.mockito.Mockito.when;
 public class ProfilePresenterTest {
 
     private ProfilePresenter profilePresenter; //we test logic of this object
-
 
     @Mock
     private ThreadPoolExecutor threadPoolExecutor;
@@ -51,17 +54,22 @@ public class ProfilePresenterTest {
 
 
     private Profile preferencesProfileModel;
-
     private UserViewModel fromPreferencesUserViewModel;
 
+    private String responseBodySuccess; //it should be refined
+    private UserViewModel fromApiUserViewModel;
+
+    MockWebServer mockWebServer;
+
     @Before
-    public void beforeEachTest() {
+    public void beforeEachTest() throws IOException {
         MockitoAnnotations.initMocks(this);
 
         ConcurrencyUtilForTest.transformToBlockingMock(threadPoolExecutor);
         ConcurrencyUtilForTest.transformToBlockingMock(mainHandler);
 
         generateLocalModels();//generate some data for using in different tests
+        generateInstructorApiModels();
 
         profilePresenter = new ProfilePresenter(
                 threadPoolExecutor,
@@ -71,12 +79,67 @@ public class ProfilePresenterTest {
                 sharedPreferenceHelper
         );
 
+        mockWebServer = new MockWebServer();
+        mockWebServer.start();
+    }
+
+    @After
+    public void afterEachTEst() throws Exception {
+        mockWebServer.shutdown();
+    }
+
+    private void generateInstructorApiModels() {
+        final String name = "Ivan";
+        String lastName = "Pushkin";
+        long profileId = 1889111;
+        String imageLink = "https://stepik.org/users/23185159/e918ce43f1d870b3acfaf7021ef40b335c7486c6/avatar.svg";
+        boolean isMyProfile = false;
+        String shortBio = "My short bio";
+        String details = " DetailedInfo";
+
+        fromApiUserViewModel = new UserViewModel(ProfileExtensionKt.getFirstAndLastName(preferencesProfileModel), shortBio, details, imageLink, isMyProfile, profileId);
+        responseBodySuccess = "{\n" +
+                "meta: {\n" +
+                "page: 1,\n" +
+                "has_next: false,\n" +
+                "has_previous: false\n" +
+                "},\n" +
+                "users: [\n" +
+                "{\n" +
+                "id: " + profileId + ",\n" +
+                "profile: " + profileId + ",\n" +
+                "is_private: false,\n" +
+                "is_active: true,\n" +
+                "is_guest: true,\n" +
+                "is_organization: false,\n" +
+                "short_bio: \"" + shortBio + "\",\n" +
+                "details: \"" + details + "\",\n" +
+                "first_name: \"" + name + "\",\n" +
+                "last_name: \"" + lastName + "\",\n" +
+                "alias: null,\n" +
+                "avatar: \"" + imageLink + "\",\n" +
+                "cover: null,\n" +
+                "level: 0,\n" +
+                "level_title: \"guest\",\n" +
+                "tag_progresses: [ ],\n" +
+                "knowledge: 0,\n" +
+                "knowledge_rank: null,\n" +
+                "reputation: 0,\n" +
+                "reputation_rank: null,\n" +
+                "join_date: null,\n" +
+                "social_profiles: [ ],\n" +
+                "created_courses_count: 0,\n" +
+                "created_lessons_count: 0,\n" +
+                "issued_certificates_count: 0\n" +
+                "}\n" +
+                "]\n" +
+                "}";
     }
 
     private void generateLocalModels() {
-        final String name = "John";
+        final String name = "Johned";
         String lastName = "Doe";
-        long profileId = 272;
+        long profileId = 27222;
         String imageLink = null;
         boolean isMyProfile = true;
         String shortBio = "it is short bio";
@@ -110,5 +173,29 @@ public class ProfilePresenterTest {
         inOrder.verify(profileView).showLoadingAll();
         inOrder.verify(profileView).showNameImageShortBio(fromPreferencesUserViewModel);
     }
+
+    @Test
+    public void initProfile_stored_nullNoInternet() throws IOException {
+        profilePresenter.attachView(profileView);
+
+        when(sharedPreferenceHelper.getProfile())
+                .thenReturn(null);
+        when(api.getUsers(any(long[].class)))
+                .thenThrow(RuntimeException.class); //throw exception on getting from api instead of executing for simplify testing
+
+        profilePresenter.initProfile();
+
+        verify(sharedPreferenceHelper, times(1)).getProfile();
+        verify(api, times(1)).getUsers(any(long[].class));
+
+        InOrder inOrder = inOrder(profileView);
+        inOrder.verify(profileView).showLoadingAll();
+        inOrder.verify(profileView).onInternetFailed();
+
+        verify(profileView, never()).onProfileNotFound();
+        verify(profileView, never()).showNameImageShortBio(any(UserViewModel.class));
+
+    }
+
 
 }
