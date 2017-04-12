00c6bbf43385491d1eb2b324a384ebbf2e9bbde7
Kirlill Makarov <kir-maka@yandex.ru>
2017-04-12 12:30:48 +0300

    implement downloading interaction presenter

--- app/src/main/java/org/stepic/droid/core/presenters/DownloadingInteractionPresenter.kt
+++ app/src/main/java/org/stepic/droid/core/presenters/DownloadingInteractionPresenter.kt
@@ -1,8 +1,10 @@
 package org.stepic.droid.core.presenters
 
+import android.support.annotation.WorkerThread
 import org.stepic.droid.concurrency.MainHandler
 import org.stepic.droid.core.presenters.contracts.DownloadingInteractionView
 import org.stepic.droid.preferences.UserPreferences
+import org.stepic.droid.util.connectivity.NetworkType
 import org.stepic.droid.util.connectivity.NetworkTypeDeterminer
 import java.util.concurrent.ThreadPoolExecutor
 import java.util.concurrent.atomic.AtomicBoolean
@@ -30,8 +32,22 @@ class DownloadingInteractionPresenter
         }
     }
 
+    @WorkerThread
     private fun checkOnLoadingExclusive(position: Int) {
-
+        val networkType = networkTypeDeterminer.determineNetworkType()
+        with(mainHandler) {
+            when (networkType) {
+                NetworkType.wifi -> post { view?.onLoadingAccepted(position) }
+                NetworkType.none -> post { view?.onShowInternetIsNotAvailableRetry(position) }
+                NetworkType.onlyMobile -> {
+                    if (userPreferences.isNetworkMobileAllowed) {
+                        post { view?.onLoadingAccepted(position) }
+                    } else {
+                        post { view?.onShowPreferenceSuggestion() }
+                    }
+                }
+            }
+        }
     }
 
 }
