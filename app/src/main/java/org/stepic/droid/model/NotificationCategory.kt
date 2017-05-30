d6351d01b2969905a27312551458780184e51b39
Kirlill Makarov <kir-maka@yandex.ru>
2017-05-30 19:00:29 +0300

    fix crash on notifications (proguard)

--- app/src/main/java/org/stepic/droid/model/NotificationCategory.kt
+++ app/src/main/java/org/stepic/droid/model/NotificationCategory.kt
@@ -11,3 +11,12 @@ enum class NotificationCategory(@StringRes val title: Int) {
     teach(R.string.teaching_title),
     default(R.string.other_title)
 }
+
+enum class NotificationCategory(@StringRes val title: Int) {
+    all(R.string.all_title),
+    learn(R.string.learning_title),
+    comments(R.string.comments_title),
+    review(R.string.reviews_title),
+    teach(R.string.teaching_title),
+    default(R.string.other_title)
+}
