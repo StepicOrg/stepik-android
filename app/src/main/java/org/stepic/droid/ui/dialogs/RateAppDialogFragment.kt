cb0f5241cfc27e5917003adb77b01b8a09971578
Kirlill Makarov <kir-maka@yandex.ru>
2017-06-19 22:23:12 +0300

    set colors and text for rate dialog elements

--- app/src/main/java/org/stepic/droid/ui/dialogs/RateAppDialogFragment.kt
+++ app/src/main/java/org/stepic/droid/ui/dialogs/RateAppDialogFragment.kt
@@ -1,13 +1,24 @@
 package org.stepic.droid.ui.dialogs
 
+import android.content.Context
+import android.graphics.Typeface
 import android.os.Bundle
+import android.support.annotation.ColorRes
+import android.support.annotation.StringRes
 import android.support.v4.app.DialogFragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import kotlinx.android.synthetic.main.dialog_rate_app.*
 import org.stepic.droid.R
+import org.stepic.droid.base.App
+import org.stepic.droid.fonts.FontType
+import org.stepic.droid.fonts.FontsProvider
 import timber.log.Timber
+import uk.co.chrisjenx.calligraphy.CalligraphyUtils
+import uk.co.chrisjenx.calligraphy.TypefaceUtils
+import javax.inject.Inject
 
 
 class RateAppDialogFragment : DialogFragment() {
@@ -18,6 +29,18 @@ class RateAppDialogFragment : DialogFragment() {
         }
     }
 
+    @Inject
+    lateinit var fontsProvider: FontsProvider
+    lateinit var boldTypeface: Typeface
+
+    init {
+        App.component().inject(this)
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        boldTypeface = TypefaceUtils.load(context.assets, fontsProvider.provideFontPath(FontType.bold))
+    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val v = inflater.inflate(R.layout.dialog_rate_app, container, false)
@@ -26,6 +49,11 @@ class RateAppDialogFragment : DialogFragment() {
 
     override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        isCancelable = false
+
+        CalligraphyUtils.applyFontToTextView(rateDialogPositive, boldTypeface)
+
         rateDialogRatingBar.setOnRatingBarChangeListener { ratingBar, rating, fromUser ->
             if (!fromUser) {
                 return@setOnRatingBarChangeListener
@@ -34,17 +62,24 @@ class RateAppDialogFragment : DialogFragment() {
             Timber.d("rating $rating")
             if (rating == 0f) {
                 rateDialogButtonsContainer.visibility = View.GONE
-            } else
-                if (rating > 0f && rating < 5) {
-                    rateDialogLater.text = "Later"
-                    rateDialogPositive.text = "Support"
-                    rateDialogButtonsContainer.visibility = View.VISIBLE
-                } else if (rating == 5f) {
-                    rateDialogLater.text = "Later"
-                    rateDialogPositive.text = "Google Play"
-                    rateDialogButtonsContainer.visibility = View.VISIBLE
+            } else {
+                if (rating > 0f && rating <= 4f) {
+                    rateDialogPositive.setTextAndColor(R.string.rate_dialog_support, R.color.rate_dialog_support)
+                } else if (rating > 4f) {
+                    rateDialogPositive.setTextAndColor(R.string.rate_dialog_google_play, R.color.rate_dialog_store)
                 }
+
+                rateDialogLater.setTextAndColor(R.string.rate_dialog_later, R.color.stepic_weak_text)
+                rateDialogButtonsContainer.visibility = View.VISIBLE
+            }
         }
     }
+
+    private fun TextView.setTextAndColor(@StringRes stringRes: Int,
+                                         @ColorRes textColorRes: Int) {
+        this.setText(stringRes)
+        this.setTextColor(org.stepic.droid.util.ColorUtil.getColorArgb(textColorRes, context))
+    }
+
 
 }
