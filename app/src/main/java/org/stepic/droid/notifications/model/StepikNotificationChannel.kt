9e82beeecc6ba8294dc45df14210ce18f0cba387
Kirill Makarov <kir-maka@yandex.ru>
2017-08-24 17:56:31 +0300

    add Notification channels to android O

---
+++ app/src/main/java/org/stepic/droid/notifications/model/StepikNotificationChannel.kt
@@ -0,0 +1,39 @@
+package org.stepic.droid.notifications.model
+
+import android.app.NotificationManager
+import android.os.Build
+import android.support.annotation.StringRes
+import org.stepic.droid.R
+
+private fun getImportanceCompat(): Int =
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            NotificationManager.IMPORTANCE_HIGH
+        } else {
+            //it is -1 because channel should not be used for previous versions
+            -1
+        }
+
+private val commentId = "commentChannel"
+private val reviewId = "reviewChannel"
+private val teachId = "teachChannel"
+private val learnId = "learnChannel"
+private val otherId = "otherChannel"
+private val userId = "userChannel"
+
+enum class StepikNotificationChannel
+(
+        val channelId: String,
+        @StringRes
+        val visibleChannelNameRes: Int,
+        @StringRes
+        val visibleChannelDescriptionRes: Int,
+        val importance: Int = getImportanceCompat()) {
+    //order is important!
+
+    comments(commentId, R.string.comments_channel_name, R.string.comments_channel_description),
+    review(reviewId, R.string.review_channel_name, R.string.review_channel_description),
+    teach(teachId, R.string.teach_channel_name, R.string.teach_channel_description),
+    learn(learnId, R.string.learn_channel_name, R.string.learn_channel_description),
+    other(otherId, R.string.other_channel_name, R.string.other_channel_name),
+    user(userId, R.string.user_channel_name, R.string.user_channel_description)
+}
---
+++ app/src/main/java/org/stepic/droid/util/NotificationChannelInitializer.kt
@@ -0,0 +1,40 @@
+package org.stepic.droid.util
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
+import android.support.annotation.RequiresApi
+import org.stepic.droid.R
+import org.stepic.droid.notifications.model.StepikNotificationChannel
+
+object NotificationChannelInitializer {
+    fun initNotificationChannels(context: Context) {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.O) {
+            //channels were introduced only in O. Before we had used in-app channels
+            return
+        }
+
+        val stepikNotificationChannels = StepikNotificationChannel.values()
+        val androidChannels = ArrayList<NotificationChannel>(stepikNotificationChannels.size)
+        stepikNotificationChannels.forEach {
+            androidChannels.add(initChannel(context, it))
+        }
+
+        val notificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.createNotificationChannels(androidChannels)
+    }
+
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun initChannel(context: Context,
+                            stepikChannel: StepikNotificationChannel): NotificationChannel {
+        val channelName = context.getString(stepikChannel.visibleChannelNameRes)
+        val channel = NotificationChannel(stepikChannel.channelId, channelName, stepikChannel.importance)
+        channel.description = context.getString(stepikChannel.visibleChannelDescriptionRes)
+        channel.enableLights(true)
+        channel.enableVibration(true)
+        channel.lightColor = ColorUtil.getColorArgb(R.color.dots_color, context)
+        return channel
+    }
+}
