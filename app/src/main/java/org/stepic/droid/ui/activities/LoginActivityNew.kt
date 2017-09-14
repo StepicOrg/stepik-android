4457632a3ac8c6bf1e20c6c5e79513954e20ac9d
Ruslan Davletshin <smartmorefeed@gmail.com>
2017-09-14 19:02:39 +0300

    fix API 15 bugs

--- app/src/main/java/org/stepic/droid/ui/activities/LoginActivityNew.kt
+++ app/src/main/java/org/stepic/droid/ui/activities/LoginActivityNew.kt
@@ -1,6 +1,7 @@
 package org.stepic.droid.ui.activities
 
 import android.os.Bundle
+import android.support.v7.app.AppCompatDelegate
 import android.text.Spannable
 import android.text.SpannableString
 import org.stepic.droid.R
@@ -12,6 +13,10 @@ import uk.co.chrisjenx.calligraphy.TypefaceUtils
 
 
 class LoginActivityNew : FragmentActivityBase() {
+
+    init {
+        AppCompatDelegate.setCompatVectorFromResourcesEnabled(true)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
