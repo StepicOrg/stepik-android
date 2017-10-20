ed86012dfad362224a280cc9f015aa5e32414154
Ruslan Davletshin <smartmorefeed@gmail.com>
2017-10-20 12:55:46 +0300

    hide default dialog title bar

--- app/src/main/java/org/stepic/droid/ui/dialogs/LoadingProgressDialog.kt
+++ app/src/main/java/org/stepic/droid/ui/dialogs/LoadingProgressDialog.kt
@@ -4,6 +4,7 @@ import android.app.Dialog
 import android.content.Context
 import android.os.Bundle
 import android.support.annotation.StringRes
+import android.view.Window
 import kotlinx.android.synthetic.main.dialog_progress.*
 
 import org.stepic.droid.R
@@ -16,6 +17,7 @@ class LoadingProgressDialog
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
         setContentView(R.layout.dialog_progress)
         title.setText(titleRes)
         message.setText(R.string.loading_message)
