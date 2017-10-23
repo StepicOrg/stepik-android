560026b42992cb302de1e2c068b1774e1d1acf76
Kirill Makarov <kir-maka@yandex.ru>
2017-10-23 15:05:48 +0300

    fix freezes in code quiz (Parser creation)

--- app/src/main/java/org/stepic/droid/util/RxUtil.kt
+++ app/src/main/java/org/stepic/droid/util/RxUtil.kt
@@ -1,3 +1,9 @@
 package org.stepic.droid.util
 
+import io.reactivex.Observable
+
 enum class RxEmpty { INSTANCE }
+
+@Suppress("UNCHECKED_CAST")
+fun <T> Observable<T?>.filterNotNull(): Observable<T> =
+        this.filter { it != null } as Observable<T>
