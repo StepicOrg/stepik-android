04c5a766c48622064b930ae5a58866e16443241d
Ruslan Davletshin <smartmorefeed@gmail.com>
2017-11-01 16:14:13 +0300

    remove useless dependencies from InvitationPresenter

--- app/src/main/java/org/stepic/droid/core/presenters/InvitationPresenter.kt
+++ app/src/main/java/org/stepic/droid/core/presenters/InvitationPresenter.kt
@@ -1,25 +1,19 @@
 package org.stepic.droid.core.presenters
 
 import org.stepic.droid.analytic.Analytic
-import org.stepic.droid.concurrency.MainHandler
 import org.stepic.droid.core.presenters.contracts.InvitationView
 import org.stepic.droid.di.course.CourseAndSectionsScope
 import org.stepic.droid.model.Course
-import java.util.concurrent.ThreadPoolExecutor
 import javax.inject.Inject
 
 @CourseAndSectionsScope
 class InvitationPresenter
 @Inject constructor(
-        private val threadPoolExecutor: ThreadPoolExecutor,
-        private val mainHandler: MainHandler,
         private val analytic: Analytic) : PresenterBase<InvitationView>() {
 
     fun needShowInvitationDialog(course: Course) {
-        threadPoolExecutor.execute {
-            analytic.reportEvent(Analytic.Interaction.SHOW_MATERIAL_DIALOG_INVITATION)
-            mainHandler.post { view?.onShowInvitationDialog(course) }
-        }
+        analytic.reportEvent(Analytic.Interaction.SHOW_MATERIAL_DIALOG_INVITATION)
+        view?.onShowInvitationDialog(course)
     }
 
 }
