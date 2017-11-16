f89b5177547a745aebdb5ed856de027106892e7e
Ruslan Davletshin <smartmorefeed@gmail.com>
2017-11-16 16:16:45 +0300

    plurals for days & minor layout changes

--- app/src/main/java/org/stepic/droid/ui/fragments/HomeFragment.kt
+++ app/src/main/java/org/stepic/droid/ui/fragments/HomeFragment.kt
@@ -2,6 +2,9 @@ package org.stepic.droid.ui.fragments
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.SpannableStringBuilder
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -12,9 +15,12 @@ import org.stepic.droid.base.App
 import org.stepic.droid.base.FragmentBase
 import org.stepic.droid.core.presenters.HomeStreakPresenter
 import org.stepic.droid.core.presenters.contracts.HomeStreakView
+import org.stepic.droid.fonts.FontType
 import org.stepic.droid.model.CoursesCarouselInfoConstants
 import org.stepic.droid.ui.util.changeVisibility
 import org.stepic.droid.ui.util.initCenteredToolbar
+import uk.co.chrisjenx.calligraphy.CalligraphyTypefaceSpan
+import uk.co.chrisjenx.calligraphy.TypefaceUtils
 import javax.inject.Inject
 
 class HomeFragment : FragmentBase(), HomeStreakView {
@@ -25,6 +31,10 @@ class HomeFragment : FragmentBase(), HomeStreakView {
 
     @Inject
     lateinit var homeStreakPresenter: HomeStreakPresenter
+
+    private val regularFontSpan by lazy {
+        CalligraphyTypefaceSpan(TypefaceUtils.load(context.assets, fontsProvider.provideFontPath(FontType.regular)))
+    }
 
     override fun injectComponent() {
         App.component().inject(this)
@@ -59,8 +69,20 @@ class HomeFragment : FragmentBase(), HomeStreakView {
     }
 
     override fun showStreak(streak: Int) {
-        streakCounter.text = streak.toString()
-        streakText.text = getString(R.string.home_streak_counter_text, streak) // todo: replace with plurals
-        homeStreak.changeVisibility(needShow = streak > 0)
+        val needShow = streak > 0
+        if (needShow) {
+            streakCounter.text = streak.toString()
+
+            val daysPlural = resources.getQuantityString(R.plurals.day_number, streak)
+            val daysSpannable = SpannableString("$streak $daysPlural")
+            daysSpannable.setSpan(regularFontSpan, 0, daysSpannable.length, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+
+            val streakTextSpannableBuilder = SpannableStringBuilder(getString(R.string.home_streak_counter_prefix))
+            streakTextSpannableBuilder.append(daysSpannable)
+            streakTextSpannableBuilder.append(getString(R.string.home_streak_counter_suffix))
+
+            streakText.text = streakTextSpannableBuilder
+        }
+        homeStreak.changeVisibility(needShow)
     }
 }
