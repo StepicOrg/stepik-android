48d90490c9b214595ba72ccee823b6c4832b0837
Kirill Makarov <kir-maka@yandex.ru>
2017-11-22 17:15:03 +0300

    add new remote config property

--- app/src/main/java/org/stepic/droid/configuration/RemoteConfig.kt
+++ app/src/main/java/org/stepic/droid/configuration/RemoteConfig.kt
@@ -3,4 +3,5 @@ package org.stepic.droid.configuration
 object RemoteConfig {
     val continueCourseExperimentEnabledKey = "continue_course_experiment_enabled"
     val minDelayRateDialogSec = "min_delay_rate_dialog_sec"
+    val showStreakDialogAfterLogin = "show_streak_dialog_after_login"
 }
