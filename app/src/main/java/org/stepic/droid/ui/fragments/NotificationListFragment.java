795c9c3e2b80048b14f9779ffe6ddd85df74dab5
Ruslan Davletshin <smartmorefeed@gmail.com>
2017-11-30 19:53:58 +0300

    sync badges on refresh

--- app/src/main/java/org/stepic/droid/ui/fragments/NotificationListFragment.java
+++ app/src/main/java/org/stepic/droid/ui/fragments/NotificationListFragment.java
@@ -17,6 +17,7 @@ import org.stepic.droid.base.App;
 import org.stepic.droid.base.FragmentBase;
 import org.stepic.droid.core.presenters.NotificationListPresenter;
 import org.stepic.droid.core.presenters.contracts.NotificationListView;
+import org.stepic.droid.notifications.badges.NotificationsBadgesManager;
 import org.stepic.droid.notifications.model.Notification;
 import org.stepic.droid.model.NotificationCategory;
 import org.stepic.droid.ui.adapters.NotificationAdapter;
@@ -49,6 +50,9 @@ public class NotificationListFragment extends FragmentBase implements Notificati
 
     @Inject
     NotificationListPresenter notificationListPresenter;
+
+    @Inject
+    NotificationsBadgesManager notificationsBadgesManager;
 
     NotificationCategory notificationCategory;
 
@@ -242,6 +246,7 @@ public class NotificationListFragment extends FragmentBase implements Notificati
     @Override
     public void onRefresh() {
         boolean wasCancelled = notificationListPresenter.init(notificationCategory);
+        notificationsBadgesManager.syncCounter();
         if (wasCancelled) {
             ProgressHelper.dismiss(notificationSwipeRefresh);
         }
