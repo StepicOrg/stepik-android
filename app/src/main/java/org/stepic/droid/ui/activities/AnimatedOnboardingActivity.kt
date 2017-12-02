3ff4180a592b2653a6cdf65d44c0dbc1524bc0dc
Kirill Makarov <kir-maka@yandex.ru>
2017-12-02 12:58:03 +0300

    add circle indicator to dependencies

--- app/src/main/java/org/stepic/droid/ui/activities/AnimatedOnboardingActivity.kt
+++ app/src/main/java/org/stepic/droid/ui/activities/AnimatedOnboardingActivity.kt
@@ -1,18 +1,14 @@
 package org.stepic.droid.ui.activities
 
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_onboarding.*
 import org.stepic.droid.R
 import org.stepic.droid.base.FragmentActivityBase
 
 
 class AnimatedOnboardingActivity : FragmentActivityBase() {
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_onboarding)
-
-        lottieAnimationView.loop(true)
     }
 }
