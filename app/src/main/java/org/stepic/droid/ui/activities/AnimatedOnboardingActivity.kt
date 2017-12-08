0acb7f7663e49419eaf9a1ba12575424e726e536
Kirill Makarov <kir-maka@yandex.ru>
2017-12-08 15:27:11 +0300

    slow down animation for clicking "Next" in onboarding

--- app/src/main/java/org/stepic/droid/ui/activities/AnimatedOnboardingActivity.kt
+++ app/src/main/java/org/stepic/droid/ui/activities/AnimatedOnboardingActivity.kt
@@ -1,13 +1,9 @@
 package org.stepic.droid.ui.activities
 
-import android.animation.Animator
-import android.animation.AnimatorInflater
 import android.os.Bundle
 import android.support.v4.view.ViewPager
-import android.view.View
 import kotlinx.android.synthetic.main.activity_onboarding.*
 import org.stepic.droid.R
-import org.stepic.droid.base.App
 import org.stepic.droid.base.FragmentActivityBase
 import org.stepic.droid.ui.activities.contracts.OnNextClickedListener
 import org.stepic.droid.ui.adapters.OnboardingAdapter
@@ -17,18 +13,13 @@ import timber.log.Timber
 
 
 class AnimatedOnboardingActivity : FragmentActivityBase(), OnNextClickedListener {
-    val setRightOut: Animator = AnimatorInflater.loadAnimator(App.getAppContext(), R.animator.onboarding_animation_out)
-    val setLeftIn: Animator = AnimatorInflater.loadAnimator(App.getAppContext(), R.animator.onboarding_animation_in)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_onboarding)
         initViewPager()
         initClose()
-        val position = 0 //todo: get from view pager
-        setAnimation(position)
     }
-
 
     private fun initViewPager() {
         onboardingViewPager.adapter = OnboardingAdapter(supportFragmentManager)
@@ -43,8 +34,8 @@ class AnimatedOnboardingActivity : FragmentActivityBase(), OnNextClickedListener
             override fun onPageSelected(position: Int) {
                 Timber.d("onPageSelected $position")
                 val tag = "android:switcher:" + R.id.onboardingViewPager + ":" + position
-                val f = supportFragmentManager.findFragmentByTag(tag)
-                (f as OnboardingFragment).startAnimation()
+                val fragment = supportFragmentManager.findFragmentByTag(tag)
+                (fragment as OnboardingFragment).startAnimation()
             }
         })
         onboardingViewPager.setPageTransformer(false, OnboardingPageTransformer())
@@ -56,32 +47,6 @@ class AnimatedOnboardingActivity : FragmentActivityBase(), OnNextClickedListener
         }
     }
 
-    private fun setAnimation(position: Int) {
-
-//        onboardingAnimationView.pauseAnimation()
-//        onboardingAnimationView.speed = 0.4f
-//        onboardingAnimationView.loop(true)
-//
-////        val distance = 8000
-////        val scale = resources.displayMetrics.density
-////        onboardingAnimationView.cameraDistance = distance * scale
-////        onboardingAnimationView2.cameraDistance = distance * scale
-//
-//        val animationFile = when (position) {
-//            0 -> {
-//                FIRST_ANIMATION_PATH
-//            }
-//            1 -> {
-//                SECOND_ANIMATION_PATH
-//            }
-//            2 -> THIRD_ANIMATION_PATH
-//            3 -> FOURTH_ANIMATION_PATH
-//            else -> throw IllegalStateException("Add animation file for onboarding")
-//        }
-//        onboardingAnimationView.setAnimation(animationFile)
-//        onboardingAnimationView.playAnimation()
-    }
-
     private fun onboardingClosed() {
         val position = onboardingViewPager.currentItem
         Timber.d("closed at $position")
@@ -91,7 +56,7 @@ class AnimatedOnboardingActivity : FragmentActivityBase(), OnNextClickedListener
         val current = onboardingViewPager.currentItem
         val next = current + 1
         if (next < onboardingViewPager.adapter.count) {
-            onboardingViewPager.setCurrentItem(next)
+            onboardingViewPager.setCurrentItem(next, true)
         } else {
             onboardingDone()
         }
@@ -103,13 +68,6 @@ class AnimatedOnboardingActivity : FragmentActivityBase(), OnNextClickedListener
 
     override fun applyTransitionPrev() {
         // no-op
-    }
-
-    fun flipCard(outView: View, inView: View) {
-        setRightOut.setTarget(outView)
-        setLeftIn.setTarget(inView)
-        setRightOut.start()
-        setLeftIn.start()
     }
 
 }
---
+++ app/src/main/java/org/stepic/droid/ui/custom/StepikSlowViewPager.kt
@@ -0,0 +1,21 @@
+package android.support.v4.view
+
+import android.content.Context
+import android.util.AttributeSet
+
+class StepikSlowViewPager
+@JvmOverloads
+constructor(context: Context, attrs: AttributeSet? = null) : ViewPager(context, attrs) {
+
+    companion object {
+        private val VELOCITY = 200
+    }
+
+    /**
+     * NOTE: it is easiest way to handle velocity of ViewPager. Be care it is in android.support.v4.view
+     * it is needed for changing velocity on setting current item programatically
+     */
+    internal override fun setCurrentItemInternal(item: Int, smoothScroll: Boolean, always: Boolean) {
+        setCurrentItemInternal(item, smoothScroll, always, VELOCITY)
+    }
+}
