eecb930f6ac36833f57fc7f70e53f48102cce270
Ruslan Davletshin <smartmorefeed@gmail.com>
2018-01-26 17:09:32 +0300

    add morphing animations

---
+++ app/src/main/java/org/stepic/droid/adaptive/ui/custom/morphing/MorphingAnimation.kt
@@ -0,0 +1,94 @@
+package org.stepic.droid.adaptive.ui.custom.morphing
+
+import android.animation.*
+import android.view.animation.Interpolator
+
+class MorphingAnimation(private val view: MorphingView, private val to: MorphingView.MorphParams, val interpolator: Interpolator? = null) {
+    val set = AnimatorSet()
+
+    fun initSet() {
+        val from = view.getMorphParams()
+
+        val fromColor = if (from.backgroundColor == -1) 0x0 else from.backgroundColor
+        val toColor = if (to.backgroundColor == -1) fromColor else to.backgroundColor
+
+        val colorAnimator = ObjectAnimator.ofInt(view.drawableWrapper, "color", fromColor, toColor)
+        colorAnimator.setEvaluator(ArgbEvaluator())
+
+        val morphParamsAnimator = ValueAnimator.ofFloat(0f, 1f)
+        morphParamsAnimator.addUpdateListener {
+            val scale = it.animatedValue as Float
+
+            view.morph(MorphingView.MorphParams(
+                    getScaledValue(scale, from.cornerRadius, to.cornerRadius),
+                    -1,
+
+                    getScaledValue(scale, from.width.toFloat(), to.width.toFloat()).toInt(),
+                    getScaledValue(scale, from.height.toFloat(), to.height.toFloat()).toInt(),
+
+                    getScaledValue(scale, from.marginLeft.toFloat(), to.marginLeft.toFloat()).toInt(),
+                    getScaledValue(scale, from.marginTop.toFloat(), to.marginTop.toFloat()).toInt(),
+                    getScaledValue(scale, from.marginRight.toFloat(), to.marginRight.toFloat()).toInt(),
+                    getScaledValue(scale, from.marginBottom.toFloat(), to.marginBottom.toFloat()).toInt(),
+
+                    if (from.text == to.text || to.text == null) from.text else "",
+                    getScaledValue(scale, from.textSize, to.textSize)
+            ))
+        }
+
+        interpolator?.let { set.interpolator = it }
+        set.playTogether(colorAnimator, morphParamsAnimator)
+
+        set.addListener(object : AnimatorListenerAdapter() {
+            override fun onAnimationEnd(animation: Animator?) {
+                view.morph(to)
+                runnable?.run()
+                next?.start()
+            }
+        })
+    }
+
+    fun setDuration(duration: Long): MorphingAnimation {
+        set.duration = duration
+        return this
+    }
+
+    private fun getScaledValue(scale: Float, from: Float, to: Float, not: Float = -1f) =
+            if (to != not) {
+                from + (to - from) * scale
+            } else {
+                to
+            }
+
+
+    private var next: MorphingAnimation? = null
+    private var runnable: Runnable? = null
+
+    fun withEndAction(run: Runnable): MorphingAnimation {
+        runnable = run
+        return this
+    }
+
+    private var startDelay = 0L
+
+    fun setStartDelay(delay: Long): MorphingAnimation {
+        startDelay = delay
+        return this
+    }
+
+    fun getAnimator(): Animator {
+        initSet()
+        set.startDelay = startDelay
+        return set
+    }
+
+    fun start(): MorphingAnimation {
+        getAnimator().start()
+        return this
+    }
+
+    fun chain(next: MorphingAnimation): MorphingAnimation {
+        this.next = next
+        return this
+    }
+}
---
+++ app/src/main/java/org/stepic/droid/adaptive/ui/custom/morphing/MorphingView.kt
@@ -0,0 +1,98 @@
+package org.stepic.droid.adaptive.ui.custom.morphing
+
+import android.content.Context
+import android.graphics.drawable.GradientDrawable
+import android.os.Build
+import android.util.AttributeSet
+import android.util.TypedValue
+import android.widget.FrameLayout
+import android.widget.TextView
+import org.stepic.droid.util.KotlinUtil.setIfNot
+
+class MorphingView
+@JvmOverloads
+constructor(context: Context, attributeSet: AttributeSet? = null, defStyleAttr: Int = 0, defStyleRes: Int = 0) : FrameLayout(context, attributeSet, defStyleAttr, defStyleRes) {
+
+    val drawableWrapper = GradientDrawableWrapper(GradientDrawable())
+
+    var nestedTextView: TextView? = null
+
+
+    init {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN) {
+            @Suppress("DEPRECATION")
+            setBackgroundDrawable(drawableWrapper.drawable)
+        } else {
+            background = drawableWrapper.drawable
+        }
+    }
+
+    fun setGradientDrawableParams(color: Int, cornerRadius: Float) {
+        drawableWrapper.drawable.shape = GradientDrawable.RECTANGLE
+        drawableWrapper.color = color
+        drawableWrapper.cornerRadius = cornerRadius
+    }
+
+    fun morph(params: MorphParams) {
+
+        setIfNot(drawableWrapper::cornerRadius::set, params.cornerRadius, -1f)
+        setIfNot(drawableWrapper::color::set, params.backgroundColor, -1)
+
+        val lParams = this.layoutParams as MarginLayoutParams
+
+        setIfNot(lParams::width::set, params.width, -1)
+        setIfNot(lParams::height::set, params.height, -1)
+
+        setIfNot(lParams::leftMargin::set, params.marginLeft, -1)
+        setIfNot(lParams::topMargin::set, params.marginTop, -1)
+        setIfNot(lParams::rightMargin::set, params.marginRight, -1)
+        setIfNot(lParams::bottomMargin::set, params.marginBottom, -1)
+
+        layoutParams = lParams
+
+        nestedTextView?.let {
+            if (params.text != null) it.text = params.text
+
+            setIfNot({ it@nestedTextView?.setTextSize(TypedValue.COMPLEX_UNIT_PX, it) }, params.textSize, -1f)
+        }
+    }
+
+    fun getMorphParams(): MorphParams {
+        val lParams = layoutParams as MarginLayoutParams
+
+        return MorphParams(
+                drawableWrapper.cornerRadius,
+                drawableWrapper.color,
+
+                width,
+                height,
+
+                lParams.leftMargin,
+                lParams.topMargin,
+                lParams.rightMargin,
+                lParams.bottomMargin,
+
+                nestedTextView?.text?.toString() ?: "",
+                nestedTextView?.textSize ?: -1f
+        )
+    }
+
+    val initialMorphParams by lazy { getMorphParams() }
+
+    data class MorphParams(
+            val cornerRadius: Float = -1f,
+
+            val backgroundColor: Int = -1,
+
+            val width: Int = -1,
+            val height: Int = -1,
+
+            val marginLeft: Int = -1,
+            val marginTop: Int = -1,
+            val marginRight: Int = -1,
+            val marginBottom: Int = -1,
+
+            val text: String? = null,
+            val textSize: Float = -1f
+    )
+}
