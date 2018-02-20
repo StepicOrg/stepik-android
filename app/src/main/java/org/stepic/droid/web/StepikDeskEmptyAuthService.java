68a56ca6fc579e04d8fb0c10b49ead60553bb92c
Ruslan Davletshin <smartmorefeed@gmail.com>
2018-02-20 14:44:33 +0300

    remove useless imports

--- app/src/main/java/org/stepic/droid/web/StepikDeskEmptyAuthService.java
+++ app/src/main/java/org/stepic/droid/web/StepikDeskEmptyAuthService.java
@@ -4,9 +4,7 @@ import org.stepic.droid.web.model.desk.DeskRequestWrapper;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
-import retrofit2.http.Multipart;
 import retrofit2.http.POST;
-import retrofit2.http.Part;
 
 public interface StepikDeskEmptyAuthService {
 
