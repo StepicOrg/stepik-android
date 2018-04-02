71dbe2cb8a7293637fe40b44c0b1a43c1517a0b3
Ruslan Davletshin <smartmorefeed@gmail.com>
2018-04-02 13:26:18 +0300

    add contracts to pure functions

--- app/src/main/java/org/stepic/droid/ui/custom/LatexSupportableExpandableFrameLayout.kt
+++ app/src/main/java/org/stepic/droid/ui/custom/LatexSupportableExpandableFrameLayout.kt
@@ -2,12 +2,18 @@ package org.stepic.droid.ui.custom
 
 import android.util.AttributeSet
 import android.content.Context
+import org.jetbrains.annotations.Contract
 import org.stepic.droid.R
 
 class LatexSupportableExpandableFrameLayout
 @JvmOverloads
 constructor(context: Context, attrs: AttributeSet? = null): LatexSupportableEnhancedFrameLayout(context, attrs) {
+    @Contract(pure = true)
     override fun getViewRes() = R.layout.latex_supportable_expandable_view
+
+    @Contract(pure = true)
     override fun getTextViewId() = R.id.expandableTextView
+
+    @Contract(pure = true)
     override fun getWebViewId() = R.id.htmlView
 }
