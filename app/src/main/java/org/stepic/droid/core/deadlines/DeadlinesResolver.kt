a9ad620abfddf66bac29e1879d6cd65da4a3e4df
Ruslan Davletshin <smartmorefeed@gmail.com>
2018-05-15 17:32:14 +0300

    chunked requests

--- app/src/main/java/org/stepic/droid/core/deadlines/DeadlinesResolver.kt
+++ app/src/main/java/org/stepic/droid/core/deadlines/DeadlinesResolver.kt
@@ -18,6 +18,8 @@ constructor(
         private val api: Api
 ) {
     companion object {
+        private const val CHUNK_SIZE = 100
+
         private const val DEFAULT_STEP_LENGTH_IN_SECONDS = 60L
         private const val TIME_MULTIPLIER = 1.3
 
@@ -41,12 +43,16 @@ constructor(
             }
 
     private fun getTimeToCompleteForSection(section: Section): Observable<Pair<Long, Long>> =
-            api.getUnitsRx(section.units).flatMap {
+            section.units.asIterable().chunked(CHUNK_SIZE).toObservable().flatMap {
+                api.getUnitsRx(it.toLongArray()).toObservable()
+            }.concatMap {
                 val ids = it.units?.map { it.lesson }?.toLongArray()
-                api.getLessonsRx(ids)
-            }.map {
-                section.id to it.lessons!!.map { if (it.timeToComplete == 0L) it.steps.size * DEFAULT_STEP_LENGTH_IN_SECONDS else it.timeToComplete }.sum()
-            }.toObservable()
+                api.getLessonsRx(ids).flatMapObservable {
+                    it.lessons?.toObservable()
+                }
+            }.reduce(0L) { acc, lesson ->
+                acc + if (lesson.timeToComplete == 0L) lesson.steps.size * DEFAULT_STEP_LENGTH_IN_SECONDS else lesson.timeToComplete
+            }.map { section.id to it }.toObservable()
 
     private fun getDeadlineDate(calendar: Calendar, timeToComplete: Long, hoursPerWeek: Long): Date {
         val timePerWeek = hoursPerWeek * MILLISECONDS_IN_HOUR
