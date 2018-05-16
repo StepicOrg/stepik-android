7c5476d590ee4f3fec58751f229e8b281fe36d4c
Ruslan Davletshin <smartmorefeed@gmail.com>
2018-05-16 20:39:09 +0300

    personal deadlines dao

---
+++ app/src/main/java/org/stepic/droid/storage/dao/PersonalDeadlinesDao.kt
@@ -0,0 +1,34 @@
+package org.stepic.droid.storage.dao
+
+import android.content.ContentValues
+import android.database.Cursor
+import org.stepic.droid.model.deadlines.DeadlineFlatItem
+import org.stepic.droid.storage.operations.DatabaseOperations
+import org.stepic.droid.storage.structure.DbStructureDeadlines
+import java.util.*
+import javax.inject.Inject
+
+class PersonalDeadlinesDao
+@Inject
+constructor(databaseOperations: DatabaseOperations): DaoBase<DeadlineFlatItem>(databaseOperations) {
+    override fun getDbName() = DbStructureDeadlines.DEADLINES
+
+    override fun getDefaultPrimaryColumn(): String = DbStructureDeadlines.Columns.RECORD_ID
+
+    override fun getDefaultPrimaryValue(persistentObject: DeadlineFlatItem): String =
+            persistentObject.recordId.toString()
+
+    override fun getContentValues(persistentObject: DeadlineFlatItem) = ContentValues().apply {
+        put(DbStructureDeadlines.Columns.RECORD_ID, persistentObject.recordId)
+        put(DbStructureDeadlines.Columns.COURSE_ID, persistentObject.courseId)
+        put(DbStructureDeadlines.Columns.SECTION_ID, persistentObject.sectionId)
+        put(DbStructureDeadlines.Columns.DEADLINE, persistentObject.deadline.time)
+    }
+
+    override fun parsePersistentObject(cursor: Cursor) = DeadlineFlatItem(
+            cursor.getLong(cursor.getColumnIndex(DbStructureDeadlines.Columns.RECORD_ID)),
+            cursor.getLong(cursor.getColumnIndex(DbStructureDeadlines.Columns.COURSE_ID)),
+            cursor.getLong(cursor.getColumnIndex(DbStructureDeadlines.Columns.SECTION_ID)),
+            Date(cursor.getLong(cursor.getColumnIndex(DbStructureDeadlines.Columns.DEADLINE)))
+    )
+}
---
+++ app/src/main/java/org/stepic/droid/storage/structure/DbStructureDeadlines.kt
@@ -0,0 +1,12 @@
+package org.stepic.droid.storage.structure
+
+object DbStructureDeadlines {
+    const val DEADLINES = "personal_deadlines"
+
+    object Columns {
+        const val RECORD_ID = "record_id"
+        const val COURSE_ID = "course_id"
+        const val SECTION_ID = "section_id"
+        const val DEADLINE = "deadline"
+    }
+}
