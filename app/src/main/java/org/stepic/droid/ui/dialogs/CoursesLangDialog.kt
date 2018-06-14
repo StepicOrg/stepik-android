8bd6572527d4294d85a4e9d5814f8666f8c9183d
Ruslan Davletshin <smartmorefeed@gmail.com>
2018-06-14 10:41:02 +0300

    add course lang picker to settings

---
+++ app/src/main/java/org/stepic/droid/ui/dialogs/CoursesLangDialog.kt
@@ -0,0 +1,11 @@
+package org.stepic.droid.ui.dialogs
+
+import android.support.v4.app.DialogFragment
+
+class CoursesLangDialog: DialogFragment() {
+    companion object {
+        fun newInstance() = CoursesLangDialog()
+    }
+
+
+}
