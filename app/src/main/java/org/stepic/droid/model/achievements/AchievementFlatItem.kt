121ccd9a167dc59a44e7ca5bf408d2d195e7cc59
Ruslan Davletshin <smartmorefeed@gmail.com>
2018-06-22 16:44:39 +0300

    parcelable implementation for achievement flat item

--- app/src/main/java/org/stepic/droid/model/achievements/AchievementFlatItem.kt
+++ app/src/main/java/org/stepic/droid/model/achievements/AchievementFlatItem.kt
@@ -1,22 +1,60 @@
 package org.stepic.droid.model.achievements
 
+import android.os.Parcel
+import android.os.Parcelable
+
 class AchievementFlatItem(
-        currentLevelAchievement: Achievement?,
-        nextLevelAchievement: Achievement,
-        nextLevelAchievementProgress: AchievementProgress,
+        val iconId: Long?,
+
+        val isLocked: Boolean,
+        val kind: String,
+        val currentScore: Int,
+        val targetScore: Int,
 
         val currentLevel: Int,
         val maxLevel: Int
-) {
-    val iconId: Long? = null // currentLevelAchievement?.iconId
+) : Parcelable {
+    constructor(parcel: Parcel): this(
+            parcel.readValue(Long::class.java.classLoader) as? Long,
+            parcel.readByte() != 0.toByte(),
+            parcel.readString(),
+            parcel.readInt(),
+            parcel.readInt(),
+            parcel.readInt(),
+            parcel.readInt()
+    )
 
-    val isLocked = currentLevelAchievement == null
+    constructor(
+            currentLevelAchievement: Achievement?,
+            nextLevelAchievement: Achievement,
+            nextLevelAchievementProgress: AchievementProgress,
 
-    val kind = nextLevelAchievement.kind
-    val currentScore = nextLevelAchievementProgress.score
-    val targetScore = nextLevelAchievement.targetScore
+            currentLevel: Int,
+            maxLevel: Int
+    ): this(
+            null,
+            currentLevelAchievement == null,
+            nextLevelAchievement.kind,
+            nextLevelAchievementProgress.score,
+            nextLevelAchievement.targetScore,
+            currentLevel,
+            maxLevel
+    )
 
-    override fun toString(): String {
-        return "AchievementFlatItem(currentLevel=$currentLevel, maxLevel=$maxLevel, iconId=$iconId, kind='$kind', currentScore=$currentScore, targetScore=$targetScore)"
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeValue(iconId)
+        parcel.writeByte(if (isLocked) 1 else 0)
+        parcel.writeString(kind)
+        parcel.writeInt(currentScore)
+        parcel.writeInt(targetScore)
+        parcel.writeInt(currentLevel)
+        parcel.writeInt(maxLevel)
+    }
+
+    override fun describeContents() = 0
+
+    companion object CREATOR : Parcelable.Creator<AchievementFlatItem> {
+        override fun createFromParcel(parcel: Parcel) = AchievementFlatItem(parcel)
+        override fun newArray(size: Int) = arrayOfNulls<AchievementFlatItem?>(size)
     }
 }
