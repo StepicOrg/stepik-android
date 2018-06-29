3912bf7a5db9f4c4649eefc2085377bd43bce6f8
Ruslan Davletshin <smartmorefeed@gmail.com>
2018-06-29 16:20:08 +0300

    clean up string resources names

--- app/src/main/java/org/stepic/droid/util/LoginUtil.kt
+++ app/src/main/java/org/stepic/droid/util/LoginUtil.kt
@@ -8,11 +8,11 @@ fun Context.getMessageFor(type: LoginFailType): String {
     with(resources) {
         return when (type) {
             LoginFailType.CONNECTION_PROBLEM -> getString(R.string.connectionProblems)
-            LoginFailType.EMAIL_ALREADY_USED -> getString(R.string.email_already_used)
-            LoginFailType.EMAIL_NOT_PROVIDED_BY_SOCIAL -> getString(R.string.email_not_provided_by_social)
+            LoginFailType.EMAIL_ALREADY_USED -> getString(R.string.auth_error_email_already_used)
+            LoginFailType.EMAIL_NOT_PROVIDED_BY_SOCIAL -> getString(R.string.auth_error_email_not_provided_by_social)
             LoginFailType.TOO_MANY_ATTEMPTS -> getString(R.string.too_many_attempts)
-            LoginFailType.EMAIL_PASSWORD_INVALID -> getString(R.string.failLogin)
-            LoginFailType.UNKNOWN_ERROR -> getString(R.string.unknown_auth_error)
+            LoginFailType.EMAIL_PASSWORD_INVALID -> getString(R.string.auth_error_invalid_credentials)
+            LoginFailType.UNKNOWN_ERROR -> getString(R.string.auth_error_unknown)
         }
     }
 }
