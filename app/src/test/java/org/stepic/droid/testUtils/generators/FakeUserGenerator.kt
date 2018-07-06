21c1d3167201088841eabd827acd45e60e43be3e
Ruslan Davletshin <smartmorefeed@gmail.com>
2018-07-06 16:09:38 +0300

    fix tests

--- app/src/test/java/org/stepic/droid/testUtils/generators/FakeUserGenerator.kt
+++ app/src/test/java/org/stepic/droid/testUtils/generators/FakeUserGenerator.kt
@@ -17,6 +17,7 @@ object FakeUserGenerator {
                 first_name = firstName,
                 last_name = lastName,
                 short_bio = shortBio,
-                details = details)
+                details = details,
+                joinDate = null)
     }
 }
