b53b0a45a73392fb6723f60f51e98811c2846efc
Ruslan Davletshin <smartmorefeed@gmail.com>
2018-07-06 12:46:30 +0300

    fix date adapter

--- app/src/main/java/org/stepic/droid/jsonHelpers/adapters/UTCDateAdapter.kt
+++ app/src/main/java/org/stepic/droid/jsonHelpers/adapters/UTCDateAdapter.kt
@@ -5,24 +5,29 @@ import java.lang.reflect.Type
 import java.text.SimpleDateFormat
 import java.util.*
 import com.google.gson.JsonParseException
+import java.text.DateFormat
 import java.text.ParseException
 
 
 class UTCDateAdapter: JsonSerializer<Date>, JsonDeserializer<Date> {
     companion object {
         private const val UTC_ISO_FORMAT = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"
+        private const val UTC_ISO_FORMAT_SIMPLE = "yyyy-MM-dd'T'HH:mm:ss"
+
+        private fun createDateFormat(pattern: String) = SimpleDateFormat(pattern, Locale.getDefault()).apply {
+            timeZone = TimeZone.getTimeZone("UTC")
+        }
     }
 
-    private val dateFormat = SimpleDateFormat(UTC_ISO_FORMAT, Locale.getDefault()).also {
-        it.timeZone = TimeZone.getTimeZone("UTC")
-    }
+    private val serializeDateFormat = createDateFormat(UTC_ISO_FORMAT)
+    private val deserializeDateFormat = createDateFormat(UTC_ISO_FORMAT_SIMPLE)
 
     override fun serialize(date: Date, typeOfSrc: Type, context: JsonSerializationContext?): JsonElement =
-            JsonPrimitive(dateFormat.format(date))
+            JsonPrimitive(serializeDateFormat.format(date))
 
 
     override fun deserialize(jsonElement: JsonElement, typeOfT: Type, context: JsonDeserializationContext): Date = try {
-        dateFormat.parse(jsonElement.asString)
+        deserializeDateFormat.parse(jsonElement.asString.take(UTC_ISO_FORMAT_SIMPLE.length))
     } catch (e: ParseException) {
         throw JsonParseException(e)
     }
