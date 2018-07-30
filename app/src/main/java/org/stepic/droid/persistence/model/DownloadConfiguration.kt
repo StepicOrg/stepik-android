d4108014b07ba2b2ccdc1025b8007093fcf107b9
Ruslan Davletshin <smartmorefeed@gmail.com>
2018-07-30 18:21:50 +0300

    add download configuration

--- app/src/main/java/org/stepic/droid/persistence/model/DownloadConfiguration.kt
+++ app/src/main/java/org/stepic/droid/persistence/model/DownloadConfiguration.kt
@@ -1,3 +1,17 @@
 package org.stepic.droid.persistence.model
 
-class DownloadConfiguration
+import android.app.DownloadManager
+import java.io.File
+import java.util.*
+
+class DownloadConfiguration(
+        val targetStorage: File,
+        val allowedNetworkTypes: EnumSet<NetworkType>,
+
+        val videoQuality: String
+) {
+    enum class NetworkType(val systemNetworkType: Int) {
+        MOBILE(DownloadManager.Request.NETWORK_MOBILE),
+        WIFI(DownloadManager.Request.NETWORK_WIFI)
+    }
+}
