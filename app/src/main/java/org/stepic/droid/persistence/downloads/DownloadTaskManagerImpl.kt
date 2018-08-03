ef47ba0c80bf2f654048464f2148b5eb0a116fb5
Ruslan Davletshin <smartmorefeed@gmail.com>
2018-08-03 15:59:42 +0300

    add task in one completable

--- app/src/main/java/org/stepic/droid/persistence/downloads/DownloadTaskManagerImpl.kt
+++ app/src/main/java/org/stepic/droid/persistence/downloads/DownloadTaskManagerImpl.kt
@@ -4,7 +4,6 @@ import android.app.DownloadManager
 import android.net.Uri
 import io.reactivex.Completable
 import io.reactivex.Observer
-import io.reactivex.Single
 import org.stepic.droid.persistence.di.PersistenceScope
 import org.stepic.droid.persistence.model.DownloadConfiguration
 import org.stepic.droid.persistence.model.DownloadTask
@@ -20,22 +19,22 @@ constructor(
         private val updatesObserver: Observer<PersistentItem>,
         private val persistentItemDao: PersistentItemDao
 ): DownloadTaskManager {
-    override fun addTask(task: DownloadTask, configuration: DownloadConfiguration): Completable = Single.fromCallable {
+    override fun addTask(task: DownloadTask, configuration: DownloadConfiguration): Completable = Completable.fromCallable {
         val request = DownloadManager.Request(Uri.parse(task.originalPath))
                 .setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE)
                 .setAllowedNetworkTypes(configuration.allowedNetworkTypes.map(DownloadConfiguration.NetworkType::systemNetworkType).reduce(Int::or))
                 .setTitle("Downloading") // todo add title
 
-        downloadManager.enqueue(request)
-    }.doOnSuccess {
+        val downloadId = downloadManager.enqueue(request)
+
         val persistentItem = PersistentItem(
-            task = task,
-            downloadId = it,
-            status = PersistentItem.Status.IN_PROGRESS
+                task = task,
+                downloadId = downloadId,
+                status = PersistentItem.Status.IN_PROGRESS
         )
         persistentItemDao.insertOrReplace(persistentItem)
         updatesObserver.onNext(persistentItem)
-    }.toCompletable()
+    }
 
     override fun removeTask(downloadId: Long): Completable {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
