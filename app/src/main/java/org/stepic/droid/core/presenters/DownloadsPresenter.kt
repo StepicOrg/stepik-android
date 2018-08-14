51aa4f8cc6f49f4154c203c7fc8f55aa45ac8bdc
Ruslan Davletshin <smartmorefeed@gmail.com>
2018-08-14 16:32:17 +0300

    downloads presenter

--- app/src/main/java/org/stepic/droid/core/presenters/DownloadsPresenter.kt
+++ app/src/main/java/org/stepic/droid/core/presenters/DownloadsPresenter.kt
@@ -1,4 +1,53 @@
 package org.stepic.droid.core.presenters
 
-class DownloadsPresenter {
+import io.reactivex.Scheduler
+import io.reactivex.disposables.Disposable
+import io.reactivex.rxkotlin.subscribeBy
+import org.stepic.droid.core.presenters.contracts.DownloadsView
+import org.stepic.droid.di.qualifiers.BackgroundScheduler
+import org.stepic.droid.di.qualifiers.MainScheduler
+import org.stepic.droid.persistence.model.DownloadProgress
+import org.stepic.droid.persistence.repository.DownloadsRepository
+import javax.inject.Inject
+
+class DownloadsPresenter
+@Inject
+constructor(
+        private val downloadsRepository: DownloadsRepository,
+
+        @BackgroundScheduler
+        private val backgroundScheduler: Scheduler,
+        @MainScheduler
+        private val mainScheduler: Scheduler
+): PresenterBase<DownloadsView>() {
+    private var disposable: Disposable? = null
+
+    private fun subscribeForDownloads() {
+        disposable?.dispose()
+        disposable = downloadsRepository.getDownloads()
+                .observeOn(backgroundScheduler)
+                .subscribeOn(mainScheduler)
+                .subscribeBy(onError = { subscribeForDownloads() }) {
+                    when(it.status) {
+                        is DownloadProgress.Status.Cached ->
+                            view?.addCompletedDonwload(it)
+
+                        is DownloadProgress.Status.NotCached ->
+                            view?.removeDownload(it)
+
+                        else ->
+                            view?.addActiveDownload(it)
+                    }
+                }
+    }
+
+    override fun attachView(view: DownloadsView) {
+        super.attachView(view)
+        subscribeForDownloads()
+    }
+
+    override fun detachView(view: DownloadsView) {
+        disposable?.dispose()
+        super.detachView(view)
+    }
 }
--- app/src/main/java/org/stepic/droid/core/presenters/contracts/DownloadsView.kt
+++ app/src/main/java/org/stepic/droid/core/presenters/contracts/DownloadsView.kt
@@ -1,4 +1,9 @@
 package org.stepic.droid.core.presenters.contracts
 
-class DownloadsView {
+import org.stepic.droid.persistence.model.DownloadItem
+
+interface DownloadsView {
+    fun addActiveDownload(downloadItem: DownloadItem)
+    fun addCompletedDonwload(downloadItem: DownloadItem)
+    fun removeDownload(downloadItem: DownloadItem)
 }
