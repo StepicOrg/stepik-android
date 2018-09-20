00a123962780865fcbbfef67e3d37bcba55e4cca
Ruslan Davletshin <smartmorefeed@gmail.com>
2018-09-20 19:43:43 +0300

    migrate to new dismisslistener

--- app/src/main/java/org/stepic/droid/features/stories/ui/activity/StoriesActivity.kt
+++ app/src/main/java/org/stepic/droid/features/stories/ui/activity/StoriesActivity.kt
@@ -5,6 +5,7 @@ import org.stepic.droid.R
 import org.stepic.droid.analytic.AmplitudeAnalytic
 import org.stepic.droid.base.FragmentActivityBase
 import org.stepic.droid.features.stories.ui.delegate.StoriesActivityDelegate
+import ru.nobird.android.stories.ui.custom.DismissableLayout
 
 class StoriesActivity : FragmentActivityBase() {
     private lateinit var storiesDelegate: StoriesActivityDelegate
@@ -15,18 +16,17 @@ class StoriesActivity : FragmentActivityBase() {
         storiesDelegate = StoriesActivityDelegate(this, analytic)
         storiesDelegate.onCreate(savedInstanceState)
 
-        val onDismiss = storiesDelegate.dismissableLayout.onDismiss
-
-        storiesDelegate.dismissableLayout.onDismiss = {
-            val story = storiesDelegate.getCurrentStory()
-            if (story != null) {
-                analytic.reportAmplitudeEvent(AmplitudeAnalytic.Stories.STORY_CLOSED, mapOf(
-                        AmplitudeAnalytic.Stories.Values.STORY_ID to story.id,
-                        AmplitudeAnalytic.Stories.Values.CLOSE_TYPE to AmplitudeAnalytic.Stories.Values.CloseTypes.SWIPE
-                ))
+        storiesDelegate.dismissableLayout.addDismissListener(object : DismissableLayout.DismissListener {
+            override fun onDismiss() {
+                val story = storiesDelegate.getCurrentStory()
+                if (story != null) {
+                    analytic.reportAmplitudeEvent(AmplitudeAnalytic.Stories.STORY_CLOSED, mapOf(
+                            AmplitudeAnalytic.Stories.Values.STORY_ID to story.id,
+                            AmplitudeAnalytic.Stories.Values.CLOSE_TYPE to AmplitudeAnalytic.Stories.Values.CloseTypes.SWIPE
+                    ))
+                }
             }
-            onDismiss?.invoke()
-        }
+        })
     }
 
     override fun onPause() {
