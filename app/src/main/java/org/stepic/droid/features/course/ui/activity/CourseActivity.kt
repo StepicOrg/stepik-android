8066ec825b7e647001ff314a344d606de0d908e9
Ruslan Davletshin <smartmorefeed@gmail.com>
2018-09-24 19:12:35 +0300

    Collapsing toolbar draft

--- app/src/main/java/org/stepic/droid/features/course/ui/activity/CourseActivity.kt
+++ app/src/main/java/org/stepic/droid/features/course/ui/activity/CourseActivity.kt
@@ -1,11 +1,18 @@
 package org.stepic.droid.features.course.ui.activity
 
 import android.os.Bundle
+import android.util.Log
+import com.bumptech.glide.Glide
+import jp.wasabeef.glide.transformations.BlurTransformation
 import kotlinx.android.synthetic.main.activity_course.*
 import org.stepic.droid.R
 import org.stepic.droid.base.FragmentActivityBase
+import org.stepic.droid.util.AppConstants
+import org.stepik.android.model.Course
 
 class CourseActivity : FragmentActivityBase() {
+
+    private val course by lazy { intent.getParcelableExtra<Course>(AppConstants.KEY_COURSE_BUNDLE) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,6 +26,11 @@ class CourseActivity : FragmentActivityBase() {
             setDisplayShowTitleEnabled(false)
             setDisplayHomeAsUpEnabled(true)
         }
-        
+
+        Glide.with(this)
+                .load(config.baseUrl + course.cover)
+                .bitmapTransform(BlurTransformation(this, 15))
+                .centerCrop()
+                .into(courseCover)
     }
 }
