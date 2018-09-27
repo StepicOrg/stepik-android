3b0b16f996e3d7dd3be447d85f9d14369ff8fc62
Ruslan Davletshin <smartmorefeed@gmail.com>
2018-09-27 21:59:25 +0300

    add course info text block

--- app/src/main/java/org/stepic/droid/features/course/ui/adapter/course_info/CourseInfoBlockAdapter.kt
+++ app/src/main/java/org/stepic/droid/features/course/ui/adapter/course_info/CourseInfoBlockAdapter.kt
@@ -1,14 +1,18 @@
 package org.stepic.droid.features.course.ui.adapter.course_info
 
-import android.support.annotation.CallSuper
 import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import kotlinx.android.synthetic.main.view_course_info_block.view.*
+import kotlinx.android.synthetic.main.view_course_info_text_block.view.*
+import org.stepic.droid.R
 import org.stepic.droid.features.course.ui.model.course_info.CourseInfoBlock
+import org.stepic.droid.features.course.ui.model.course_info.CourseInfoTextBlock
 import org.stepic.droid.features.course.ui.model.course_info.CourseInfoType
+import java.lang.IllegalStateException
 
 class CourseInfoBlockAdapter : RecyclerView.Adapter<CourseInfoBlockAdapter.CourseInfoViewHolder<CourseInfoBlock>>() {
     companion object {
@@ -35,20 +39,49 @@ class CourseInfoBlockAdapter : RecyclerView.Adapter<CourseInfoBlockAdapter.Cours
                     VIEW_TYPE_TEXT_BLOCK
             }
 
-    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): CourseInfoViewHolder<CourseInfoBlock> = TODO()
+    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): CourseInfoViewHolder<CourseInfoBlock> =
+            when(viewType) {
+                VIEW_TYPE_INSTRUCTORS_BLOCK -> TODO()
+                VIEW_TYPE_TEXT_BLOCK ->
+                    CourseInfoTextBlockViewHolder(LayoutInflater.from(parent?.context).inflate(R.layout.view_course_info_text_block, parent, false))
+                            as CourseInfoViewHolder<CourseInfoBlock>
+
+                else -> throw IllegalStateException("Unknown viewType = $viewType")
+            }
 
 
     override fun getItemCount() =
             blocks.size
 
-    override fun onBindViewHolder(holder: CourseInfoViewHolder<CourseInfoBlock>, position: Int) {}
+    override fun onBindViewHolder(holder: CourseInfoViewHolder<CourseInfoBlock>, position: Int) {
+        val block = blocks[position]
+        when(getItemViewType(position)) {
+            VIEW_TYPE_TEXT_BLOCK -> {
+                holder as CourseInfoTextBlockViewHolder
+                block as CourseInfoTextBlock
+                holder.onBind(block)
+            }
 
+            VIEW_TYPE_INSTRUCTORS_BLOCK -> {
+
+            }
+        }
+    }
+
+
+    class CourseInfoTextBlockViewHolder(root: View) : CourseInfoViewHolder<CourseInfoTextBlock>(root) {
+        private val blockMessage = root.blockMessage
+
+        override fun onBind(data: CourseInfoTextBlock) {
+            super.onBind(data)
+            blockMessage.text = data.message
+        }
+    }
 
     abstract class CourseInfoViewHolder<T : CourseInfoBlock>(root: View) : RecyclerView.ViewHolder(root) {
         protected val blockIcon: ImageView = root.blockIcon
         protected val blockTitle: TextView = root.blockTitle
 
-        @CallSuper
         open fun onBind(data: T) {
             blockIcon.setImageResource(data.type.icon)
             blockTitle.setText(data.type.title)
--- app/src/main/java/org/stepic/droid/features/course/ui/fragment/CourseInfoFragment.kt
+++ app/src/main/java/org/stepic/droid/features/course/ui/fragment/CourseInfoFragment.kt
@@ -2,25 +2,54 @@ package org.stepic.droid.features.course.ui.fragment
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.LinearLayoutManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import kotlinx.android.synthetic.main.error_course_not_found.*
 import kotlinx.android.synthetic.main.error_no_connection_with_button.*
+import kotlinx.android.synthetic.main.fragment_course_info.*
 import org.stepic.droid.R
+import org.stepic.droid.features.course.ui.adapter.course_info.CourseInfoBlockAdapter
+import org.stepic.droid.features.course.ui.model.course_info.CourseInfoTextBlock
+import org.stepic.droid.features.course.ui.model.course_info.CourseInfoType
 import org.stepic.droid.ui.util.changeVisibility
+import org.stepic.droid.util.argument
+import org.stepik.android.model.Course
 
 class CourseInfoFragment : Fragment() {
     companion object {
-        fun newInstance(): Fragment =
-                CourseInfoFragment()
+        fun newInstance(course: Course): Fragment =
+                CourseInfoFragment().apply {
+                    this.course = course
+                }
     }
+
+    private var course by argument<Course>()
+
+    private val adapter = CourseInfoBlockAdapter()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View =
             inflater.inflate(R.layout.fragment_course_info, container, false)
 
     override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
-        course_not_found.changeVisibility(true)
+        course_not_found.changeVisibility(false)
         error.changeVisibility(false)
+
+        courseInfoRecycler.layoutManager = LinearLayoutManager(context)
+        courseInfoRecycler.adapter = adapter
+
+        setCourseInfo(course)
+    }
+
+    fun setCourseInfo(course: Course) {
+        adapter.setData(listOf(
+                CourseInfoTextBlock(CourseInfoType.ABOUT, course.description ?: ""),
+                CourseInfoTextBlock(CourseInfoType.REQUIREMENTS, course.requirements ?: ""),
+                CourseInfoTextBlock(CourseInfoType.TARGET_AUDIENCE, course.targetAudience ?: ""),
+                CourseInfoTextBlock(CourseInfoType.TIME_TO_COMPLETE, course.timeToComplete.toString() ?: ""),
+                CourseInfoTextBlock(CourseInfoType.LANGUAGE, course.language ?: ""),
+                CourseInfoTextBlock(CourseInfoType.CERTIFICATE, course.certificate ?: "")
+        ))
     }
 }
