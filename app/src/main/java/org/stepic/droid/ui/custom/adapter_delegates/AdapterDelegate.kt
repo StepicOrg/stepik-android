88cbe79ee5ae27fb082dae940b4e32c8be813440
Ruslan Davletshin <smartmorefeed@gmail.com>
2018-10-04 23:49:58 +0300

    fix arg name

--- app/src/main/java/org/stepic/droid/ui/custom/adapter_delegates/AdapterDelegate.kt
+++ app/src/main/java/org/stepic/droid/ui/custom/adapter_delegates/AdapterDelegate.kt
@@ -1,5 +1,8 @@
 package org.stepic.droid.ui.custom.adapter_delegates
 
+import android.support.annotation.LayoutRes
+import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 
 abstract class AdapterDelegate<D, VH : DelegateViewHolder<D>>(
@@ -7,6 +10,9 @@ abstract class AdapterDelegate<D, VH : DelegateViewHolder<D>>(
 ) {
     protected fun getItemAtPosition(position: Int): D =
             adapter.getItemAtPosition(position)
+
+    protected fun createView(parent: ViewGroup, @LayoutRes layoutId: Int): View =
+            LayoutInflater.from(parent.context).inflate(layoutId, parent, false)
 
     abstract fun onCreateViewHolder(parent: ViewGroup): VH
     abstract fun isForViewType(position: Int): Boolean
--- app/src/main/java/org/stepic/droid/ui/custom/adapter_delegates/RecyclerViewDelegateAdapter.kt
+++ app/src/main/java/org/stepic/droid/ui/custom/adapter_delegates/RecyclerViewDelegateAdapter.kt
@@ -12,11 +12,14 @@ abstract class RecyclerViewDelegateAdapter<D, VH : DelegateViewHolder<D>> : Recy
     fun removeDelegate(delegate: AdapterDelegate<D, VH>) =
             delegates.remove(delegate)
 
-    abstract fun getItemAtPosition(position: Int): D
+    override fun getItemViewType(position: Int): Int =
+            delegates.indexOfFirst { it.isForViewType(position) }
 
-    override fun onCreateViewHolder(parent: ViewGroup, position: Int): VH =
-            delegates.first { it.isForViewType(position) }.onCreateViewHolder(parent)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VH =
+            delegates[viewType].onCreateViewHolder(parent)
 
     override fun onBindViewHolder(holder: VH, position: Int) =
-        holder.bind(getItemAtPosition(position))
+            holder.bind(getItemAtPosition(position))
+
+    abstract fun getItemAtPosition(position: Int): D
 }
