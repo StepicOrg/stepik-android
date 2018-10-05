cb6d518726e7ac9c07d0aa72baf3d8e23e067146
Ruslan Davletshin <smartmorefeed@gmail.com>
2018-10-05 17:43:12 +0300

    control bar draft

--- app/src/main/java/org/stepic/droid/ui/custom/control_bar/ControlBarView.kt
+++ app/src/main/java/org/stepic/droid/ui/custom/control_bar/ControlBarView.kt
@@ -1,4 +1,43 @@
 package org.stepic.droid.ui.custom.control_bar
 
-class ControlBarView {
+import android.content.Context
+import android.support.annotation.AttrRes
+import android.support.annotation.LayoutRes
+import android.support.annotation.MenuRes
+import android.util.AttributeSet
+import android.view.Menu
+import android.view.MenuInflater
+import android.widget.FrameLayout
+import android.widget.PopupMenu
+import org.stepic.droid.R
+
+class ControlBarView
+@JvmOverloads
+constructor(
+        context: Context,
+        attrs: AttributeSet? = null,
+        @AttrRes defStyleAttr: Int = 0
+) : FrameLayout(context, attrs, defStyleAttr) {
+    private val menu: Menu =
+            PopupMenu(context, null).menu
+
+    @LayoutRes
+    private val itemLayoutRes: Int
+
+    init {
+        val typedArray = context.obtainStyledAttributes(attrs, R.styleable.ControlBarView)
+
+        try {
+            val menuInflater = MenuInflater(context)
+            @MenuRes
+            val menuRes = typedArray.getResourceId(R.styleable.ControlBarView_menu, 0)
+            menuInflater.inflate(menuRes, menu)
+
+            itemLayoutRes = typedArray.getResourceId(R.styleable.ControlBarView_item_layout, 0)
+        } finally {
+            typedArray.recycle()
+        }
+    }
+
+
 }
