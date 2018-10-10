856307b3f4f72cb7878d93d2efa40f3145343805
Tatyana Rodionva <ekorodionova@gmail.com>
2018-10-10 19:14:11 +0300

    Add comparator

--- app/src/main/java/org/stepic/droid/code/data/AutocompleteDictionary.kt
+++ app/src/main/java/org/stepic/droid/code/data/AutocompleteDictionary.kt
@@ -5,7 +5,7 @@ import org.stepic.droid.util.isNotOrdered
 /**
  * Class for fast search strings with common prefix
  */
-class AutocompleteDictionary(private val dict: Array<String>, needSort: Boolean = true, val isCaseSensitive : Boolean = true ) {
+class AutocompleteDictionary(private val dict: Array<String>, needSort: Boolean = true, private val isCaseSensitive : Boolean = true) {
     companion object {
         /**
          * Returns next in chars string.
@@ -48,8 +48,9 @@ class AutocompleteDictionary(private val dict: Array<String>, needSort: Boolean 
     }
 
     fun getAutocompleteForPrefix(prefix: String): List<String> {
-        val start = dict.binarySearch(prefix).let { getBinarySearchPosition(it) }
-        val end = dict.binarySearch(incrementString(prefix)).let { getBinarySearchPosition(it) }
+        val comparator = Comparator { s1: String, s2: String -> s1.compareTo(s2, ignoreCase = !isCaseSensitive) }
+        val start = dict.binarySearch(prefix, comparator = comparator).let { getBinarySearchPosition(it) }
+        val end = dict.binarySearch(incrementString(prefix), comparator = comparator).let { getBinarySearchPosition(it) }
         return dict.slice(start until end)
     }
 }
