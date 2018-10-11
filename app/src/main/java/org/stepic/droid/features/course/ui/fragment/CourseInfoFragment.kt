0da41849edf12aa89ad6d84023e96dc41faad589
Ruslan Davletshin <smartmorefeed@gmail.com>
2018-10-11 09:56:01 +0300

    fix set data to adapter

--- app/src/main/java/org/stepic/droid/features/course/ui/fragment/CourseInfoFragment.kt
+++ app/src/main/java/org/stepic/droid/features/course/ui/fragment/CourseInfoFragment.kt
@@ -61,5 +61,7 @@ class CourseInfoFragment : Fragment() {
         course.introVideo?.let {
             courseInfoItems.add(CourseInfoItem.VideoBlock(it))
         }
+
+        adapter.setData(courseInfoItems)
     }
 }
