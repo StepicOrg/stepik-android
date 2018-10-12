7165695a3607257d22b8746c3f63d332cbb43e0e
Ruslan Davletshin <smartmorefeed@gmail.com>
2018-10-12 15:59:51 +0300

    add context to delegate vh

--- app/src/main/java/org/stepic/droid/ui/custom/adapter_delegates/DelegateViewHolder.kt
+++ app/src/main/java/org/stepic/droid/ui/custom/adapter_delegates/DelegateViewHolder.kt
@@ -1,11 +1,15 @@
 package org.stepic.droid.ui.custom.adapter_delegates
 
+import android.content.Context
 import android.support.v7.widget.RecyclerView
 import android.view.View
 
 abstract class DelegateViewHolder<D>(root: View) : RecyclerView.ViewHolder(root) {
     protected var itemData: D? = null
         private set
+
+    protected val context: Context
+        get() = itemView.context
 
     internal fun bind(data: D) {
         itemData = data
