cbfd6af79f51bfded1d745c7701050dcc2f4e6a2
Ruslan Davletshin <smartmorefeed@gmail.com>
2018-10-12 14:53:50 +0300

    protect delegates property

--- app/src/main/java/org/stepic/droid/ui/custom/adapter_delegates/DelegateAdapter.kt
+++ app/src/main/java/org/stepic/droid/ui/custom/adapter_delegates/DelegateAdapter.kt
@@ -4,13 +4,16 @@ import android.support.v7.widget.RecyclerView
 import android.view.ViewGroup
 
 abstract class DelegateAdapter<D, VH : DelegateViewHolder<D>> : RecyclerView.Adapter<VH>() {
-    val delegates = mutableListOf<AdapterDelegate<D, VH>>()
+    private val _delegates = mutableListOf<AdapterDelegate<D, VH>>()
+
+    val delegates: List<AdapterDelegate<D, VH>>
+        get() = _delegates
 
     fun addDelegate(delegate: AdapterDelegate<D, VH>) =
-            delegates.add(delegate)
+            _delegates.add(delegate)
 
     fun removeDelegate(delegate: AdapterDelegate<D, VH>) =
-            delegates.remove(delegate)
+            _delegates.remove(delegate)
 
     override fun getItemViewType(position: Int): Int =
             delegates.indexOfFirst { it.isForViewType(position) }
