7020d8ff759059607f9d464c2a3eaa8a377d63b7
Ruslan Davletshin <smartmorefeed@gmail.com>
2018-11-06 15:45:58 +0300

    add SerializedName annotation

--- app/src/main/java/org/stepic/droid/model/AchievementNotification.kt
+++ app/src/main/java/org/stepic/droid/model/AchievementNotification.kt
@@ -1,7 +1,14 @@
 package org.stepic.droid.model
 
+import com.google.gson.annotations.SerializedName
+
 class AchievementNotification(
+        @SerializedName("user")
         val user: Long,
+
+        @SerializedName("achievement")
         val achievement: Int,
+
+        @SerializedName("kind")
         val kind: String
 )
