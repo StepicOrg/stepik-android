76f8730175e7154835ded763b45bc66bebdd9ca5
Ruslan Davletshin <smartmorefeed@gmail.com>
2018-11-08 15:39:43 +0300

    expose setter of itemData property due to Kotlin bug https://youtrack.jetbrains.com/issue/KT-22465

--- app/src/main/java/org/stepic/droid/ui/custom/adapter_delegates/DelegateViewHolder.kt
+++ app/src/main/java/org/stepic/droid/ui/custom/adapter_delegates/DelegateViewHolder.kt
@@ -6,7 +6,7 @@ import android.view.View
 
 abstract class DelegateViewHolder<D>(root: View) : RecyclerView.ViewHolder(root) {
     protected var itemData: D? = null
-        private set
+//        private set // should be private but can't be enabled due to https://youtrack.jetbrains.com/issue/KT-22465
 
     protected val context: Context
         get() = itemView.context
