3b7a425b63e82e8337ca9dcd91ae34735a3f902c
Ruslan Davletshin <smartmorefeed@gmail.com>
2018-11-14 15:26:45 +0300

    fix selectable text view constructor

--- app/src/main/java/org/stepic/droid/ui/custom/SelectableTextView.kt
+++ app/src/main/java/org/stepic/droid/ui/custom/SelectableTextView.kt
@@ -5,11 +5,9 @@ import android.util.AttributeSet
 import android.view.MotionEvent
 import android.widget.TextView
 
-class SelectableTextView : TextView {
-    constructor(context: Context) : super(context)
-    constructor(context: Context, attrs: AttributeSet) : super(context, attrs)
-    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
-
+class SelectableTextView
+@JvmOverloads
+constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0): TextView(context, attrs, defStyleAttr) {
     override fun dispatchTouchEvent(event: MotionEvent?): Boolean {
         if (event?.actionMasked == MotionEvent.ACTION_DOWN && selectionStart != selectionEnd) {
             val txt = this.text
