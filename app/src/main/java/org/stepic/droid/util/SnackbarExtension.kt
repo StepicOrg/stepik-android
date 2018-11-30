32010e36282cee73c377a8f084e3593755211d16
Ruslan Davletshin <smartmorefeed@gmail.com>
2018-11-30 19:11:06 +0300

    fix snack bar extensions

--- app/src/main/java/org/stepic/droid/util/SnackbarExtension.kt
+++ app/src/main/java/org/stepic/droid/util/SnackbarExtension.kt
@@ -1,15 +1,13 @@
 package org.stepic.droid.util
 
-
 import android.support.annotation.ColorInt
 import android.support.design.widget.Snackbar
 import android.widget.TextView
 
-fun Snackbar.setTextColor(@ColorInt textColor: Int): Snackbar {
-    val tv = this.view.findViewById<TextView>(android.support.design.R.id.snackbar_text)
-    tv.setTextColor(textColor)
-    return this
-}
-
+fun Snackbar.setTextColor(@ColorInt textColor: Int): Snackbar =
+    apply {
+        view.findViewById<TextView>(android.support.design.R.id.snackbar_text)
+            .setTextColor(textColor)
+    }
 
 val duration5Sec = 5000
