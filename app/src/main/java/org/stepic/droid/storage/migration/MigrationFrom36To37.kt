324e567418415cac1413ea64d54b496ffd5fb193
Ruslan Davletshin <smartmorefeed@gmail.com>
2018-12-05 21:44:56 +0300

    add migration for blocks videos

--- app/src/main/java/org/stepic/droid/storage/migration/MigrationFrom36To37.kt
+++ app/src/main/java/org/stepic/droid/storage/migration/MigrationFrom36To37.kt
@@ -1,9 +1,7 @@
 package org.stepic.droid.storage.migration
 
 import android.database.sqlite.SQLiteDatabase
-import org.stepic.droid.storage.structure.DbStructureCourse
-import org.stepic.droid.storage.structure.DbStructureCourseList
-import org.stepic.droid.storage.structure.DbStructureLastStep
+import org.stepic.droid.storage.structure.*
 import org.stepik.android.cache.video.structure.VideoDbScheme
 import org.stepik.android.cache.video.structure.VideoUrlDbScheme
 
@@ -11,6 +9,7 @@ object MigrationFrom36To37 : Migration {
     override fun migrate(db: SQLiteDatabase) {
         migrateLastStep(db)
         migrateCourses(db)
+        migrateBlockVideos(db)
     }
 
     private fun migrateLastStep(db: SQLiteDatabase) {
@@ -23,5 +22,28 @@ object MigrationFrom36To37 : Migration {
         DbStructureCourseList.createTable(db)
         VideoDbScheme.createTable(db)
         VideoUrlDbScheme.createTable(db)
+    }
+
+    private fun migrateBlockVideos(db: SQLiteDatabase) {
+        // Migrate urls
+        db.execSQL("""
+            INSERT INTO ${VideoUrlDbScheme.TABLE_NAME}
+            SELECT
+                ${DbStructureVideoUrl.Column.videoId},
+                ${DbStructureVideoUrl.Column.url},
+                ${DbStructureVideoUrl.Column.quality}
+            FROM ${DbStructureVideoUrl.externalVideosName}
+        """.trimIndent())
+
+        // Migrate videos
+        db.execSQL("""
+            INSERT INTO ${VideoDbScheme.TABLE_NAME}
+            SELECT
+                ${DbStructureBlock.Column.EXTERNAL_VIDEO_ID},
+                ${DbStructureBlock.Column.EXTERNAL_THUMBNAIL},
+                ${DbStructureBlock.Column.EXTERNAL_VIDEO_DURATION}
+            FROM ${DbStructureBlock.BLOCKS}
+            WHERE ${DbStructureBlock.BLOCKS}.${DbStructureBlock.Column.EXTERNAL_VIDEO_ID} > 0
+        """.trimIndent())
     }
 }
