c6c54136e32cbe045faad890b5a10f0a01646f2a
Ruslan Davletshin <smartmorefeed@gmail.com>
2018-12-12 15:12:08 +0300

    remove unused code

--- app/src/main/java/org/stepic/droid/persistence/downloads/resolvers/DownloadTitleResolverImpl.kt
+++ app/src/main/java/org/stepic/droid/persistence/downloads/resolvers/DownloadTitleResolverImpl.kt
@@ -2,18 +2,20 @@ package org.stepic.droid.persistence.downloads.resolvers
 
 import io.reactivex.Single
 import org.stepic.droid.persistence.di.PersistenceScope
-import org.stepic.droid.storage.repositories.Repository
-import org.stepik.android.model.Lesson
+import org.stepik.android.domain.lesson.repository.LessonRepository
 import javax.inject.Inject
 
 @PersistenceScope
 class DownloadTitleResolverImpl
 @Inject
 constructor(
-        private val lessonRepository: Repository<Lesson>
+        private val lessonRepository: LessonRepository
 ) : DownloadTitleResolver {
     override fun resolveTitle(lessonId: Long, stepId: Long): Single<String> =
-            Single.fromCallable { lessonRepository.getObject(lessonId) }.map {
+        lessonRepository
+            .getLesson(lessonId)
+            .map {
                 "${it.title} - ${it.steps.indexOf(stepId) + 1}"
             }
+            .toSingle()
 }
