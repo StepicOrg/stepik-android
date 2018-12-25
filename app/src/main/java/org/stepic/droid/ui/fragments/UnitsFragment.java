100dda8ce7186ec48ac04775fd85dc298f734107
Ruslan Davletshin <smartmorefeed@gmail.com>
2018-12-25 17:41:40 +0300

    remove units learning progress presenter

--- app/src/main/java/org/stepic/droid/ui/fragments/UnitsFragment.java
+++ app/src/main/java/org/stepic/droid/ui/fragments/UnitsFragment.java
@@ -7,9 +7,7 @@ import android.view.View;
 import org.jetbrains.annotations.NotNull;
 import org.stepic.droid.R;
 import org.stepic.droid.base.FragmentBase;
-import org.stepic.droid.core.presenters.UnitsLearningProgressPresenter;
 import org.stepic.droid.core.presenters.UnitsPresenter;
-import org.stepic.droid.core.presenters.contracts.UnitsLearningProgressView;
 import org.stepic.droid.core.presenters.contracts.UnitsView;
 import org.stepic.droid.persistence.model.DownloadProgress;
 import org.stepik.android.model.Section;
@@ -18,11 +16,7 @@ import org.stepic.droid.ui.util.ToolbarHelperKt;
 import javax.inject.Inject;
 
 public class UnitsFragment extends FragmentBase implements
-        UnitsView,
-        UnitsLearningProgressView {
-
-    private static final int ANIMATION_DURATION = 0;
-    public static final int DELETE_POSITION_REQUEST_CODE = 165;
+        UnitsView {
 
     private final static String SECTION_KEY = "section_key";
 
@@ -40,9 +34,6 @@ public class UnitsFragment extends FragmentBase implements
     @Inject
     UnitsPresenter unitsPresenter;
 
-    @Inject
-    UnitsLearningProgressPresenter unitsLearningProgressPresenter;
-
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
@@ -53,15 +44,11 @@ public class UnitsFragment extends FragmentBase implements
 
 
         unitsPresenter.attachView(this);
-        unitsLearningProgressPresenter.attachView(this);
-        getLocalProgressManager().subscribe(unitsLearningProgressPresenter);
         unitsPresenter.showUnits(section, false);
     }
 
     @Override
     public void onDestroyView() {
-        getLocalProgressManager().unsubscribe(unitsLearningProgressPresenter);
-        unitsLearningProgressPresenter.detachView(this);
         unitsPresenter.detachView(this);
 
         super.onDestroyView();
@@ -85,14 +72,6 @@ public class UnitsFragment extends FragmentBase implements
 
     @Override
     public void showOnRemoveDownloadDialog(int position) {
-    }
-
-    @Override
-    public void setNewScore(long unitId, double newScore) {
-    }
-
-    @Override
-    public void setUnitPassed(long unitId) {
     }
 
     @Override
