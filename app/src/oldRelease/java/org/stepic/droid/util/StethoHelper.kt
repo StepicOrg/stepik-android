c01864f8cc38acb9e3095ba4d9d52fe1ad407bd7
Ruslan Davletshin <smartmorefeed@gmail.com>
2019-01-09 13:53:52 +0300

    fix stetho imports in oldRelease build

--- app/src/oldRelease/java/org/stepic/droid/util/StethoHelper.kt
+++ app/src/oldRelease/java/org/stepic/droid/util/StethoHelper.kt
@@ -1,6 +1,8 @@
 package org.stepic.droid.util
 
 import android.app.Application
+import com.facebook.stetho.Stetho
+import com.facebook.stetho.okhttp3.StethoInterceptor
 import okhttp3.Interceptor
 
 object StethoHelper {
