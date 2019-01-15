d742fe46b6233ba485220469ce72c783924b60f7
Ruslan Davletshin <smartmorefeed@gmail.com>
2019-01-15 16:39:14 +0300

    single item methods for billing repository

--- app/src/main/java/org/stepik/android/domain/billing/repository/BillingRepository.kt
+++ app/src/main/java/org/stepik/android/domain/billing/repository/BillingRepository.kt
@@ -1,8 +1,10 @@
 package org.stepik.android.domain.billing.repository
 
+import io.reactivex.Maybe
 import io.reactivex.Single
 import org.solovyev.android.checkout.Purchase
 import org.solovyev.android.checkout.Sku
+import org.stepic.droid.util.maybeFirst
 
 interface BillingRepository {
     /**
@@ -11,7 +13,23 @@ interface BillingRepository {
     fun getInventory(productType: String, skuIds: List<String>): Single<List<Sku>>
 
     /**
+     * Return sku
+     */
+    fun getInventory(productType: String, sku: String): Maybe<Sku> =
+        getInventory(productType, listOf(sku))
+            .maybeFirst()
+
+    /**
      * Return all available purchases (not consumed)
      */
     fun getAllPurchases(productType: String): Single<List<Purchase>>
+
+    /**
+     * Returns all available purchases with skus
+     */
+    fun getAllPurchases(productType: String, skus: List<String>): Single<List<Purchase>> =
+        getAllPurchases(productType)
+            .map { purchases ->
+                purchases.filter { it.sku in skus }
+            }
 }
