2f903a52bd5aedba0e4653b633965dd2789d48b9
Ruslan Davletshin <smartmorefeed@gmail.com>
2019-01-15 17:53:32 +0300

    serialized course enrolment state

--- app/src/main/java/org/stepik/android/domain/course/model/EnrollmentState.kt
+++ app/src/main/java/org/stepik/android/domain/course/model/EnrollmentState.kt
@@ -1,8 +1,9 @@
 package org.stepik.android.domain.course.model
 
 import org.solovyev.android.checkout.Sku
+import java.io.Serializable
 
-sealed class EnrollmentState {
+sealed class EnrollmentState : Serializable {
     object Enrolled : EnrollmentState()
     object NotEnrolledFree : EnrollmentState()
     class NotEnrolledInApp(val sku: Sku) : EnrollmentState()
