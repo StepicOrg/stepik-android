da1df7143ea214003451e08a405d71709752cfe6
Ruslan Davletshin <smartmorefeed@gmail.com>
2019-01-16 13:25:33 +0300

    add course payment model

--- app/src/main/java/org/stepik/android/domain/course_payments/model/CoursePayment.kt
+++ app/src/main/java/org/stepik/android/domain/course_payments/model/CoursePayment.kt
@@ -1,3 +1,43 @@
 package org.stepik.android.domain.course_payments.model
 
-class CoursePayment
+import com.google.gson.annotations.SerializedName
+
+class CoursePayment(
+    @SerializedName("id")
+    val id: Long,
+
+    @SerializedName("course")
+    val course: Long,
+
+    @SerializedName("is_paid")
+    val isPaid: Boolean,
+
+    @SerializedName("status")
+    val status: Status,
+
+    @SerializedName("user")
+    val user: Long
+) {
+    /**
+     * CoursePaymentStatus
+     */
+    enum class Status {
+        @SerializedName("pending")
+        PENDING,
+
+        @SerializedName("amount_blocked")
+        AMOUNT_BLOCKED,
+
+        @SerializedName("success")
+        SUCCESS,
+
+        @SerializedName("canceled")
+        CANCELED,
+
+        @SerializedName("failed")
+        FAILED,
+
+        @SerializedName("expired")
+        EXPIRED
+    }
+}
