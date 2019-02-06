bf3f33e9876dddb29bbb7b36351aeff0ca8985ad
Rostislav Smirnov <rostislavsmirnoff@gmail.com>
2019-02-07 00:05:03 +0300

    ChooseCalendarDialog refactor

--- app/src/main/java/org/stepik/android/view/course_calendar/ui/ChooseCalendarDialog.kt
+++ app/src/main/java/org/stepik/android/view/course_calendar/ui/ChooseCalendarDialog.kt
@@ -1,7 +1,10 @@
 package org.stepik.android.view.course_calendar.ui
 
+import android.app.Activity
 import android.app.Dialog
+import android.content.Intent
 import android.os.Bundle
+import android.os.Parcelable
 import android.support.v4.app.DialogFragment
 import android.support.v7.app.AlertDialog
 import org.stepic.droid.R
@@ -9,6 +12,22 @@ import org.stepic.droid.model.CalendarItem
 import java.util.*
 
 class ChooseCalendarDialog : DialogFragment() {
+
+    companion object {
+        private const val parcelableArrayListKey = "parcelable_list_key"
+        const val KEY_CALENDAR_ITEM = "calendar_item"
+        const val CHOOSE_CALENDAR_REQUEST_CODE = 3203
+
+        const val TAG = "choose_calendar_dialog"
+
+        fun newInstance(primariesCalendars: List<CalendarItem>): ChooseCalendarDialog {
+            val args = Bundle()
+            args.putParcelableArrayList(parcelableArrayListKey, ArrayList(primariesCalendars))
+            val fragment = ChooseCalendarDialog()
+            fragment.arguments = args
+            return fragment
+        }
+    }
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         val calendarItems = arguments?.getParcelableArrayList<CalendarItem>(parcelableArrayListKey) ?: arrayListOf()
@@ -21,28 +40,13 @@ class ChooseCalendarDialog : DialogFragment() {
                     dialog.dismiss()
                     val selectedPosition = (dialog as AlertDialog).listView.checkedItemPosition
                     val chosenCalendarItem = calendarItems[selectedPosition]
-                    (targetFragment as CallbackContract).onCalendarChosen(chosenCalendarItem)
+                    targetFragment?.onActivityResult(
+                            CHOOSE_CALENDAR_REQUEST_CODE,
+                            Activity.RESULT_OK,
+                            Intent().putExtra(KEY_CALENDAR_ITEM, chosenCalendarItem as Parcelable)
+                    )
                 }
                 .setSingleChoiceItems(ownerTitles, 0, null)
         return builder.create()
-    }
-
-    companion object {
-        private const val parcelableArrayListKey = "parcelable_list_key"
-
-        fun newInstance(primariesCalendars: ArrayList<CalendarItem>): ChooseCalendarDialog {
-            val args = Bundle()
-            args.putParcelableArrayList(parcelableArrayListKey, primariesCalendars)
-            val fragment = ChooseCalendarDialog()
-            fragment.arguments = args
-            return fragment
-        }
-    }
-
-    /**
-     * The callback should be implemented by caller fragment
-     */
-    interface CallbackContract {
-        fun onCalendarChosen(calendarItem: CalendarItem)
     }
 }
