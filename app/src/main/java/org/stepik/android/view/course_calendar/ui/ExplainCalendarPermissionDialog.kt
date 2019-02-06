185891d30b2567236348c0260f85d1dc7c372a14
Rostislav Smirnov <rostislavsmirnoff@gmail.com>
2019-02-06 22:26:59 +0300

    refactored explain permission dialog

---
+++ app/src/main/java/org/stepik/android/view/course_calendar/ui/ExplainCalendarPermissionDialog.kt
@@ -0,0 +1,36 @@
+package org.stepik.android.view.course_calendar.ui
+
+import android.app.Dialog
+import android.os.Bundle
+import android.support.v4.app.DialogFragment
+import android.support.v7.app.AlertDialog
+import org.stepic.droid.R
+
+class ExplainCalendarPermissionDialog : DialogFragment() {
+
+    companion object {
+        const val TAG = "explain_permission_calendar_dialog"
+        const val REQUEST_CALENDAR_PERMISSION = 1122
+
+        fun newInstance(): DialogFragment {
+            return ExplainCalendarPermissionDialog()
+        }
+    }
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val builder = AlertDialog.Builder(activity!!)
+        builder.setTitle(R.string.allow_question)
+                .setMessage(R.string.explain_calendar_permission)
+                .setPositiveButton(R.string.allow) { _, _ ->
+                    (targetFragment as Callback).onCalendarPermissionChosen(true)
+                }
+                .setNegativeButton(R.string.deny) { _, _ ->
+                    (targetFragment as Callback).onCalendarPermissionChosen(false)
+                }
+        return builder.create()
+    }
+
+    interface Callback {
+        fun onCalendarPermissionChosen(isAgreed: Boolean)
+    }
+}
