91de9c367071bb24e57a5e5936118b431c3b2242
Rostislav Smirnov <rostislavsmirnoff@gmail.com>
2019-02-13 00:08:55 +0300

    section date event dao implementation

--- app/src/main/java/org/stepic/droid/storage/dao/SectionDateEventDaoImpl.kt
+++ app/src/main/java/org/stepic/droid/storage/dao/SectionDateEventDaoImpl.kt
@@ -1,4 +1,42 @@
 package org.stepic.droid.storage.dao
 
-class SectionDateEventDaoImpl {
+import android.content.ContentValues
+import android.database.Cursor
+import org.stepic.droid.storage.operations.DatabaseOperations
+import org.stepik.android.cache.course_calendar.structure.DbStructureSectionDateEvent
+import org.stepik.android.domain.course_calendar.model.SectionDateEvent
+import javax.inject.Inject
+
+class SectionDateEventDaoImpl
+@Inject
+constructor(
+    databaseOperations: DatabaseOperations
+) : DaoBase<SectionDateEvent>(databaseOperations) {
+
+    override fun getDbName(): String = DbStructureSectionDateEvent.TABLE_NAME
+
+    override fun getDefaultPrimaryColumn(): String =
+            DbStructureSectionDateEvent.Columns.EVENT_ID
+
+    override fun getDefaultPrimaryValue(persistentObject: SectionDateEvent): String =
+            persistentObject.eventId.toString()
+
+    override fun getContentValues(persistentObject: SectionDateEvent): ContentValues {
+        val contentValues = ContentValues()
+        contentValues.put(DbStructureSectionDateEvent.Columns.EVENT_ID, persistentObject.eventId)
+        contentValues.put(DbStructureSectionDateEvent.Columns.SECTION_ID, persistentObject.sectionId)
+        return contentValues
+    }
+
+    override fun parsePersistentObject(cursor: Cursor): SectionDateEvent {
+        val eventIdIndex = cursor.getColumnIndex(DbStructureSectionDateEvent.Columns.EVENT_ID)
+        val sectionIdIndex = cursor.getColumnIndex(DbStructureSectionDateEvent.Columns.SECTION_ID)
+
+        return SectionDateEvent(
+                eventId = cursor.getLong(eventIdIndex),
+                sectionId = cursor.getLong(sectionIdIndex)
+        )
+    }
+
+
 }
