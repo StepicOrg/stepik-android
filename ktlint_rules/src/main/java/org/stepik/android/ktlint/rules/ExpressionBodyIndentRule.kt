b097f1482369c37ec250ed085ce4b82d942a5b3c
Ruslan Davletshin <smartmorefeed@gmail.com>
2019-02-16 20:31:20 +0300

    add constants

--- ktlint_rules/src/main/java/org/stepik/android/ktlint/rules/ExpressionBodyIndentRule.kt
+++ ktlint_rules/src/main/java/org/stepik/android/ktlint/rules/ExpressionBodyIndentRule.kt
@@ -8,6 +8,7 @@ import org.jetbrains.kotlin.psi.KtNamedFunction
 class ExpressionBodyIndentRule : Rule("expression-body-indent") {
     companion object {
         private const val MAX_EXPRESSION_BODY_LEN = 10
+        private const val INDENT_SIZE = 4
     }
 
     override fun visit(node: ASTNode, autoCorrect: Boolean, emit: (offset: Int, errorMessage: String, canBeAutoCorrected: Boolean) -> Unit) {
@@ -28,8 +29,8 @@ class ExpressionBodyIndentRule : Rule("expression-body-indent") {
                             val funIndent = funPrev.text.split('\n').last().length
                             val eqIndent = nextEq.text.split('\n').last().length
 
-                            if (funIndent + 4 != eqIndent) {
-                                emit(node.startOffset, "Unexpected indentation of expression body (expected ${funIndent + 4}, actual $eqIndent) ", false)
+                            if (funIndent + INDENT_SIZE != eqIndent) {
+                                emit(node.startOffset, "Unexpected indentation of expression body (expected ${funIndent + INDENT_SIZE}, actual $eqIndent) ", false)
                             }
                         }
                     }
