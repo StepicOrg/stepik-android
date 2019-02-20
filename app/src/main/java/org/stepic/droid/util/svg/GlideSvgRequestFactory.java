581dc62784ec11c9e4a129320de9e0bb0be2e5f9
Rostislav Smirnov <rostislavsmirnoff@gmail.com>
2019-02-20 03:07:43 +0300

    update util/svg package classes

--- app/src/main/java/org/stepic/droid/util/svg/GlideSvgRequestFactory.java
+++ app/src/main/java/org/stepic/droid/util/svg/GlideSvgRequestFactory.java
@@ -3,27 +3,15 @@ package org.stepic.droid.util.svg;
 import android.content.Context;
 import android.graphics.drawable.Drawable;
 import android.graphics.drawable.PictureDrawable;
-import android.net.Uri;
 
-import com.bumptech.glide.GenericRequestBuilder;
 import com.bumptech.glide.Glide;
-import com.bumptech.glide.load.model.StreamEncoder;
-import com.bumptech.glide.load.resource.file.FileToStreamDecoder;
-import com.caverock.androidsvg.SVG;
-
-import java.io.InputStream;
+import com.bumptech.glide.RequestBuilder;
 
 public class GlideSvgRequestFactory {
-    public static GenericRequestBuilder<Uri, InputStream, SVG, PictureDrawable> create(Context context, Drawable placeholder) {
+    public static RequestBuilder<PictureDrawable> create(Context context, Drawable placeholder) {
         return Glide.with(context.getApplicationContext())
-                .using(Glide.buildStreamModelLoader(Uri.class, context), InputStream.class)
-                .from(Uri.class)
-                .as(SVG.class)
-                .transcode(new SvgDrawableTranscoder(), PictureDrawable.class)
-                .sourceEncoder(new StreamEncoder())
-                .cacheDecoder(new FileToStreamDecoder<>(new SvgDecoder()))
-                .decoder(new SvgDecoder())
-                .placeholder(placeholder)
-                .listener(new SvgSoftwareLayerSetter());
+            .as(PictureDrawable.class)
+            .placeholder(placeholder)
+            .listener(new SvgSoftwareLayerSetter());
     }
 }
