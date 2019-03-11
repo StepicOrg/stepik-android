f364c1705b02059f7a3a96abe67ee78c74a9830f
Ruslan Davletshin <smartmorefeed@gmail.com>
2019-03-11 23:15:09 +0300

    add link + details custom fields

---
+++ app/src/main/java/org/stepic/droid/web/model/desk/DeskCustomFields.kt
@@ -0,0 +1,10 @@
+package org.stepic.droid.web.model.desk
+
+import com.google.gson.annotations.SerializedName
+
+class DeskCustomFields(
+    @SerializedName("360015393634")
+    val technicalDetails: String,
+    @SerializedName("360015393334")
+    val link: String
+)
--- app/src/main/java/org/stepic/droid/web/model/desk/DeskRequestWrapper.kt
+++ app/src/main/java/org/stepic/droid/web/model/desk/DeskRequestWrapper.kt
@@ -1,5 +1,28 @@
 package org.stepic.droid.web.model.desk
 
-class DeskRequestWrapper(val request: DeskRequest) {
-    constructor(name: String, email: String, subject: String, comment: String) : this(DeskRequest(DeskRequester(name, email), subject, DeskComment(comment)))
+import com.google.gson.annotations.SerializedName
+
+class DeskRequestWrapper(
+    @SerializedName("request")
+    val request: DeskRequest
+) {
+    constructor(
+        name: String,
+        email: String,
+        subject: String,
+        comment: String,
+
+        link: String,
+        technicalDetails: String
+    ) : this(
+        request = DeskRequest(
+            requester = DeskRequester(name, email),
+            subject = subject,
+            comment = DeskComment(comment),
+            customFields = DeskCustomFields(
+                link = link,
+                technicalDetails = technicalDetails
+            )
+        )
+    )
 }
