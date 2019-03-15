1c0709db97b2f0f2f21c66fd0fa677f70588d695
Ruslan Davletshin <smartmorefeed@gmail.com>
2019-03-15 14:39:15 +0300

    email address data module

---
+++ app/src/main/java/org/stepik/android/view/injection/email_address/EmailAddressDataModule.kt
@@ -0,0 +1,38 @@
+package org.stepik.android.view.injection.email_address
+
+import dagger.Binds
+import dagger.Module
+import dagger.Provides
+import org.stepik.android.data.email_address.repository.EmailAddressRepositoryImpl
+import org.stepik.android.data.email_address.source.EmailAddressRemoteDataSource
+import org.stepik.android.domain.email_address.repository.EmailAddressRepository
+import org.stepik.android.remote.email_address.EmailAddressRemoteDataSourceImpl
+import org.stepik.android.remote.email_address.service.EmailAddressService
+import org.stepik.android.view.injection.base.Authorized
+import retrofit2.Retrofit
+
+@Module
+abstract class EmailAddressDataModule {
+    @Binds
+    internal abstract fun bindEmailAddressRepository(
+        emailAddressRepositoryImpl: EmailAddressRepositoryImpl
+    ): EmailAddressRepository
+
+    @Binds
+    internal abstract fun bindEmailAddressRemoteDataSource(
+        emailAddressRemoteDataSourceImpl: EmailAddressRemoteDataSourceImpl
+    ): EmailAddressRemoteDataSource
+
+//    @Binds
+//    internal abstract fun bindEmailAddressCacheDataSource(
+//        emailAddressCacheDataSourceImpl: EmailAddressCacheDataSource
+//    )
+
+    @Module
+    companion object {
+        @Provides
+        @JvmStatic
+        internal fun provideEmailAddressService(@Authorized retrofit: Retrofit): EmailAddressService =
+            retrofit.create(EmailAddressService::class.java)
+    }
+}
