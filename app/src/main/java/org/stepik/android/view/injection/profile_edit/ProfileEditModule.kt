573b86863bd768d8d632eeea5c3b8f36dbd87b92
Ruslan Davletshin <smartmorefeed@gmail.com>
2019-03-15 18:09:05 +0300

    profile edit module

---
+++ app/src/main/java/org/stepik/android/view/injection/profile_edit/ProfileEditModule.kt
@@ -0,0 +1,26 @@
+package org.stepik.android.view.injection.profile_edit
+
+import android.arch.lifecycle.ViewModel
+import dagger.Binds
+import dagger.Module
+import dagger.multibindings.IntoMap
+import org.stepik.android.presentation.base.injection.ViewModelKey
+import org.stepik.android.presentation.profile_edit.ProfileEditInfoPresenter
+import org.stepik.android.presentation.profile_edit.ProfileEditPasswordPresenter
+
+@Module
+abstract class ProfileEditModule {
+
+    /**
+     * Presentation layer
+     */
+    @Binds
+    @IntoMap
+    @ViewModelKey(ProfileEditInfoPresenter::class)
+    internal abstract fun bindProfileEditInfoPresenter(profileEditInfoPresenter: ProfileEditInfoPresenter): ViewModel
+
+    @Binds
+    @IntoMap
+    @ViewModelKey(ProfileEditPasswordPresenter::class)
+    internal abstract fun bindProfileEditPasswordPresenter(profileEditPasswordPresenter: ProfileEditPasswordPresenter): ViewModel
+}
