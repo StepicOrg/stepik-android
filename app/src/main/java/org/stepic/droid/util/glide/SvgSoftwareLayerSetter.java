2d49b8516e63d0737eaae809140eadbf1d636119
Ruslan Davletshin <smartmorefeed@gmail.com>
2019-04-12 15:57:24 +0300

    ignore non ImageViewTarget targets in layer setter

--- app/src/main/java/org/stepic/droid/util/glide/SvgSoftwareLayerSetter.java
+++ app/src/main/java/org/stepic/droid/util/glide/SvgSoftwareLayerSetter.java
@@ -23,16 +23,20 @@ public class SvgSoftwareLayerSetter implements RequestListener<PictureDrawable> 
     @Override
     public boolean onLoadFailed(GlideException e, Object model, Target<PictureDrawable> target,
                                 boolean isFirstResource) {
-        ImageView view = ((ImageViewTarget<?>) target).getView();
-        view.setLayerType(ImageView.LAYER_TYPE_NONE, null);
+        if (target instanceof ImageViewTarget) {
+            ImageView view = ((ImageViewTarget<?>) target).getView();
+            view.setLayerType(ImageView.LAYER_TYPE_NONE, null);
+        }
         return false;
     }
 
     @Override
     public boolean onResourceReady(PictureDrawable resource, Object model,
                                    Target<PictureDrawable> target, DataSource dataSource, boolean isFirstResource) {
-        ImageView view = ((ImageViewTarget<?>) target).getView();
-        view.setLayerType(ImageView.LAYER_TYPE_SOFTWARE, null);
+        if (target instanceof ImageViewTarget) {
+            ImageView view = ((ImageViewTarget<?>) target).getView();
+            view.setLayerType(ImageView.LAYER_TYPE_SOFTWARE, null);
+        }
         return false;
     }
 }
