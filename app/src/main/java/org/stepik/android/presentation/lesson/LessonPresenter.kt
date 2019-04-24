3691858d056c4378e740e3c2bd68cd25747cc1cb
Ruslan Davletshin <smartmorefeed@gmail.com>
2019-04-24 15:37:22 +0300

    lesson view + presenter

---
+++ app/src/main/java/org/stepik/android/presentation/lesson/LessonPresenter.kt
@@ -0,0 +1,27 @@
+package org.stepik.android.presentation.lesson
+
+import io.reactivex.Scheduler
+import org.stepic.droid.di.qualifiers.BackgroundScheduler
+import org.stepic.droid.di.qualifiers.MainScheduler
+import org.stepik.android.presentation.base.PresenterBase
+import javax.inject.Inject
+
+class LessonPresenter
+@Inject
+constructor(
+    @BackgroundScheduler
+    private val backgroundScheduler: Scheduler,
+    @MainScheduler
+    private val mainScheduler: Scheduler
+) : PresenterBase<LessonView>() {
+    private var state: LessonView.State = LessonView.State.Idle
+        set(value) {
+            field = value
+            view?.setState(value)
+        }
+
+    override fun attachView(view: LessonView) {
+        super.attachView(view)
+        view.setState(state)
+    }
+}
---
+++ app/src/main/java/org/stepik/android/presentation/lesson/LessonView.kt
@@ -0,0 +1,35 @@
+package org.stepik.android.presentation.lesson
+
+import org.stepic.droid.persistence.model.StepPersistentWrapper
+import org.stepik.android.model.Lesson
+
+interface LessonView {
+    sealed class State {
+        object Idle : State()
+        object Loading : State()
+
+        object LessonNotFound : State()
+        object EmptyLogin : State()
+        object NewtorkError : State()
+
+        data class EmptyLesson(
+            val lesson: Lesson
+        ) : State()
+
+        data class LessonLoaded(
+            val lesson: Lesson,
+            val stepsState: StepsState
+        ) : State()
+    }
+
+    sealed class StepsState {
+        object Idle : StepsState()
+        object Loading : StepsState()
+        object NetworkError : StepsState()
+        class Loaded(
+            val steps: List<StepPersistentWrapper>
+        ) : StepsState()
+    }
+
+    fun setState(state: State)
+}
