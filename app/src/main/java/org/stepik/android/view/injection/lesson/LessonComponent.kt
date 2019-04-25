1104cc1f2d9e59d9394327ff1168f6a683ebcae0
Ruslan Davletshin <smartmorefeed@gmail.com>
2019-04-25 15:30:11 +0300

    step data module

--- app/src/main/java/org/stepik/android/view/injection/lesson/LessonComponent.kt
+++ app/src/main/java/org/stepik/android/view/injection/lesson/LessonComponent.kt
@@ -1,11 +1,16 @@
 package org.stepik.android.view.injection.lesson
 
 import dagger.Subcomponent
+import org.stepik.android.view.injection.progress.ProgressDataModule
+import org.stepik.android.view.injection.step.StepDataModule
 import org.stepik.android.view.lesson.ui.activity.LessonActivity
 
 @Subcomponent(modules = [
     LessonModule::class,
-    LessonDataModule::class
+    LessonDataModule::class,
+
+    StepDataModule::class,
+    ProgressDataModule::class
 ])
 interface LessonComponent {
     @Subcomponent.Builder
