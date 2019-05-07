c9254a639640e1871189db2fc112dc6a0f8be56f
Rostislav Smirnov <rostislavsmirnoff@gmail.com>
2019-05-07 16:53:02 +0300

    notification manager updated

--- app/src/main/java/org/stepik/android/view/notification/StepikNotifManager.kt
+++ app/src/main/java/org/stepik/android/view/notification/StepikNotifManager.kt
@@ -1,6 +1,9 @@
 package org.stepik.android.view.notification
 
+import android.app.Notification
+
 interface StepikNotifManager {
     fun scheduleNotification(id: String, millis: Long)
     fun rescheduleActiveNotifications()
+    fun showNotification(id: Long, notification: Notification)
 }
--- app/src/main/java/org/stepik/android/view/notification/StepikNotifManagerImpl.kt
+++ app/src/main/java/org/stepik/android/view/notification/StepikNotifManagerImpl.kt
@@ -1,27 +1,43 @@
 package org.stepik.android.view.notification
 
 import android.app.AlarmManager
+import android.app.Notification
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
-import org.stepic.droid.preferences.SharedPreferenceHelper
+import org.stepic.droid.receivers.AlarmReceiver
+import org.stepic.droid.util.scheduleCompat
 import java.util.concurrent.ThreadPoolExecutor
 import javax.inject.Inject
 
 class StepikNotifManagerImpl
 @Inject constructor(
-    private val context: Context,
-    private val sharedPreferenceHelper: SharedPreferenceHelper,
-    private val threadPoolExecutor: ThreadPoolExecutor
+        private val context: Context,
+        private val threadPoolExecutor: ThreadPoolExecutor
 ) : StepikNotifManager {
 
     private val alarmManager: AlarmManager by lazy { context.getSystemService(Context.ALARM_SERVICE) as AlarmManager }
-    private val notifcationManager: NotificationManager by lazy { context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager }
+    private val notificationManager: NotificationManager by lazy { context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager }
 
     override fun scheduleNotification(id: String, millis: Long) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        threadPoolExecutor.execute {
+            val intent = AlarmReceiver
+                    .createIntent(context, id)
+
+            val pendingIntent = PendingIntent
+                    .getBroadcast(context, AlarmReceiver.REQUEST_CODE, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+
+            alarmManager.cancel(pendingIntent)
+
+            alarmManager.scheduleCompat(millis, AlarmManager.INTERVAL_FIFTEEN_MINUTES, pendingIntent)
+        }
     }
 
     override fun rescheduleActiveNotifications() {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun showNotification(id: Long, notification: Notification) {
+        notificationManager.notify(id.toInt(), notification)
     }
 }
