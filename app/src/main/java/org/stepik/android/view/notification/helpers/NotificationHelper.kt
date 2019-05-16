797f93a7e86cc9abfc1c3518205661b9ff02fb3e
Rostislav Smirnov <rostislavsmirnoff@gmail.com>
2019-05-16 16:21:16 +0300

    transfer more methods from old notification manager to notification helper

--- app/src/main/java/org/stepik/android/view/notification/helpers/NotificationHelper.kt
+++ app/src/main/java/org/stepik/android/view/notification/helpers/NotificationHelper.kt
@@ -1,12 +1,19 @@
 package org.stepik.android.view.notification.helpers
 
 import android.app.PendingIntent
+import android.content.Intent
 import android.support.v4.app.NotificationCompat
 import android.support.v4.app.TaskStackBuilder
+import org.stepic.droid.notifications.model.Notification
 
 interface NotificationHelper {
     fun makeSimpleNotificationBuilder(stepikNotification: org.stepic.droid.notifications.model.Notification?, justText: String, taskBuilder: TaskStackBuilder, title: String?, deleteIntent: PendingIntent = getDeleteIntent(), id: Long): NotificationCompat.Builder
     fun getDeleteIntent(courseId: Long = -1): PendingIntent
     fun addVibrationIfNeed(builder: NotificationCompat.Builder)
     fun addSoundIfNeed(builder: NotificationCompat.Builder)
+    fun getTeachIntent(notification: Notification): Intent?
+    fun getLicenseIntent(notification: Notification): Intent?
+    fun getDefaultIntent(notification: Notification): Intent?
+    fun getReviewIntent(notification: Notification): Intent?
+    fun getCommentIntent(notification: Notification): Intent?
 }
--- app/src/main/java/org/stepik/android/view/notification/helpers/NotificationHelperImpl.kt
+++ app/src/main/java/org/stepik/android/view/notification/helpers/NotificationHelperImpl.kt
@@ -9,19 +9,26 @@ import android.support.v4.app.NotificationCompat
 import android.support.v4.app.TaskStackBuilder
 import org.stepic.droid.R
 import org.stepic.droid.analytic.Analytic
+import org.stepic.droid.configuration.Config
+import org.stepic.droid.core.ScreenManager
 import org.stepic.droid.notifications.NotificationBroadcastReceiver
 import org.stepic.droid.notifications.NotificationTimeChecker
 import org.stepic.droid.notifications.model.Notification
 import org.stepic.droid.notifications.model.StepikNotificationChannel
 import org.stepic.droid.preferences.UserPreferences
+import org.stepic.droid.ui.activities.StepsActivity
 import org.stepic.droid.util.AppConstants
 import org.stepic.droid.util.ColorUtil
 import org.stepic.droid.util.DateTimeHelper
+import org.stepic.droid.util.HtmlHelper
+import org.stepik.android.view.course.ui.activity.CourseActivity
 import javax.inject.Inject
 
 class NotificationHelperImpl
 @Inject constructor(
     val context: Context,
+    val configs: Config,
+    val screenManager: ScreenManager,
     val notificationTimeChecker: NotificationTimeChecker,
     val analytic: Analytic,
     val userPreferences: UserPreferences
@@ -86,5 +93,65 @@ class NotificationHelperImpl
                     + context.packageName + "/" + R.raw.default_sound)
             builder.setSound(stepicSound)
         }
+    }
+
+    override fun getTeachIntent(notification: Notification): Intent? {
+        val link = HtmlHelper.parseNLinkInText(notification.htmlText ?: "", configs.baseUrl, 0) ?: return null
+        try {
+            val url = Uri.parse(link)
+            val identifier = url.pathSegments[0]
+            val intent: Intent =
+                when(identifier){
+                    "course" ->
+                        Intent(context, CourseActivity::class.java)
+                    "lesson" ->
+                        Intent(context, StepsActivity::class.java)
+                    else ->
+                        return null
+                }
+            intent.data = url
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            return intent
+        } catch (exception: Exception) {
+            return null
+        }
+    }
+
+    override fun getLicenseIntent(notification: Notification): Intent? {
+        val link = HtmlHelper.parseNLinkInText(notification.htmlText ?: "", configs.baseUrl, 0) ?: return null
+        val intent = screenManager.getOpenInWebIntent(link)
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        return intent
+    }
+
+    override fun getDefaultIntent(notification: Notification): Intent? {
+        val data = HtmlHelper.parseNLinkInText(notification.htmlText ?: "", configs.baseUrl, 1) ?: return null
+        val intent = Intent(context, CourseActivity::class.java)
+        intent.data = Uri.parse(data)
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        return intent
+    }
+
+    override fun getReviewIntent(notification: Notification): Intent? {
+        val data = HtmlHelper.parseNLinkInText(notification.htmlText ?: "", configs.baseUrl, 0) ?: return null
+        val intent = Intent(context, StepsActivity::class.java)
+        intent.data = Uri.parse(data)
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        return intent
+    }
+
+    override fun getCommentIntent(notification: Notification): Intent? {
+        val link: String
+        val action = notification.action
+        val htmlText = notification.htmlText ?: ""
+        if (action == org.stepic.droid.notifications.NotificationHelper.REPLIED) {
+            link = HtmlHelper.parseNLinkInText(htmlText, configs.baseUrl, 1) ?: return null
+        } else {
+            link = HtmlHelper.parseNLinkInText(htmlText, configs.baseUrl, 3) ?: return null
+        }
+        val intent = Intent(context, StepsActivity::class.java)
+        intent.data = Uri.parse(link)
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        return intent
     }
 }
