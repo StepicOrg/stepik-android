47db7d4e57ae560bd58f60237a81077ab203589d
Rostislav Smirnov <rostislavsmirnoff@gmail.com>
2019-05-27 22:44:11 +0300

    replaced notifications manager with delegate

--- app/src/main/java/org/stepik/android/domain/personal_deadlines/interactor/DeadlinesInteractor.kt
+++ app/src/main/java/org/stepik/android/domain/personal_deadlines/interactor/DeadlinesInteractor.kt
@@ -4,13 +4,13 @@ import io.reactivex.Completable
 import io.reactivex.Maybe
 import io.reactivex.Single
 import org.stepik.android.domain.personal_deadlines.model.DeadlinesWrapper
-import org.stepic.droid.features.deadlines.notifications.DeadlinesNotificationsManager
 import org.stepic.droid.util.doCompletableOnSuccess
 import org.stepik.android.domain.personal_deadlines.model.LearningRate
 import org.stepic.droid.web.storage.model.StorageRecord
 import org.stepik.android.domain.personal_deadlines.repository.DeadlinesBannerRepository
 import org.stepik.android.domain.personal_deadlines.repository.DeadlinesRepository
 import org.stepik.android.domain.personal_deadlines.resolver.DeadlinesResolver
+import org.stepik.android.view.notification.delegate.DeadlinesDelegate
 import javax.inject.Inject
 
 class DeadlinesInteractor
@@ -19,23 +19,23 @@ constructor(
     private val deadlinesRepository: DeadlinesRepository,
     private val deadlinesBannerRepository: DeadlinesBannerRepository,
     private val deadlinesResolver: DeadlinesResolver,
-    private val deadlinesNotificationsManager: DeadlinesNotificationsManager
+    private val deadlinesDelegate: DeadlinesDelegate
 ) {
     fun createPersonalDeadlines(courseId: Long, learningRate: LearningRate): Single<StorageRecord<DeadlinesWrapper>> =
         deadlinesResolver
             .calculateDeadlinesForCourse(courseId, learningRate)
             .flatMap(deadlinesRepository::createDeadlineRecord)
-            .doOnSuccess { deadlinesNotificationsManager.scheduleDeadlinesNotifications() }
+            .doOnSuccess { deadlinesDelegate.scheduleDeadlinesNotifications() }
 
     fun updatePersonalDeadlines(record: StorageRecord<DeadlinesWrapper>): Single<StorageRecord<DeadlinesWrapper>> =
         deadlinesRepository
             .updateDeadlineRecord(record)
-            .doOnSuccess { deadlinesNotificationsManager.scheduleDeadlinesNotifications() }
+            .doOnSuccess { deadlinesDelegate.scheduleDeadlinesNotifications() }
 
     fun removePersonalDeadline(recordId: Long): Completable =
         deadlinesRepository
             .removeDeadlineRecord(recordId)
-            .doOnComplete { deadlinesNotificationsManager.scheduleDeadlinesNotifications() }
+            .doOnComplete { deadlinesDelegate.scheduleDeadlinesNotifications() }
 
     fun getPersonalDeadlineByCourseId(courseId: Long): Maybe<StorageRecord<DeadlinesWrapper>> =
         deadlinesRepository
