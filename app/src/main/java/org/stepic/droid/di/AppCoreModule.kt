7ea41262cef3a07ead3f4b4437054d19de8d6023
Rostislav Smirnov <rostislavsmirnoff@gmail.com>
2019-05-27 22:31:06 +0300

    providing all of the delegates in app core module

--- app/src/main/java/org/stepic/droid/di/AppCoreModule.kt
+++ app/src/main/java/org/stepic/droid/di/AppCoreModule.kt
@@ -52,7 +52,7 @@ import org.stepic.droid.web.UserAgentProvider
 import org.stepic.droid.web.UserAgentProviderImpl
 import org.stepik.android.presentation.base.injection.DaggerViewModelFactory
 import org.stepik.android.view.notification.*
-import org.stepik.android.view.notification.delegate.DeadlinesDelegate
+import org.stepik.android.view.notification.delegate.*
 import org.stepik.android.view.notification.helpers.NotificationHelper
 import org.stepik.android.view.notification.helpers.NotificationHelperImpl
 import retrofit2.Retrofit
@@ -159,6 +159,22 @@ abstract class AppCoreModule {
     @Binds
     @IntoSet
     internal abstract fun provideDeadlinesNotificationDelegate(deadlinesDelegate: DeadlinesDelegate): NotificationDelegate
+
+    @Binds
+    @IntoSet
+    internal abstract fun provideRemindRegistrationNotificationDelegate(remindRegistrationDelegate: RemindRegistrationDelegate): NotificationDelegate
+
+    @Binds
+    @IntoSet
+    internal abstract fun provideRemindAppNotificationDelegate(remindAppDelegate: RemindAppDelegate): NotificationDelegate
+
+    @Binds
+    @IntoSet
+    internal abstract fun provideRetentionNotificationDelegate(retentionDelegate: RetentionDelegate): NotificationDelegate
+
+    @Binds
+    @IntoSet
+    internal abstract fun provideStreakNotificationDelegate(streakDelegate: StreakDelegate): NotificationDelegate
 
     @Module
     companion object {
