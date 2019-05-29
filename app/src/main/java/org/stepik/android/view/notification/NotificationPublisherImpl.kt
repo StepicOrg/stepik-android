3a40ad96a9f88322620a2336bb6454dfd8d072ed
Rostislav Smirnov <rostislavsmirnoff@gmail.com>
2019-05-30 01:48:51 +0300

    code style

--- app/src/main/java/org/stepik/android/view/notification/NotificationPublisherImpl.kt
+++ app/src/main/java/org/stepik/android/view/notification/NotificationPublisherImpl.kt
@@ -3,7 +3,9 @@ package org.stepik.android.view.notification
 import javax.inject.Inject
 
 class NotificationPublisherImpl
-@Inject constructor(val notificationDelegates: Set<@JvmSuppressWildcards NotificationDelegate>) : NotificationPublisher {
+@Inject constructor(
+    private val notificationDelegates: Set<@JvmSuppressWildcards NotificationDelegate>
+) : NotificationPublisher {
     override fun onNeedShowNotificationWithId(id: String) {
         notificationDelegates
             .find { it.id == id }
