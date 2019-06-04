41e68849cc3a4727f39270e49368b0f54bbb5587
Rostislav Smirnov <rostislavsmirnoff@gmail.com>
2019-06-04 20:33:06 +0300

    add id field to CourseReviewSummary

--- model/src/main/java/org/stepik/android/model/CourseReviewSummary.kt
+++ model/src/main/java/org/stepik/android/model/CourseReviewSummary.kt
@@ -3,6 +3,8 @@ package org.stepik.android.model
 import com.google.gson.annotations.SerializedName
 
 class CourseReviewSummary(
+    @SerializedName("id")
+    val id: Long,
     @SerializedName("course")
     val course: Long,
     @SerializedName("average")
