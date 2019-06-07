757b1a62cdf9d51ea0e35da0f8ca1f97600162ed
Ruslan Davletshin <smartmorefeed@gmail.com>
2019-06-07 18:44:09 +0300

    add parcelable to lesson data

--- app/src/main/java/org/stepik/android/domain/lesson/model/LessonData.kt
+++ app/src/main/java/org/stepik/android/domain/lesson/model/LessonData.kt
@@ -1,5 +1,7 @@
 package org.stepik.android.domain.lesson.model
 
+import android.os.Parcel
+import android.os.Parcelable
 import org.stepik.android.model.Course
 import org.stepik.android.model.Lesson
 import org.stepik.android.model.Section
@@ -11,4 +13,28 @@ data class LessonData(
     val section: Section?,
     val course: Course?,
     val stepPosition: Int = 0
-)
+) : Parcelable {
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeParcelable(lesson, flags)
+        parcel.writeParcelable(unit, flags)
+        parcel.writeParcelable(section, flags)
+        parcel.writeParcelable(course, flags)
+        parcel.writeInt(stepPosition)
+    }
+
+    override fun describeContents(): Int = 0
+
+    companion object CREATOR : Parcelable.Creator<LessonData> {
+        override fun createFromParcel(parcel: Parcel): LessonData =
+            LessonData(
+                parcel.readParcelable(Lesson::class.java.classLoader)!!,
+                parcel.readParcelable(Unit::class.java.classLoader),
+                parcel.readParcelable(Section::class.java.classLoader),
+                parcel.readParcelable(Course::class.java.classLoader),
+                parcel.readInt()
+            )
+
+        override fun newArray(size: Int): Array<LessonData?> =
+            arrayOfNulls(size)
+    }
+}
