4d5e00c75e846d9fbabb10ae873da475b79bfe3c
Rostislav Smirnov <rostislavsmirnoff@gmail.com>
2019-06-10 23:43:12 +0300

    FeedbackFragment implements FeedbackView from new arch

--- app/src/main/java/org/stepic/droid/ui/fragments/FeedbackFragment.kt
+++ app/src/main/java/org/stepic/droid/ui/fragments/FeedbackFragment.kt
@@ -1,5 +1,7 @@
 package org.stepic.droid.ui.fragments
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -7,8 +9,10 @@ import android.view.ViewGroup
 import kotlinx.android.synthetic.main.fragment_feedback.*
 import org.stepic.droid.R
 import org.stepic.droid.base.FragmentBase
+import org.stepic.droid.util.DeviceInfoUtil
+import org.stepik.android.presentation.feedback.FeedbackView
 
-class FeedbackFragment : FragmentBase() {
+class FeedbackFragment : FragmentBase(), FeedbackView {
     companion object {
         fun newInstance(): FeedbackFragment {
             val args = Bundle()
@@ -19,6 +23,11 @@ class FeedbackFragment : FragmentBase() {
         }
     }
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        injectComponent()
+    }
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View =
             inflater.inflate(R.layout.fragment_feedback, container, false)
 
@@ -27,15 +36,33 @@ class FeedbackFragment : FragmentBase() {
         initButtons()
     }
 
+    override fun sendTextFeedback(mailTo: String, subject: String, body: String) {
+        val emailIntent = Intent(Intent.ACTION_SENDTO)
+        val mailData = getString(R.string.email_intent_template).format(
+            Uri.encode(mailTo),
+            Uri.encode(subject),
+            Uri.encode(body)
+        )
+        emailIntent.data = Uri.parse(mailData)
+        startActivity(emailIntent)
+    }
+
+    override fun getMailToString(): String = getString(R.string.support_email)
+
+    override fun getEmailSubjectString(): String = getString(R.string.feedback_subject)
+
+    override fun getAboutSystemInfo(): String = DeviceInfoUtil.getInfosAboutDevice(context, "<br>")
+
+    // TODO Replace old text feed back action with call to presenter
     private fun initButtons() {
         feedbackGoodButton.setOnClickListener {
             if (config.isAppInStore) {
                 screenManager.showStoreWithApp(activity)
             } else {
-                screenManager.showTextFeedback(activity)
+                // screenManager.showTextFeedback(activity)
             }
         }
-        feedbackBadButton.setOnClickListener { screenManager.showTextFeedback(activity) }
+        // feedbackBadButton.setOnClickListener { screenManager.showTextFeedback(activity) }
     }
 
     private fun destroyButtons() {
--- app/src/main/java/org/stepik/android/presentation/feedback/FeedbackView.kt
+++ app/src/main/java/org/stepik/android/presentation/feedback/FeedbackView.kt
@@ -1,5 +1,8 @@
 package org.stepik.android.presentation.feedback
 
 interface FeedbackView {
-    fun sendTextFeedback()
+    fun sendTextFeedback(mailTo: String, subject: String, body: String)
+    fun getMailToString(): String
+    fun getEmailSubjectString(): String
+    fun getAboutSystemInfo(): String
 }
