6ae69b459a980264c008c5678581a4146976b7b0
Rostislav Smirnov <rostislavsmirnoff@gmail.com>
2019-06-11 00:08:39 +0300

    implemented feedback presenter

--- app/src/main/java/org/stepik/android/presentation/feedback/FeedbackPresenter.kt
+++ app/src/main/java/org/stepik/android/presentation/feedback/FeedbackPresenter.kt
@@ -1,10 +1,16 @@
 package org.stepik.android.presentation.feedback
 
+import org.stepik.android.domain.feedback.interactor.FeedbackInteractor
 import org.stepik.android.presentation.base.PresenterBase
 import javax.inject.Inject
 
 class FeedbackPresenter
 @Inject
-constructor() : PresenterBase<FeedbackView>() {
-
+constructor(
+    private val feedbackInteractor: FeedbackInteractor
+) : PresenterBase<FeedbackView>() {
+    fun sendTextFeedback(mailTo: String, subject: String, aboutSystemInfo: String) {
+        val emailUriData = feedbackInteractor.initUriData(mailTo, subject, aboutSystemInfo)
+        view?.sendTextFeedback(emailUriData.mailTo, emailUriData.subject, emailUriData.body)
+    }
 }
