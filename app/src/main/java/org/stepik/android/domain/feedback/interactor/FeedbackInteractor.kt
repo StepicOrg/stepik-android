9981ae47efbe4e1b8e1837751c71f6d809a52bb0
Rostislav Smirnov <rostislavsmirnoff@gmail.com>
2019-06-11 00:08:19 +0300

    Feedback interactor

---
+++ app/src/main/java/org/stepik/android/domain/feedback/interactor/FeedbackInteractor.kt
@@ -0,0 +1,29 @@
+package org.stepik.android.domain.feedback.interactor
+
+import org.stepic.droid.configuration.Config
+import org.stepic.droid.preferences.SharedPreferenceHelper
+import org.stepik.android.domain.feedback.model.EmailUriData
+import javax.inject.Inject
+
+class FeedbackInteractor
+@Inject
+constructor(
+    private val config: Config,
+    private val sharedPreferenceHelper: SharedPreferenceHelper
+) {
+    fun initUriData(mailTo: String, subject: String, aboutSystem: String): EmailUriData {
+
+        return EmailUriData(
+            mailTo = mailTo,
+            subject = subject,
+            body = formBodyField(aboutSystem)
+        )
+    }
+
+    private fun formBodyField(aboutSystem: String): String {
+        val profileData = sharedPreferenceHelper.profile.let { profile ->
+            "${profile?.fullName}\n${config.baseUrl}/users/${profile?.id}"
+        }
+        return "$profileData\n$aboutSystem"
+    }
+}
---
+++ app/src/main/java/org/stepik/android/domain/feedback/model/EmailUriData.kt
@@ -0,0 +1,7 @@
+package org.stepik.android.domain.feedback.model
+
+data class EmailUriData(
+    val mailTo: String,
+    val subject: String,
+    val body: String
+)
