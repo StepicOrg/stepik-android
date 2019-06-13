e7c749daa5578fd51bcff579fe8550d39e5f7114
Rostislav Smirnov <rostislavsmirnoff@gmail.com>
2019-06-13 19:43:44 +0300

    replaced stack with list

--- app/src/main/java/org/stepic/droid/util/IntentExtensions.kt
+++ app/src/main/java/org/stepic/droid/util/IntentExtensions.kt
@@ -3,10 +3,9 @@ package org.stepic.droid.util
 import android.content.Context
 import android.content.Intent
 import android.net.Uri
-import java.util.*
 
 fun Intent.createEmailOnlyChooserIntent(context: Context, title: CharSequence): Intent {
-    val intents = Stack<Intent>()
+    val intents = mutableListOf<Intent>()
     val dummyIntent = Intent(Intent.ACTION_SENDTO, Uri.fromParts("mailto", "info@domain.com", null))
     val activities = context.packageManager.queryIntentActivities(dummyIntent, 0)
 
@@ -16,9 +15,9 @@ fun Intent.createEmailOnlyChooserIntent(context: Context, title: CharSequence): 
         intents.add(target)
     }
 
-    return if (!intents.empty()) {
+    return if (!intents.isEmpty()) {
         val chooserIntent = Intent.createChooser(intents.removeAt(0), title)
-        chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, intents.toArray())
+        chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, intents.toTypedArray())
         chooserIntent
     } else {
         Intent.createChooser(this, title)
