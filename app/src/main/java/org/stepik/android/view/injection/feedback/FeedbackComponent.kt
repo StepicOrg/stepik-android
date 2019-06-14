14e2a352e70f6e6fd4fd1a7074ad460cecc6526f
Rostislav Smirnov <rostislavsmirnoff@gmail.com>
2019-06-14 18:47:21 +0300

    add data module to feedback component

--- app/src/main/java/org/stepik/android/view/injection/feedback/FeedbackComponent.kt
+++ app/src/main/java/org/stepik/android/view/injection/feedback/FeedbackComponent.kt
@@ -3,7 +3,7 @@ package org.stepik.android.view.injection.feedback
 import dagger.Subcomponent
 import org.stepic.droid.ui.fragments.FeedbackFragment
 
-@Subcomponent(modules = [FeedbackModule::class])
+@Subcomponent(modules = [FeedbackModule::class, FeedbackDataModule::class])
 interface FeedbackComponent {
     @Subcomponent.Builder
     interface Builder {
