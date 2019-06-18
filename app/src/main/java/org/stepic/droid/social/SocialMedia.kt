0f0429b62f4aeb0f52ce47111c722f1a014882a6
Rostislav Smirnov <rostislavsmirnoff@gmail.com>
2019-06-18 19:08:44 +0300

    naming change

--- app/src/main/java/org/stepic/droid/social/SocialMedia.kt
+++ app/src/main/java/org/stepic/droid/social/SocialMedia.kt
@@ -10,6 +10,6 @@ enum class SocialMedia(
     val drawable: Int
 ) {
     vk("https://vk.com/rustepik", R.drawable.ic_login_social_vk),
-    fb("https://www.facebook.com/rustepik/", R.drawable.ic_login_social_fb),
+    facebook("https://www.facebook.com/rustepik/", R.drawable.ic_login_social_fb),
     instagram("https://www.instagram.com/stepik.education/", R.drawable.ic_social_instagram)
 }
