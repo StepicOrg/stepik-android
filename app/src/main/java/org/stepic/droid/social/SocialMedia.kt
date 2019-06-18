ce9a17246ad47007c9dda79e9b33b118c4c8eca2
Rostislav Smirnov <rostislavsmirnoff@gmail.com>
2019-06-18 18:57:26 +0300

    add social media enum

---
+++ app/src/main/java/org/stepic/droid/social/SocialMedia.kt
@@ -0,0 +1,15 @@
+package org.stepic.droid.social
+
+import android.support.annotation.DrawableRes
+import org.stepic.droid.R
+
+
+enum class SocialMedia(
+    val link: String,
+    @DrawableRes
+    val drawable: Int
+) {
+    vk("https://vk.com/rustepik", R.drawable.ic_login_social_vk),
+    fb("https://www.facebook.com/rustepik/", R.drawable.ic_login_social_fb),
+    instagram("https://www.instagram.com/stepik.education/", R.drawable.ic_social_instagram)
+}
