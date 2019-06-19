07f941ee9a6b6b44160fb4d47ec4d81b65b749bc
Ruslan Davletshin <smartmorefeed@gmail.com>
2019-06-19 14:29:40 +0300

    handle empty video

--- app/src/main/java/org/stepik/android/view/step_content_video/ui/fragment/VideoStepContentFragment.kt
+++ app/src/main/java/org/stepik/android/view/step_content_video/ui/fragment/VideoStepContentFragment.kt
@@ -1,7 +1,9 @@
 package org.stepik.android.view.step_content_video.ui.fragment
 
 import android.os.Bundle
+import android.support.design.widget.Snackbar
 import android.support.v4.app.Fragment
+import android.support.v4.content.ContextCompat
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -13,6 +15,7 @@ import org.stepic.droid.base.App
 import org.stepic.droid.core.ScreenManager
 import org.stepic.droid.persistence.model.StepPersistentWrapper
 import org.stepic.droid.util.argument
+import org.stepic.droid.util.setTextColor
 import org.stepik.android.domain.lesson.model.LessonData
 import org.stepik.android.view.video_player.model.VideoPlayerMediaData
 import javax.inject.Inject
@@ -57,7 +60,18 @@ class VideoStepContentFragment : Fragment() {
             .placeholder(R.drawable.general_placeholder)
             .into(videoThumbnail)
 
-        videoContainer.setOnClickListener {
+        videoContainer.setOnClickListener { openVideoPlayer() }
+    }
+
+    private fun openVideoPlayer() {
+        if (stepWrapper.cachedVideo == null && stepWrapper.step.block?.video == null) {
+            Snackbar
+                .make(videoStepContent, R.string.step_content_video_no_video, Snackbar.LENGTH_SHORT)
+                .setTextColor(ContextCompat.getColor(requireContext(), R.color.white))
+                .show()
+        } else {
+            val thumbnail = stepWrapper.cachedVideo?.thumbnail
+                ?: stepWrapper.step.block?.video?.thumbnail
             screenManager.showVideo(activity, VideoPlayerMediaData(
                 thumbnail = thumbnail,
                 title = lessonData.lesson.title ?: "",
