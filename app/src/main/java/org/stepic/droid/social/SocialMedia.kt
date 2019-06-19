3067c67cb403796db83b90aa02b3041b0667bd0c
Rostislav Smirnov <rostislavsmirnoff@gmail.com>
2019-06-19 14:28:54 +0300

    change enum naming

--- app/src/main/java/org/stepic/droid/social/SocialMedia.kt
+++ app/src/main/java/org/stepic/droid/social/SocialMedia.kt
@@ -9,7 +9,7 @@ enum class SocialMedia(
     @DrawableRes
     val drawable: Int
 ) {
-    vk("https://vk.com/rustepik", R.drawable.ic_login_social_vk),
-    facebook("https://www.facebook.com/rustepik/", R.drawable.ic_login_social_fb),
-    instagram("https://www.instagram.com/stepik.education/", R.drawable.ic_social_instagram)
+    VK("https://vk.com/rustepik", R.drawable.ic_login_social_vk),
+    FACEBOOK("https://www.facebook.com/rustepik/", R.drawable.ic_login_social_fb),
+    INSTAGRAM("https://www.instagram.com/stepik.education/", R.drawable.ic_social_instagram)
 }
