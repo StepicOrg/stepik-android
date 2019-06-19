126aa276a8ba44558face4bcb27f9fc51ecd25a7
Ruslan Davletshin <smartmorefeed@gmail.com>
2019-06-19 15:51:55 +0300

    remove video step view

--- app/src/main/java/org/stepic/droid/ui/fragments/VideoStepFragment.kt
+++ app/src/main/java/org/stepic/droid/ui/fragments/VideoStepFragment.kt
@@ -5,7 +5,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.DataSource
 import com.bumptech.glide.load.engine.GlideException
@@ -15,24 +14,15 @@ import kotlinx.android.synthetic.main.fragment_video_step.*
 import kotlinx.android.synthetic.main.player_placeholder.*
 import kotlinx.android.synthetic.main.view_length_video_thumbnail.*
 import org.stepic.droid.R
-import org.stepic.droid.analytic.Analytic
 import org.stepic.droid.base.App
 import org.stepic.droid.base.StepBaseFragment
 import org.stepic.droid.core.presenters.VideoLengthPresenter
-import org.stepic.droid.core.presenters.VideoStepPresenter
 import org.stepic.droid.core.presenters.contracts.VideoLengthView
-import org.stepic.droid.core.presenters.contracts.VideoStepView
 import org.stepic.droid.util.ThumbnailParser
-import org.stepik.android.model.Video
-import org.stepik.android.view.video_player.model.VideoPlayerMediaData
 import javax.inject.Inject
 
 class VideoStepFragment : StepBaseFragment(),
-        VideoStepView,
         VideoLengthView {
-
-    @Inject
-    lateinit var videoStepPresenter: VideoStepPresenter
 
     @Inject
     lateinit var videoLengthPresenter: VideoLengthPresenter
@@ -50,14 +40,11 @@ class VideoStepFragment : StepBaseFragment(),
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        videoStepPresenter.attachView(this)
         videoLengthPresenter.attachView(this)
 
-        videoStepPresenter.initVideo(stepWrapper)
 
         playerLayout.setOnClickListener {
             playerLayout?.isClickable = false
-            videoStepPresenter.playVideo(stepWrapper)
         }
     }
 
@@ -66,7 +53,6 @@ class VideoStepFragment : StepBaseFragment(),
 
     override fun onDestroyView() {
         videoLengthPresenter.detachView(this)
-        videoStepPresenter.detachView(this)
         playerLayout.setOnClickListener(null)
         super.onDestroyView()
     }
@@ -103,31 +89,6 @@ class VideoStepFragment : StepBaseFragment(),
             videoLength?.visibility = View.VISIBLE
             videoLength?.text = it
         }
-    }
-
-    override fun onNeedOpenVideo(videoId: Long, cachedVideo: Video?, externalVideo: Video?) {
-        playerLayout.isClickable = true
-        screenManager.showVideo(activity, VideoPlayerMediaData(
-            thumbnail     = cachedVideo?.thumbnail ?: externalVideo?.thumbnail,
-            title         = lesson?.title ?: videoId.toString(),
-            cachedVideo   = cachedVideo,
-            externalVideo = externalVideo
-        ))
-    }
-
-    override fun onVideoLoaded(thumbnailPath: String?, cachedVideo: Video?, externalVideo: Video?) {
-        //show thumbnail and show length should be synchronized event, because we do not show thumbnail now, only after fetching length
-        val video = cachedVideo ?: externalVideo
-        if (video != null) {
-            videoLengthPresenter.fetchLength(video, thumbnailPath)
-        } else {
-            analytic.reportEvent(Analytic.Error.NO_VIDEO_ON_STEP_SHOWING)
-        }
-    }
-
-    override fun onInternetProblem() {
-        playerLayout.isClickable = true
-        Toast.makeText(context, R.string.sync_problem, Toast.LENGTH_SHORT).show()
     }
 
     override fun onVideoLengthDetermined(presentationString: String, thumbnail: String?) {
