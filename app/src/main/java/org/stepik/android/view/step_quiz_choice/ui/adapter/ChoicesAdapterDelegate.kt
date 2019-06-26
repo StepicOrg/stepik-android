ab296f9656078d746134eda7da23602df4d2e48d
Rostislav Smirnov <rostislavsmirnoff@gmail.com>
2019-06-26 11:48:55 +0300

    highlighting chosen and not chosen options

--- app/src/main/java/org/stepik/android/view/step_quiz_choice/ui/adapter/ChoicesAdapterDelegate.kt
+++ app/src/main/java/org/stepik/android/view/step_quiz_choice/ui/adapter/ChoicesAdapterDelegate.kt
@@ -1,9 +1,12 @@
 package org.stepik.android.view.step_quiz_choice.ui.adapter
 
+import android.graphics.drawable.GradientDrawable
+import android.support.v4.content.ContextCompat
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import org.stepic.droid.R
+import org.stepic.droid.util.DpPixelsHelper
 import ru.nobird.android.ui.adapterdelegatessupport.AdapterDelegate
 import ru.nobird.android.ui.adapterdelegatessupport.DelegateViewHolder
 import ru.nobird.android.ui.adapterssupport.selection.SelectionHelper
@@ -27,9 +30,17 @@ class ChoicesAdapterDelegate(
         }
 
         override fun onBind(data: String) {
-            val choiceText = itemView.findViewById(R.id.item_choice_text) as TextView
+            itemView as TextView
             itemView.isSelected = selectionHelper.isSelected(adapterPosition)
-            choiceText.text = data
+            itemView.text = data
+            val shape = itemView.background as GradientDrawable
+            if (itemView.isSelected) {
+                shape.setColor(ContextCompat.getColor(itemView.context, R.color.choice_checked))
+                shape.setStroke(DpPixelsHelper.convertDpToPixel(1f).toInt(), ContextCompat.getColor(itemView.context, R.color.choice_checked_border))
+            } else {
+                shape.setColor(ContextCompat.getColor(itemView.context, R.color.choice_not_checked))
+                shape.setStroke(DpPixelsHelper.convertDpToPixel(1f).toInt(), ContextCompat.getColor(itemView.context, R.color.choice_not_checked_border))
+            }
         }
     }
 }
