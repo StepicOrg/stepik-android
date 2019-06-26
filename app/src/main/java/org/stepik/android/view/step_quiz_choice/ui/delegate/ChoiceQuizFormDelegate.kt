fa933f04a83dd0e13c60c09a07c99acce6cbd468
Rostislav Smirnov <rostislavsmirnoff@gmail.com>
2019-06-26 11:49:39 +0300

    implement choice quiz form delegate

--- app/src/main/java/org/stepik/android/view/step_quiz_choice/ui/delegate/ChoiceQuizFormDelegate.kt
+++ app/src/main/java/org/stepik/android/view/step_quiz_choice/ui/delegate/ChoiceQuizFormDelegate.kt
@@ -3,17 +3,20 @@ package org.stepik.android.view.step_quiz_choice.ui.delegate
 import android.support.v7.widget.LinearLayoutManager
 import android.view.View
 import kotlinx.android.synthetic.main.view_choice_quiz_attempt.view.*
+import org.stepik.android.model.Reply
 import org.stepik.android.model.Submission
 import org.stepik.android.model.attempts.Attempt
 import org.stepik.android.view.step_quiz_choice.ui.adapter.ChoicesAdapterDelegate
 import ru.nobird.android.ui.adapterssupport.DefaultDelegateAdapter
 import ru.nobird.android.ui.adapterssupport.selection.MultipleChoiceSelectionHelper
+import ru.nobird.android.ui.adapterssupport.selection.SelectionHelper
 import ru.nobird.android.ui.adapterssupport.selection.SingleChoiceSelectionHelper
 
 class ChoiceQuizFormDelegate(
     private val choiceAttemptView: View
 ) : StepQuizFormDelegate() {
     private var choicesAdapter: DefaultDelegateAdapter<String> = DefaultDelegateAdapter()
+    private lateinit var selectionHelper: SelectionHelper
 
     init {
         choiceAttemptView.choices_recycler.apply {
@@ -22,27 +25,53 @@ class ChoiceQuizFormDelegate(
         }
     }
 
-    override var isEnabled: Boolean = false
-        set(value) {
-            field = value
-            choiceAttemptView.isEnabled = value
-        }
+    override var isEnabled: Boolean = true
 
     override fun setAttempt(attempt: Attempt?) {
         val dataSet = attempt?.dataset
         dataSet?.options?.let { options ->
             choicesAdapter.items = options
-            val selectionHelper = if (dataSet.isMultipleChoice) {
-                SingleChoiceSelectionHelper(choicesAdapter)
-            } else {
+            selectionHelper = if (dataSet.isMultipleChoice) {
                 MultipleChoiceSelectionHelper(choicesAdapter)
+            } else {
+                SingleChoiceSelectionHelper(choicesAdapter)
             }
-            choicesAdapter += ChoicesAdapterDelegate(selectionHelper) { selectionHelper.select(choicesAdapter.items.indexOf(it)) }
+            choicesAdapter += ChoicesAdapterDelegate(selectionHelper, onClick = ::handleChoiceClick)
         }
-
     }
 
     override fun setSubmission(submission: Submission?) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        submission?.reply?.choices?.let { setChoices(it)}
     }
+
+    private fun handleChoiceClick(choice: String) {
+        if (!isEnabled) return
+        when (selectionHelper) {
+            is SingleChoiceSelectionHelper -> {
+                selectionHelper.reset()
+                selectionHelper.select(choicesAdapter.items.indexOf(choice))
+                choicesAdapter.notifyDataSetChanged()
+            }
+            is MultipleChoiceSelectionHelper -> {
+                selectionHelper.toggle(choicesAdapter.items.indexOf(choice))
+            }
+        }
+    }
+
+    private fun setChoices(choices: List<Boolean>) {
+        (0 until choices.size).forEach {pos ->
+            if (choices[pos]) {
+                selectionHelper.select(pos)
+            }
+        }
+    }
+
+    val reply: Reply
+        get() {
+            val selection = (0 until choicesAdapter.itemCount)
+                .map {
+                    selectionHelper.isSelected(it)
+                }
+            return Reply(choices = selection)
+        }
 }
