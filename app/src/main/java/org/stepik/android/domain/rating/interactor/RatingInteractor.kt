f79e8ee03e2fc774768cc11d877ddd2c86501c36
Rostislav Smirnov <rostislavsmirnoff@gmail.com>
2019-07-01 16:18:12 +0300

    rating interactor draft

---
+++ app/src/main/java/org/stepik/android/domain/rating/interactor/RatingInteractor.kt
@@ -0,0 +1,41 @@
+package org.stepik.android.domain.rating.interactor
+
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig
+import org.stepic.droid.configuration.RemoteConfig
+import org.stepic.droid.preferences.SharedPreferenceHelper
+import org.stepic.droid.util.DateTimeHelper
+import javax.inject.Inject
+
+class RatingInteractor
+@Inject
+constructor(
+    private val sharedPreferenceHelper: SharedPreferenceHelper,
+    private val firebaseRemoteConfig: FirebaseRemoteConfig
+) {
+    companion object {
+        private const val MAX_RATE_TIMES = 5
+        private const val MIN_SOLVED_STEPS_FOR_RATING = 5
+    }
+
+    private fun isRateDelayGreater(): Boolean {
+        val wasShownMillis = sharedPreferenceHelper.whenRateWasShown()
+        if (wasShownMillis < 0) {
+            return true
+        }
+
+        val delayMillis = firebaseRemoteConfig.getLong(RemoteConfig.MIN_DELAY_RATE_DIALOG_SEC).toInt() * 1000L // TODO Discuss remote config field
+
+        return DateTimeHelper.isBeforeNowUtc(delayMillis + wasShownMillis) //if delay is expired (before now) -> show
+    }
+
+    private fun isRateWasShownFewTimes(): Boolean {
+        val wasShown = sharedPreferenceHelper.howManyRateWasShownBefore()
+        return wasShown <= MAX_RATE_TIMES
+    }
+
+    private fun isUserSolveEnough(): Boolean {
+        val numberOfSolved = sharedPreferenceHelper.numberOfSolved()
+        return numberOfSolved >= MIN_SOLVED_STEPS_FOR_RATING
+    }
+
+}
