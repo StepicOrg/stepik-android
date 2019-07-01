f90fece192d7ccc994ee4b362511651badd1231c
Ruslan Davletshin <smartmorefeed@gmail.com>
2019-07-01 11:32:35 +0300

    add quiz description

--- app/src/main/java/org/stepik/android/view/step_quiz_text/ui/delegate/TextStepQuizFormDelegate.kt
+++ app/src/main/java/org/stepik/android/view/step_quiz_text/ui/delegate/TextStepQuizFormDelegate.kt
@@ -6,6 +6,7 @@ import android.widget.TextView
 import kotlinx.android.synthetic.main.fragment_step_quiz_text.view.*
 import org.stepic.droid.R
 import org.stepic.droid.persistence.model.StepPersistentWrapper
+import org.stepic.droid.ui.util.changeVisibility
 import org.stepic.droid.util.AppConstants
 import org.stepik.android.model.Reply
 import org.stepik.android.presentation.step_quiz_text.TextStepQuizView
@@ -19,23 +20,31 @@ class TextStepQuizFormDelegate(
     private val stepQuizFormMapper = StepQuizFormMapper()
 
     private val textField = containerView.stringStepQuizField as TextView
+    private val quizDescription = containerView.stringStepQuizDescription
 
     init {
         when (stepWrapper.step.block?.name) {
             AppConstants.TYPE_STRING -> {
                 textField.inputType = InputType.TYPE_CLASS_TEXT
+                quizDescription.setText(R.string.step_quiz_string_description)
+                quizDescription.changeVisibility(true)
             }
 
             AppConstants.TYPE_NUMBER -> {
                 textField.inputType = InputType.TYPE_CLASS_NUMBER
+                quizDescription.setText(R.string.step_quiz_number_description)
+                quizDescription.changeVisibility(true)
             }
 
             AppConstants.TYPE_MATH -> {
                 textField.inputType = InputType.TYPE_CLASS_TEXT
+                quizDescription.setText(R.string.step_quiz_math_description)
+                quizDescription.changeVisibility(true)
             }
 
             AppConstants.TYPE_FREE_ANSWER -> {
                 textField.inputType = InputType.TYPE_CLASS_TEXT
+                quizDescription.changeVisibility(false)
             }
         }
     }
