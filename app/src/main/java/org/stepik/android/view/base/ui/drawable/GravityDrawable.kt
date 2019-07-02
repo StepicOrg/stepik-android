ebe0e3c2f811b4b1043f6ad68ce08a7171606591
Ruslan Davletshin <smartmorefeed@gmail.com>
2019-07-02 15:26:47 +0300

    add text quiz status compound drawable

---
+++ app/src/main/java/org/stepik/android/view/base/ui/drawable/GravityDrawable.kt
@@ -0,0 +1,47 @@
+package org.stepik.android.view.base.ui.drawable
+
+import android.annotation.SuppressLint
+import android.content.res.Resources
+import android.graphics.Canvas
+import android.graphics.ColorFilter
+import android.graphics.drawable.Drawable
+import android.support.annotation.Px
+import android.view.Gravity
+
+class GravityDrawable(
+    private val drawable: Drawable,
+    private val gravity: Int,
+    @Px
+    private val lineHeight: Int
+) : Drawable() {
+    init {
+        drawable.setBounds(0, 0, drawable.intrinsicWidth, drawable.intrinsicHeight)
+        setBounds(0, 0, drawable.intrinsicWidth, drawable.intrinsicHeight)
+    }
+
+    override fun getOpacity(): Int =
+        drawable.opacity
+
+    override fun setColorFilter(colorFilter: ColorFilter?) {
+        drawable.colorFilter = colorFilter
+    }
+
+    override fun setAlpha(alpha: Int) {
+        drawable.alpha = alpha
+    }
+
+    override fun getIntrinsicHeight(): Int =
+        drawable.intrinsicHeight
+
+    override fun getIntrinsicWidth(): Int =
+        drawable.intrinsicWidth
+
+    @SuppressLint("CanvasSize")
+    override fun draw(canvas: Canvas) {
+        canvas.save()
+        val dy = canvas.height / 2f - lineHeight / 2
+        canvas.translate(0f, if (gravity == Gravity.BOTTOM) dy else -dy)
+        drawable.draw(canvas)
+        canvas.restore()
+    }
+}
--- app/src/main/java/org/stepik/android/view/step_quiz_text/ui/delegate/TextStepQuizFormDelegate.kt
+++ app/src/main/java/org/stepik/android/view/step_quiz_text/ui/delegate/TextStepQuizFormDelegate.kt
@@ -1,15 +1,21 @@
 package org.stepik.android.view.step_quiz_text.ui.delegate
 
+import android.support.annotation.DrawableRes
+import android.support.v4.view.ViewCompat
+import android.support.v4.widget.TextViewCompat
+import android.support.v7.content.res.AppCompatResources
 import android.text.InputType
+import android.view.Gravity
 import android.view.View
 import android.widget.TextView
 import kotlinx.android.synthetic.main.fragment_step_quiz_text.view.*
 import org.stepic.droid.R
 import org.stepic.droid.persistence.model.StepPersistentWrapper
-import org.stepic.droid.ui.util.changeVisibility
 import org.stepic.droid.util.AppConstants
 import org.stepik.android.model.Reply
+import org.stepik.android.model.Submission
 import org.stepik.android.presentation.step_quiz.StepQuizView
+import org.stepik.android.view.base.ui.drawable.GravityDrawable
 import org.stepik.android.view.step_quiz.mapper.StepQuizFormMapper
 import org.stepik.android.view.step_quiz.ui.delegate.StepQuizFormDelegate
 
@@ -26,7 +32,7 @@ class TextStepQuizFormDelegate(
         val (inputType, textRes) =
             when (val blockName = stepWrapper.step.block?.name) {
                 AppConstants.TYPE_STRING ->
-                    InputType.TYPE_CLASS_TEXT to R.string.step_quiz_string_description
+                    InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_FLAG_MULTI_LINE to R.string.step_quiz_string_description
 
                 AppConstants.TYPE_NUMBER ->
                     InputType.TYPE_CLASS_NUMBER to R.string.step_quiz_number_description
@@ -35,7 +41,7 @@ class TextStepQuizFormDelegate(
                     InputType.TYPE_CLASS_TEXT to R.string.step_quiz_math_description
 
                 AppConstants.TYPE_FREE_ANSWER ->
-                    InputType.TYPE_CLASS_TEXT to R.string.step_quiz_free_answer_description
+                    InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_FLAG_MULTI_LINE to R.string.step_quiz_free_answer_description
 
                 else ->
                     throw IllegalArgumentException("Unsupported block type = $blockName")
@@ -58,11 +64,32 @@ class TextStepQuizFormDelegate(
     override fun setState(state: StepQuizView.State) {
         if (state !is StepQuizView.State.AttemptLoaded) return
 
-        textField.isEnabled = stepQuizFormMapper.isQuizEnabled(state)
-        textField.text = (state.submissionState as? StepQuizView.SubmissionState.Loaded)
+        val submission = (state.submissionState as? StepQuizView.SubmissionState.Loaded)
             ?.submission
+
+        textField.isEnabled = stepQuizFormMapper.isQuizEnabled(state)
+        textField.text = submission
             ?.reply
             ?.text
             ?: ""
+
+        @DrawableRes
+        val drawableRes =
+            when (submission?.status) {
+                Submission.Status.CORRECT ->
+                    R.drawable.ic_step_quiz_text_correct
+
+                Submission.Status.WRONG ->
+                    R.drawable.ic_step_quiz_text_wrong
+
+                else ->
+                    null
+            }
+
+        val drawable = drawableRes
+            ?.let { AppCompatResources.getDrawable(textField.context, it) }
+            ?.let { GravityDrawable(it, Gravity.BOTTOM, textField.resources.getDimensionPixelSize(R.dimen.step_quiz_text_field_min_height)) }
+
+        TextViewCompat.setCompoundDrawablesRelativeWithIntrinsicBounds(textField, null, null, drawable, null)
     }
 }
