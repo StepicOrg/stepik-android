9f783d34ae5bb1a74827a82596f426e8119e4996
Ruslan Davletshin <smartmorefeed@gmail.com>
2019-07-02 12:34:44 +0300

    pass presenter to step quiz delegate

--- app/src/main/java/org/stepik/android/view/step_quiz/ui/delegate/StepQuizDelegate.kt
+++ app/src/main/java/org/stepik/android/view/step_quiz/ui/delegate/StepQuizDelegate.kt
@@ -1,18 +1,21 @@
 package org.stepik.android.view.step_quiz.ui.delegate
 
-import android.view.View
+import android.support.annotation.StringRes
+import android.widget.TextView
+import org.stepic.droid.R
 import org.stepik.android.model.Reply
+import org.stepik.android.model.Submission
+import org.stepik.android.presentation.step_quiz.StepQuizPresenter
 import org.stepik.android.presentation.step_quiz.StepQuizView
 import org.stepik.android.view.step_quiz.mapper.StepQuizFeedbackMapper
 import org.stepik.android.view.step_quiz.mapper.StepQuizFormMapper
 import org.stepik.android.view.step_quiz.model.StepQuizFeedbackState
-import timber.log.Timber
 
 class StepQuizDelegate(
     private val stepQuizFormDelegate: StepQuizFormDelegate,
     private val stepQuizFeedbackBlocksDelegate: StepQuizFeedbackBlocksDelegate,
-    private val submitButton: View,
-    private val onSubmitReply: (Reply) -> Unit
+    private val submitButton: TextView,
+    private val presenter: StepQuizPresenter
 ) {
     private val stepQuizFeedbackMapper = StepQuizFeedbackMapper()
     private val stepQuizFormMapper = StepQuizFormMapper()
@@ -24,7 +27,7 @@ class StepQuizDelegate(
     private fun trySubmitReply() {
         val validation = stepQuizFormDelegate.validateForm()
         if (validation == null) {
-            onSubmitReply(stepQuizFormDelegate.createReply())
+            presenter.createSubmission(stepQuizFormDelegate.createReply())
         } else {
             stepQuizFeedbackBlocksDelegate.setState(StepQuizFeedbackState.Validation(validation))
         }
@@ -34,8 +37,20 @@ class StepQuizDelegate(
         stepQuizFeedbackBlocksDelegate.setState(stepQuizFeedbackMapper.mapToStepQuizFeedbackState(state))
         stepQuizFormDelegate.setState(state)
 
-        Timber.d(state.toString())
-
         submitButton.isEnabled = stepQuizFormMapper.isQuizSubmitEnabled(state)
+
+        if (state is StepQuizView.State.AttemptLoaded) {
+            @StringRes
+            val submitButtonTextRes =
+                when ((state.submissionState as? StepQuizView.SubmissionState.Loaded)?.submission?.status) {
+                    Submission.Status.CORRECT,
+                    Submission.Status.WRONG ->
+                        R.string.step_quiz_submit_button_try_again
+
+                    else ->
+                        R.string.step_quiz_submit_button_action
+                }
+            submitButton.setText(submitButtonTextRes)
+        }
     }
 }
