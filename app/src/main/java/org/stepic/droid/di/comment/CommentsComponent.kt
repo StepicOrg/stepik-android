736b5d8792e76f30544ade4153de294efd9b1539
Ruslan Davletshin <smartmorefeed@gmail.com>
2019-07-05 23:07:37 +0300

    use comment interactor

--- app/src/main/java/org/stepic/droid/di/comment/CommentsComponent.kt
+++ app/src/main/java/org/stepic/droid/di/comment/CommentsComponent.kt
@@ -2,9 +2,13 @@ package org.stepic.droid.di.comment
 
 import dagger.Subcomponent
 import org.stepic.droid.ui.fragments.CommentsFragment
+import org.stepik.android.view.injection.comment.CommentDataModule
 
 @CommentsScope
-@Subcomponent(modules = arrayOf(CommentsModule::class))
+@Subcomponent(modules = [
+    CommentsModule::class,
+    CommentDataModule::class
+])
 interface CommentsComponent {
 
     @Subcomponent.Builder
