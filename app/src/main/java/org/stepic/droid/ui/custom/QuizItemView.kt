eeb458231884c81a855c3998e960fb09a6a140b5
Rostislav Smirnov <rostislavsmirnoff@gmail.com>
2019-07-08 15:28:57 +0300

    fix clicking outside of webview

--- app/src/main/java/org/stepic/droid/ui/custom/QuizItemView.kt
+++ app/src/main/java/org/stepic/droid/ui/custom/QuizItemView.kt
@@ -24,7 +24,8 @@ class QuizItemView @JvmOverloads constructor(
     private var startClickTime: Long = 0
 
     override fun dispatchTouchEvent(ev: MotionEvent?): Boolean {
-        val dispatched = super.dispatchTouchEvent(ev)
+        val webview = getChildAt(2) as ProgressLatexView
+        val dispatched = webview.webView.dispatchTouchEvent(ev)
         if (ev?.action == MotionEvent.ACTION_DOWN) {
             startClickTime = Calendar.getInstance().timeInMillis
         }
