426bc2a8af9f2d6d73ca6ced5491757a3ff32087
Rostislav Smirnov <rostislavsmirnoff@gmail.com>
2019-07-11 19:07:10 +0300

    redundant method removed

--- app/src/main/java/org/stepik/android/view/step_quiz_choice/mapper/ChoiceStepQuizOptionsMapper.kt
+++ app/src/main/java/org/stepik/android/view/step_quiz_choice/mapper/ChoiceStepQuizOptionsMapper.kt
@@ -3,7 +3,6 @@ package org.stepik.android.view.step_quiz_choice.mapper
 import org.stepik.android.model.Submission
 import org.stepik.android.model.feedback.ChoiceFeedback
 import org.stepik.android.view.step_quiz_choice.model.Choice
-import ru.nobird.android.ui.adapterssupport.selection.SelectionHelper
 
 class ChoiceStepQuizOptionsMapper {
     fun mapChoices(options: List<String>, choices: List<Boolean>?, submission: Submission?, isQuizEnabled: Boolean): List<Choice> {
@@ -27,14 +26,5 @@ class ChoiceStepQuizOptionsMapper {
                     isEnabled = isQuizEnabled
                 )
             }
-    }
-
-    fun mapSelections(choices: List<Boolean>?, selectionHelper: SelectionHelper) {
-        if (choices == null) return
-        choices.forEachIndexed { index, choice ->
-            if (choice) {
-                selectionHelper.select(index)
-            }
-        }
     }
 }
