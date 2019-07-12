c05baf20d002f06d5f21cf8fd15531cbbed01b21
Rostislav Smirnov <rostislavsmirnoff@gmail.com>
2019-07-12 13:46:16 +0300

    clean up domain, data and di related to comment banner

--- app/src/main/java/org/stepic/droid/di/step/StepComponent.kt
+++ app/src/main/java/org/stepic/droid/di/step/StepComponent.kt
@@ -6,11 +6,10 @@ import org.stepic.droid.di.comment.CommentsComponent
 import org.stepic.droid.di.step.code.CodeComponent
 import org.stepic.droid.di.streak.StreakModule
 import org.stepic.droid.ui.fragments.StepAttemptFragment
-import org.stepik.android.view.injection.comment_banner.CommentBannerDataModule
 import org.stepik.android.view.injection.feedback.FeedbackDataModule
 
 @StepScope
-@Subcomponent(modules = arrayOf(StreakModule::class, CommentCountModule::class, CommentBannerDataModule::class, FeedbackDataModule::class))
+@Subcomponent(modules = arrayOf(StreakModule::class, CommentCountModule::class, FeedbackDataModule::class))
 interface StepComponent {
     @Subcomponent.Builder
     interface Builder {
