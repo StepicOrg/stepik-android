b25f8ab3d75f43ca866d596afd5ea27120cb2570
Ruslan Davletshin <smartmorefeed@gmail.com>
2019-08-08 14:08:32 +0300

    add sorting item draft

--- app/src/main/java/org/stepik/android/view/step_quiz_sorting/ui/adapter/delegate/SortingOptionAdapterDelegate.kt
+++ app/src/main/java/org/stepik/android/view/step_quiz_sorting/ui/adapter/delegate/SortingOptionAdapterDelegate.kt
@@ -1,7 +1,9 @@
 package org.stepik.android.view.step_quiz_sorting.ui.adapter.delegate
 
+import android.support.v4.view.ViewCompat
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.item_step_quiz_sorting.view.*
 import org.stepic.droid.R
 import org.stepik.android.view.step_quiz_sorting.ui.model.SortingOption
 import ru.nobird.android.ui.adapterdelegatessupport.AdapterDelegate
@@ -12,7 +14,17 @@ class SortingOptionAdapterDelegate : AdapterDelegate<SortingOption, DelegateView
         true
 
     override fun onCreateViewHolder(parent: ViewGroup): DelegateViewHolder<SortingOption> =
-        ViewHolder(createView(parent, R.layout.item_step_quiz_choice))
+        ViewHolder(createView(parent, R.layout.item_step_quiz_sorting))
 
-    class ViewHolder(root: View): DelegateViewHolder<SortingOption>(root)
+    class ViewHolder(root: View): DelegateViewHolder<SortingOption>(root) {
+        private val stepQuizSortingOption = root.stepQuizSortingOption
+
+        init {
+            ViewCompat.setElevation(root, context.resources.getDimension(R.dimen.step_quiz_sorting_item_elevation))
+        }
+
+        override fun onBind(data: SortingOption) {
+            stepQuizSortingOption.text = data.option
+        }
+    }
 }
