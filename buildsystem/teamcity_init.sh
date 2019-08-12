f1fa915cd30df73084e239eae510bcfa2b72d1db
Ruslan Davletshin <smartmorefeed@gmail.com>
2019-08-12 16:41:42 +0300

    update deps

--- buildsystem/teamcity_init.sh
+++ buildsystem/teamcity_init.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+rm -rf /home/buildagent/.gradle/caches/transforms-1
+rm -rf /home/buildagent/.gradle/caches/modules-2
+rm -rf /home/buildagent/.gradle/caches/build-cache-1
+
 SDK_VERSION=`grep compileSdk dependencies.gradle | awk '{ print $3+0}'`
 SDK_DIR=$ANDROID_HOME/platforms/android-$SDK_VERSION
 TOOLS_VERSION=`grep buildTools dependencies.gradle | cut -d \' -f 2`
