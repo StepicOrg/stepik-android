8c0aa8cf57c32d0e13290899467f772a6fe2368d
Rostislav Smirnov <rostislavsmirnoff@gmail.com>
2019-08-13 13:46:43 +0300

    data source in certificates repository

--- app/src/main/java/org/stepik/android/data/certificates/repository/CertificatesRepositoryImpl.kt
+++ app/src/main/java/org/stepik/android/data/certificates/repository/CertificatesRepositoryImpl.kt
@@ -1,7 +1,10 @@
 package org.stepik.android.data.certificates.repository
 
 import io.reactivex.Single
+import org.stepic.droid.util.doCompletableOnSuccess
+import org.stepik.android.data.certificates.source.CertificatesCacheDataSource
 import org.stepik.android.data.certificates.source.CertificatesRemoteDataSource
+import org.stepik.android.domain.base.DataSourceType
 import org.stepik.android.domain.certificates.repository.CertificatesRepository
 import org.stepik.android.model.Certificate
 import javax.inject.Inject
@@ -9,8 +12,26 @@ import javax.inject.Inject
 class CertificatesRepositoryImpl
 @Inject
 constructor(
+    private val certificatesCacheDataSource: CertificatesCacheDataSource,
     private val certificatesRemoteDataSource: CertificatesRemoteDataSource
 ) : CertificatesRepository {
-    override fun getCertificates(userId: Long): Single<List<Certificate>> =
-        certificatesRemoteDataSource.getCertificates(userId)
+    override fun getCertificates(userId: Long, primarySourceType: DataSourceType): Single<List<Certificate>> {
+        val remoteSource = certificatesRemoteDataSource
+            .getCertificates(userId)
+            .doCompletableOnSuccess(certificatesCacheDataSource::saveCertificates)
+
+        val cacheSource = certificatesCacheDataSource
+            .getCertificates(userId)
+
+        return when (primarySourceType) {
+            DataSourceType.REMOTE ->
+                remoteSource
+
+            DataSourceType.CACHE ->
+                cacheSource
+
+            else ->
+                throw IllegalArgumentException("Unsupported source type = $primarySourceType")
+        }
+    }
 }
--- app/src/main/java/org/stepik/android/domain/certificates/repository/CertificatesRepository.kt
+++ app/src/main/java/org/stepik/android/domain/certificates/repository/CertificatesRepository.kt
@@ -1,8 +1,9 @@
 package org.stepik.android.domain.certificates.repository
 
 import io.reactivex.Single
+import org.stepik.android.domain.base.DataSourceType
 import org.stepik.android.model.Certificate
 
 interface CertificatesRepository {
-    fun getCertificates(userId: Long): Single<List<Certificate>>
+    fun getCertificates(userId: Long, primarySourceType: DataSourceType = DataSourceType.REMOTE): Single<List<Certificate>>
 }
