1f1d8497c0c04bfc118763b5d782892b582926c7
Ruslan Davletshin <smartmorefeed@gmail.com>
2019-09-16 21:03:24 +0300

    clean constants

--- app/src/main/java/org/stepic/droid/util/RWLocks.java
+++ app/src/main/java/org/stepic/droid/util/RWLocks.java
@@ -5,8 +5,5 @@ import java.util.concurrent.locks.ReentrantReadWriteLock;
 public class RWLocks {
     public static final ReentrantReadWriteLock AuthLock = new ReentrantReadWriteLock();
     public static final ReentrantReadWriteLock DatabaseLock = new ReentrantReadWriteLock();
-    public static final ReentrantReadWriteLock DownloadLock = new ReentrantReadWriteLock();
-    public static final ReentrantReadWriteLock CancelLock = new ReentrantReadWriteLock();
     public static final ReentrantReadWriteLock ClearEnrollmentsLock = new ReentrantReadWriteLock();
-    public static final ReentrantReadWriteLock SectionCancelLock = new ReentrantReadWriteLock();
 }
