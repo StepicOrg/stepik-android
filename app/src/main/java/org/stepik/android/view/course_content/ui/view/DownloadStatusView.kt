a75e5b7354b3105c9f1164adf015839372ffe306
Ruslan Davletshin <smartmorefeed@gmail.com>
2019-09-19 18:16:38 +0300

    refactor download status view

--- app/src/main/java/org/stepik/android/view/course_content/ui/view/DownloadStatusView.kt
+++ app/src/main/java/org/stepik/android/view/course_content/ui/view/DownloadStatusView.kt
@@ -3,13 +3,14 @@ package org.stepik.android.view.course_content.ui.view
 import android.content.Context
 import android.util.AttributeSet
 import android.view.LayoutInflater
-import android.view.View
 import android.widget.FrameLayout
 import android.widget.ProgressBar
+import android.widget.TextView
 import kotlinx.android.synthetic.main.view_download_status.view.*
 import org.stepic.droid.R
 import org.stepic.droid.persistence.model.DownloadProgress
-import org.stepic.droid.ui.util.hideAllChildren
+import org.stepic.droid.ui.util.setCompoundDrawables
+import org.stepic.droid.util.TextUtil
 import org.stepik.android.view.ui.delegate.ViewStateDelegate
 
 class DownloadStatusView
@@ -22,9 +23,8 @@ constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
 
             isEnabled = status !is DownloadProgress.Status.Pending
             when (value) {
-                is DownloadProgress.Status.Cached -> {
-                    // show mb
-                }
+                is DownloadProgress.Status.Cached ->
+                    statusCached.text = TextUtil.formatBytes(value.bytesTotal)
 
                 is DownloadProgress.Status.InProgress ->
                     statusProgress.progress = (value.progress * statusProgress.max).toInt()
@@ -32,15 +32,20 @@ constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
         }
 
     private val statusProgress: ProgressBar
+    private val statusCached: TextView
 
     private val viewStateDelegate = ViewStateDelegate<DownloadProgress.Status>()
 
     init {
         val view = LayoutInflater.from(context).inflate(R.layout.view_download_status, this, true)
+        statusCached = view.statusCached
+
         viewStateDelegate.addState<DownloadProgress.Status.NotCached>(view.statusNotCached)
-        viewStateDelegate.addState<DownloadProgress.Status.Cached>(view.statusCached)
+        viewStateDelegate.addState<DownloadProgress.Status.Cached>(statusCached)
         viewStateDelegate.addState<DownloadProgress.Status.Pending>(view.statusPending)
         viewStateDelegate.addState<DownloadProgress.Status.InProgress>(view.statusInProgress)
+
+        statusCached.setCompoundDrawables(top = R.drawable.ic_download_remove)
 
         statusProgress = view.statusProgress
     }
