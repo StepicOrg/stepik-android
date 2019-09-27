8681e547b77f3111c18d14aa3946d16a1fb723b3
Rostislav Smirnov <rostislavsmirnoff@gmail.com>
2019-09-27 14:43:19 +0300

    rounded drawable for downloaded courses

--- app/src/main/java/org/stepik/android/view/download/ui/adapter/DownloadedCoursesAdapterDelegate.kt
+++ app/src/main/java/org/stepik/android/view/download/ui/adapter/DownloadedCoursesAdapterDelegate.kt
@@ -1,10 +1,14 @@
 package org.stepik.android.view.download.ui.adapter
 
+import android.graphics.BitmapFactory
+import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory
 import android.view.View
 import android.view.ViewGroup
 import com.bumptech.glide.Glide
+import com.bumptech.glide.request.target.BitmapImageViewTarget
 import kotlinx.android.synthetic.main.downloaded_course_item.view.*
 import org.stepic.droid.R
+import org.stepic.droid.ui.util.RoundedBitmapImageViewTarget
 import org.stepik.android.model.Course
 import ru.nobird.android.ui.adapterdelegatessupport.AdapterDelegate
 import ru.nobird.android.ui.adapterdelegatessupport.DelegateViewHolder
@@ -23,8 +27,22 @@ class DownloadedCoursesAdapterDelegate(
         private val downloadedCourseTitle = root.downloadedCourseName
         private val downloadedCourseImage = root.downloadedCourseImage
 
+        private val coursePlaceholderBitmap = BitmapFactory.decodeResource(
+            context.resources,
+            R.drawable.general_placeholder
+        )
+
+        private val circularBitmapDrawable = RoundedBitmapDrawableFactory.create(
+            context.resources,
+            coursePlaceholderBitmap
+        )
+
+
+        private val imageViewTarget: BitmapImageViewTarget =  RoundedBitmapImageViewTarget(itemView.resources.getDimension(R.dimen.course_image_radius), downloadedCourseImage)
+
         init {
             root.setOnClickListener { onItemClick(itemData as Course) }
+            circularBitmapDrawable.cornerRadius = context.resources.getDimension(R.dimen.course_image_radius)
         }
 
         override fun onBind(data: Course) {
@@ -33,9 +51,9 @@ class DownloadedCoursesAdapterDelegate(
             Glide.with(context)
                 .asBitmap()
                 .load(data.cover)
-                .placeholder(R.drawable.general_placeholder)
+                .placeholder(circularBitmapDrawable)
                 .fitCenter()
-                .into(downloadedCourseImage)
+                .into(imageViewTarget)
         }
     }
 }
