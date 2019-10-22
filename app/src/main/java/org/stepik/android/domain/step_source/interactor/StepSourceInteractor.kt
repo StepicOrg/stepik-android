62aae588bcf32a7bd433f42f275f61fcf6223406
Ruslan Davletshin <smartmorefeed@gmail.com>
2019-10-22 17:10:30 +0300

    re fetch step after update

--- app/src/main/java/org/stepik/android/domain/step_source/interactor/StepSourceInteractor.kt
+++ app/src/main/java/org/stepik/android/domain/step_source/interactor/StepSourceInteractor.kt
@@ -1,6 +1,8 @@
 package org.stepik.android.domain.step_source.interactor
 
 import io.reactivex.Single
+import org.stepik.android.domain.base.DataSourceType
+import org.stepik.android.domain.step.repository.StepRepository
 import org.stepik.android.domain.step_source.repository.StepSourceRepository
 import org.stepik.android.model.Step
 import javax.inject.Inject
@@ -8,7 +10,8 @@ import javax.inject.Inject
 class StepSourceInteractor
 @Inject
 constructor(
-    private val stepSourceRepository: StepSourceRepository
+    private val stepSourceRepository: StepSourceRepository,
+    private val stepRepository: StepRepository
 ) {
     fun changeStepBlockText(step: Step, text: String): Single<Step> =
         stepSourceRepository
@@ -17,7 +20,9 @@ constructor(
                 stepSourceRepository
                     .saveStepSource(source.copy(block = source.block.copy(text = text)))
             }
-            .map { source ->
-                step.copy(block = source.block)
+            .flatMap {
+                stepRepository
+                    .getStep(step.id, primarySourceType = DataSourceType.REMOTE)
+                    .toSingle()
             }
 }
