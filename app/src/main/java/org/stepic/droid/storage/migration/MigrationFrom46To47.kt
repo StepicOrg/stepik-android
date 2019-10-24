49ef9be120c5271d0330c318c25c087369ccaf24
Ruslan Davletshin <smartmorefeed@gmail.com>
2019-10-24 19:15:26 +0300

    add discussion threads to step

--- app/src/main/java/org/stepic/droid/storage/migration/MigrationFrom46To47.kt
+++ app/src/main/java/org/stepic/droid/storage/migration/MigrationFrom46To47.kt
@@ -2,11 +2,16 @@ package org.stepic.droid.storage.migration
 
 import android.database.sqlite.SQLiteDatabase
 import org.stepik.android.cache.lesson.structure.DbStructureLesson
+import org.stepik.android.cache.step.structure.DbStructureStep
 
 object MigrationFrom46To47 : Migration {
     override fun migrate(db: SQLiteDatabase) {
         db.execSQL("""
             ALTER TABLE ${DbStructureLesson.TABLE_NAME} ADD COLUMN ${DbStructureLesson.Columns.ACTIONS} TEXT
+        """.trimIndent())
+
+        db.execSQL("""
+            ALTER TABLE ${DbStructureStep.TABLE_NAME} ADD COLUMN ${DbStructureStep.Column.DISCUSSION_THREADS} TEXT
         """.trimIndent())
     }
 }
