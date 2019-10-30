d730405873a30805075b184126f203d08d01663b
Rostislav Smirnov <rostislavsmirnoff@gmail.com>
2019-10-30 19:42:17 +0300

    use constants for table names and columns

--- app/src/main/java/org/stepik/android/cache/download/dao/DownloadedCoursesDaoImpl.kt
+++ app/src/main/java/org/stepik/android/cache/download/dao/DownloadedCoursesDaoImpl.kt
@@ -2,9 +2,14 @@ package org.stepik.android.cache.download.dao
 
 import android.content.ContentValues
 import android.database.Cursor
+import org.stepic.droid.persistence.model.PersistentState
+import org.stepic.droid.persistence.storage.structure.DBStructurePersistentState
 import org.stepic.droid.storage.dao.DaoBase
 import org.stepic.droid.storage.operations.DatabaseOperations
 import org.stepic.droid.util.getLong
+import org.stepik.android.cache.section.structure.DbStructureSection
+import org.stepik.android.cache.step.structure.DbStructureStep
+import org.stepik.android.cache.unit.structure.DbStructureUnit
 import javax.inject.Inject
 
 class DownloadedCoursesDaoImpl
@@ -26,7 +31,11 @@ constructor(databaseOperations: DatabaseOperations) : DaoBase<Long>(databaseOper
         cursor.getLong(Columns.COURSE)
 
     override fun getCourseIds(): List<Long> {
-        val sql = "SELECT course FROM section WHERE id IN (SELECT section FROM unit WHERE lesson IN (SELECT lesson_id FROM step WHERE id IN (SELECT id FROM persistent_state WHERE type = \'STEP\' AND state = \'CACHED\')))"
+        val sql = "SELECT ${DbStructureSection.Columns.COURSE} FROM ${DbStructureSection.TABLE_NAME} WHERE ${DbStructureSection.Columns.ID} " +
+                "IN (SELECT ${DbStructureUnit.Columns.SECTION} FROM ${DbStructureUnit.TABLE_NAME} WHERE ${DbStructureUnit.Columns.LESSON} " +
+                "IN (SELECT ${DbStructureStep.Column.LESSON_ID} FROM ${DbStructureStep.TABLE_NAME} WHERE ${DbStructureStep.Column.ID} " +
+                "IN (SELECT ${DBStructurePersistentState.Columns.ID} FROM ${DBStructurePersistentState.TABLE_NAME} WHERE " +
+                "${DBStructurePersistentState.Columns.TYPE} = \'${PersistentState.Type.STEP}\' AND ${DBStructurePersistentState.Columns.STATE} = \'${PersistentState.State.CACHED}\')))"
         return rawQuery(sql, null) {
             val res = ArrayList<Long>()
 
