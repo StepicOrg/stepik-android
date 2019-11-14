9a4f94613bf47242d828ba2170e829c1527e2721
Rostislav Smirnov <rostislavsmirnoff@gmail.com>
2019-11-14 14:37:09 +0300

    missing query annotation

--- app/src/main/java/org/stepik/android/remote/progress/service/ProgressService.kt
+++ app/src/main/java/org/stepik/android/remote/progress/service/ProgressService.kt
@@ -3,8 +3,9 @@ package org.stepik.android.remote.progress.service
 import io.reactivex.Single
 import org.stepik.android.remote.progress.model.ProgressResponse
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface ProgressService {
     @GET("api/progresses")
-    fun getProgressesReactive(progresses: Array<String?>): Single<ProgressResponse>
+    fun getProgressesReactive(@Query("ids[]") progresses: Array<String?>): Single<ProgressResponse>
 }
