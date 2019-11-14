8366cba2722ede2a44fc429a06a24a3828f51edc
Ruslan Davletshin <smartmorefeed@gmail.com>
2019-11-14 10:22:37 +0300

    preload attempts with step

--- app/src/main/java/org/stepic/droid/persistence/downloads/resolvers/structure/StepStructureResolverImpl.kt
+++ app/src/main/java/org/stepic/droid/persistence/downloads/resolvers/structure/StepStructureResolverImpl.kt
@@ -1,9 +1,12 @@
 package org.stepic.droid.persistence.downloads.resolvers.structure
 
+import io.reactivex.Completable
 import io.reactivex.Observable
 import io.reactivex.rxkotlin.toObservable
 import org.stepic.droid.persistence.di.PersistenceScope
 import org.stepic.droid.persistence.model.Structure
+import org.stepic.droid.util.maybeFirst
+import org.stepik.android.domain.attempt.repository.AttemptRepository
 import org.stepik.android.domain.progress.mapper.getProgresses
 import org.stepik.android.domain.progress.repository.ProgressRepository
 import org.stepik.android.domain.step.repository.StepRepository
@@ -14,7 +17,8 @@ class StepStructureResolverImpl
 @Inject
 constructor(
     private val stepRepository: StepRepository,
-    private val progressRepository: ProgressRepository
+    private val progressRepository: ProgressRepository,
+    private val attemptRepository: AttemptRepository
 ): StepStructureResolver {
     override fun resolveStructure(
         courseId: Long,
@@ -32,9 +36,20 @@ constructor(
                     }
                     .toObservable()
 
+                val attemptCompletable = Completable
+                    .concat(steps.map { resolveStepAttempt(it.id) })
+
                 progressRepository
                     .getProgresses(*steps.getProgresses())
                     .ignoreElement()
+                    .andThen(attemptCompletable)
                     .andThen(observables)
             }
+
+    private fun resolveStepAttempt(stepId: Long): Completable =
+        attemptRepository
+            .getAttemptsForStep(stepId)
+            .maybeFirst()
+            .switchIfEmpty(attemptRepository.createAttemptForStep(stepId))
+            .ignoreElement()
 }
