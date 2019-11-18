f462f0b315614f9ca22b8ca6d7521d12b26b75f0
Rostislav Smirnov <rostislavsmirnoff@gmail.com>
2019-11-18 18:02:01 +0300

    fix interop

--- app/src/main/java/org/stepik/android/remote/course/source/EnrollmentRemoteDataSourceImpl.kt
+++ app/src/main/java/org/stepik/android/remote/course/source/EnrollmentRemoteDataSourceImpl.kt
@@ -1,7 +1,6 @@
 package org.stepik.android.remote.course.source
 
 import io.reactivex.Completable
-import org.stepic.droid.configuration.Config
 import org.stepik.android.data.course.source.EnrollmentRemoteDataSource
 import org.stepik.android.model.Enrollment
 import org.stepik.android.remote.course.model.EnrollmentRequest
@@ -11,14 +10,11 @@ import javax.inject.Inject
 class EnrollmentRemoteDataSourceImpl
 @Inject
 constructor(
-    private val config: Config,
     private val enrollmentService: EnrollmentService
 ) : EnrollmentRemoteDataSource {
     override fun addEnrollment(courseId: Long): Completable =
         enrollmentService.joinCourse(EnrollmentRequest(Enrollment(courseId)))
 
-    override fun removeEnrollment(courseId: Long): Completable {
-        if (!config.isUserCanDropCourse) Completable.complete()
-        return enrollmentService.dropCourse(courseId)
-    }
+    override fun removeEnrollment(courseId: Long): Completable =
+        enrollmentService.dropCourse(courseId)
 }
--- app/src/main/java/org/stepik/android/remote/discussion_proxy/DiscussionProxyRemoteDataSourceImpl.kt
+++ app/src/main/java/org/stepik/android/remote/discussion_proxy/DiscussionProxyRemoteDataSourceImpl.kt
@@ -4,6 +4,7 @@ import io.reactivex.Single
 import io.reactivex.functions.Function
 import org.stepik.android.data.discussion_proxy.source.DiscussionProxyRemoteDataSource
 import org.stepik.android.model.comments.DiscussionProxy
+import org.stepik.android.remote.base.chunkedSingleMap
 import org.stepik.android.remote.discussion_proxy.model.DiscussionProxyResponse
 import org.stepik.android.remote.discussion_proxy.service.DiscussionProxyService
 import javax.inject.Inject
@@ -13,11 +14,13 @@ class DiscussionProxyRemoteDataSourceImpl
 constructor(
     private val discussionProxyService: DiscussionProxyService
 ) : DiscussionProxyRemoteDataSource {
-    private val discussionProxyResponseMapper = Function(DiscussionProxyResponse::discussionProxies)
+    private val discussionProxyResponseMapper =
+        Function<DiscussionProxyResponse, List<DiscussionProxy>>(DiscussionProxyResponse::discussionProxies)
 
-    @Suppress("UNCHECKED_CAST") // discussionProxyIds to Array<String> interop not working
     override fun getDiscussionProxies(vararg discussionProxyIds: String): Single<List<DiscussionProxy>> =
-        discussionProxyService
-            .getDiscussionProxies(discussionProxyIds as Array<String>)
-            .map(discussionProxyResponseMapper)
+        discussionProxyIds
+            .chunkedSingleMap {ids ->
+                discussionProxyService.getDiscussionProxies(ids)
+                    .map(discussionProxyResponseMapper)
+            }
 }
