c0a6b84a93ca6ee80ab4f79d83b630e3145060ac
Rostislav Smirnov <rostislavsmirnoff@gmail.com>
2019-12-06 19:21:57 +0300

    resolve icon through provider

--- app/src/main/java/org/stepik/android/view/profile_links/ui/delegate/ProfileLinksAdapterDelegate.kt
+++ app/src/main/java/org/stepik/android/view/profile_links/ui/delegate/ProfileLinksAdapterDelegate.kt
@@ -5,6 +5,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.widget.AppCompatTextView
 import org.stepic.droid.R
+import org.stepic.droid.ui.util.setCompoundDrawables
 import org.stepik.android.model.SocialProfile
 import ru.nobird.android.ui.adapterdelegates.AdapterDelegate
 import ru.nobird.android.ui.adapterdelegates.DelegateViewHolder
@@ -31,6 +32,21 @@ class ProfileLinksAdapterDelegate(
         override fun onBind(data: SocialProfile) {
             profileLinkTextView.movementMethod = LinkMovementMethod.getInstance()
             profileLinkTextView.text = data.name
+            profileLinkTextView.setCompoundDrawables(start = resolveProfileLinkDrawable(data.provider))
         }
     }
+
+    private fun resolveProfileLinkDrawable(provider: String): Int =
+        when (provider) {
+            "facebook" ->
+                R.drawable.ic_profile_fb
+            "instagram" ->
+                R.drawable.ic_profile_instagram
+            "twitter" ->
+                R.drawable.ic_profile_twitter
+            "vk" ->
+                R.drawable.ic_profile_vk
+            else ->
+                R.drawable.ic_profile_web
+        }
 }
