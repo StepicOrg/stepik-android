e857782ac8d6d3f51fdd6f4285fae1af00de8ddb
Ruslan Davletshin <smartmorefeed@gmail.com>
2019-12-09 20:28:52 +0300

    lint

--- app/src/main/java/org/stepik/android/cache/profile/ProfileCacheDataSourceImpl.kt
+++ app/src/main/java/org/stepik/android/cache/profile/ProfileCacheDataSourceImpl.kt
@@ -3,7 +3,6 @@ package org.stepik.android.cache.profile
 import io.reactivex.Completable
 import io.reactivex.Maybe
 import org.stepic.droid.preferences.SharedPreferenceHelper
-import org.stepic.droid.util.toMaybe
 import org.stepik.android.data.profile.source.ProfileCacheDataSource
 import org.stepik.android.model.user.Profile
 import javax.inject.Inject
