c39f3d34b8038e85d52e215cce5adeca66af1d4e
Rostislav Smirnov <rostislavsmirnoff@gmail.com>
2019-12-10 19:45:18 +0300

    add annotations to SocialAuthError

--- app/src/main/java/org/stepik/android/remote/auth/model/SocialAuthError.kt
+++ app/src/main/java/org/stepik/android/remote/auth/model/SocialAuthError.kt
@@ -1,7 +1,12 @@
 package org.stepik.android.remote.auth.model
 
+import com.google.gson.annotations.SerializedName
+
 class SocialAuthError(
+    @SerializedName("error")
     val error: String?,
+    @SerializedName("email")
     val email: String?,
+    @SerializedName("provider")
     val provider: String?
 )
