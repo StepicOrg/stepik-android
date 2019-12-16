ba201a2bb99c74ed7ba4241ae569215678e8e711
Ruslan Davletshin <smartmorefeed@gmail.com>
2019-12-16 20:11:59 +0300

    load all pages for profile courses

--- app/src/main/java/org/stepik/android/domain/course_list/interactor/CourseListInteractor.kt
+++ app/src/main/java/org/stepik/android/domain/course_list/interactor/CourseListInteractor.kt
@@ -1,7 +1,9 @@
 package org.stepik.android.domain.course_list.interactor
 
+import io.reactivex.Observable
 import io.reactivex.Single
 import org.stepic.droid.util.PagedList
+import org.stepic.droid.util.plus
 import org.stepik.android.domain.course_list.model.CourseListQuery
 import org.stepik.android.domain.course_list.repository.CourseListRepository
 import org.stepik.android.model.Course
@@ -13,6 +15,11 @@ constructor(
     private val courseListRepository: CourseListRepository
 ) {
     fun getCourseList(courseListQuery: CourseListQuery): Single<PagedList<Course>> =
-        courseListRepository
-            .getCourseList(courseListQuery)
+        Observable.range(1, Int.MAX_VALUE)
+            .concatMapSingle { page ->
+                courseListRepository
+                    .getCourseList(courseListQuery.copy(page = page))
+            }
+            .takeUntil { !it.hasNext }
+            .reduce(PagedList(emptyList())) { a, b -> a + b }
 }
