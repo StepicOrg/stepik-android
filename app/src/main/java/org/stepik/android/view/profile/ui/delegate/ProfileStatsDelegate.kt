93db57504a750e2a98e5516e742e8a8aad8e7a5d
Ruslan Davletshin <smartmorefeed@gmail.com>
2020-01-13 16:38:41 +0300

    add profile stat clicked analytics

--- app/src/main/java/org/stepik/android/view/profile/ui/delegate/ProfileStatsDelegate.kt
+++ app/src/main/java/org/stepik/android/view/profile/ui/delegate/ProfileStatsDelegate.kt
@@ -9,12 +9,15 @@ import androidx.core.text.buildSpannedString
 import androidx.core.view.isVisible
 import kotlinx.android.synthetic.main.header_profile.view.*
 import org.stepic.droid.R
+import org.stepic.droid.analytic.AmplitudeAnalytic
+import org.stepic.droid.analytic.Analytic
 import org.stepic.droid.ui.util.hideAllChildren
 import org.stepik.android.model.user.User
 import org.stepik.android.view.base.ui.span.TypefaceSpanCompat
 
 class ProfileStatsDelegate(
-    view: View
+    view: View,
+    private val analytic: Analytic
 ) {
     companion object {
         private const val MIN_REPUTATION_RANK = 1000
@@ -30,6 +33,16 @@ class ProfileStatsDelegate(
     private val profileCoursesPublished = view.profileCoursesPublished
     private val profileKnowledgeRank = view.profileKnowledgeRank
     private val profileReputationRank = view.profileReputationRank
+
+    init {
+        profileKnowledgeRank.setOnClickListener {
+            analytic.reportAmplitudeEvent(AmplitudeAnalytic.Profile.PROFILE_STAT_CLICKED, mapOf(AmplitudeAnalytic.Profile.Params.TYPE to AmplitudeAnalytic.Profile.Values.KNOWLEDGE))
+        }
+
+        profileReputationRank.setOnClickListener {
+            analytic.reportAmplitudeEvent(AmplitudeAnalytic.Profile.PROFILE_STAT_CLICKED, mapOf(AmplitudeAnalytic.Profile.Params.TYPE to AmplitudeAnalytic.Profile.Values.REPUTATION))
+        }
+    }
 
     fun setProfileStats(user: User) {
         profileStats.hideAllChildren()
