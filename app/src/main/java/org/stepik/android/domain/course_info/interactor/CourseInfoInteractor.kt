89cea9eac374d9ea738cb7ce6c3610f67cc7f147
Rostislav Smirnov <rostislavsmirnoff@gmail.com>
2020-01-15 22:29:41 +0300

    handle not issuing certificates

--- app/src/main/java/org/stepik/android/domain/course_info/interactor/CourseInfoInteractor.kt
+++ app/src/main/java/org/stepik/android/domain/course_info/interactor/CourseInfoInteractor.kt
@@ -57,7 +57,7 @@ constructor(
             instructors    = (instructors ?: course.instructors?.map { null })?.takeIf { it.isNotEmpty() },
             language       = course.language,
             certificate    = course.certificate
-                ?.takeIf { course.isCertificateAutoIssued }
+                ?.takeIf { course.isCertificateAutoIssued || course.isCertificateIssued }
                 ?.let {
                     CourseInfoData.Certificate(
                         title = it,
