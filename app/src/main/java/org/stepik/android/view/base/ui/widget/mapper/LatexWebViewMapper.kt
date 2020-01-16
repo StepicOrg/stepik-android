f62a2c9a0e2b31ca3e15dc2bc928ce8733e6e495
Ruslan Davletshin <smartmorefeed@gmail.com>
2020-01-16 20:57:25 +0300

    latex web view mapper

--- app/src/main/java/org/stepik/android/view/base/ui/widget/mapper/LatexWebViewMapper.kt
+++ app/src/main/java/org/stepik/android/view/base/ui/widget/mapper/LatexWebViewMapper.kt
@@ -1,5 +1,51 @@
 package org.stepik.android.view.base.ui.widget.mapper
 
-class LatexWebViewMapper {
+import android.content.Context
+import org.stepic.droid.configuration.Config
+import org.stepik.android.view.base.ui.widget.attributes.TextAttributes
+import org.stepik.android.view.base.ui.widget.block.BaseStyleBlock
+import org.stepik.android.view.base.ui.widget.block.ContentBlock
+import org.stepik.android.view.base.ui.widget.block.MetaBlock
+import org.stepik.android.view.base.ui.widget.block.SelectionColorStyleBlock
+import org.stepik.android.view.base.ui.widget.model.LatexData
 
+class LatexWebViewMapper(
+    private val config: Config,
+    private val context: Context
+) {
+    companion object {
+        private const val ASSETS = "file:///android_asset/"
+        private const val FONTS = "fonts/"
+        private const val EXTENSION = ".ttf"
+    }
+
+    fun mapLatexData(webData: LatexData.Web, attributes: TextAttributes, width: Int): String {
+        val fontPath = "$ASSETS$FONTS${context.resources.getResourceEntryName(attributes.fontResId)}$EXTENSION"
+
+        val blocks =
+            listOf(
+                BaseStyleBlock(fontPath),
+                MetaBlock(config.baseUrl, width),
+                SelectionColorStyleBlock(attributes.textColorHighlight)
+            )
+            .filter { it.isEnabled(webData.body) }
+
+        val header = webData.header + blocks.joinToString(separator = "", transform = ContentBlock::header)
+        val preBody = webData.preBody + blocks.joinToString(separator = "", transform = ContentBlock::preBody)
+        val postBody = webData.postBody + blocks.joinToString(separator = "", transform = ContentBlock::postBody)
+
+        return """
+            <!DOCTYPE html>
+            <html>
+            <head>
+                $header
+            </head>
+            <body style='margin:0;padding:0;'>
+                $preBody
+                ${webData.body}
+                $postBody
+            </body>
+            </html>
+        """.trimIndent()
+    }
 }
