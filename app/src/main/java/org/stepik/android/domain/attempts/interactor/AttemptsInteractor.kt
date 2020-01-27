b4a1870ba8244efbfd8f7cbb5cef2f58e6e13f05
Rostislav Smirnov <rostislavsmirnoff@gmail.com>
2020-01-27 12:06:16 +0300

    replace with rx

--- app/src/main/java/org/stepik/android/domain/attempts/interactor/AttemptsInteractor.kt
+++ app/src/main/java/org/stepik/android/domain/attempts/interactor/AttemptsInteractor.kt
@@ -1,17 +1,21 @@
 package org.stepik.android.domain.attempts.interactor
 
 import io.reactivex.Single
+import io.reactivex.rxkotlin.toObservable
 import org.stepic.droid.storage.dao.IDao
+import org.stepic.droid.util.mapToLongArray
 import org.stepik.android.cache.submission.structure.DbStructureSubmission
 import org.stepik.android.domain.base.DataSourceType
 import org.stepik.android.domain.lesson.repository.LessonRepository
 import org.stepik.android.domain.section.repository.SectionRepository
 import org.stepik.android.domain.step.repository.StepRepository
 import org.stepik.android.domain.unit.repository.UnitRepository
+import org.stepik.android.model.Lesson
+import org.stepik.android.model.Step
 import org.stepik.android.model.Submission
+import org.stepik.android.model.Unit
 import org.stepik.android.model.attempts.Attempt
 import org.stepik.android.view.attempts.model.AttemptCacheItem
-import timber.log.Timber
 import javax.inject.Inject
 
 class AttemptsInteractor
@@ -26,15 +30,18 @@ constructor(
 ) {
 
     // TODO Remove blocking gets
-    fun fetchAttemptCacheItems(): Single<List<AttemptCacheItem>> =
+    /* fun fetchAttemptCacheItems(): Single<List<AttemptCacheItem>> =
         Single.fromCallable {
             val attempts = attemptDao.getAll()
             val submissions = submissionDao.getAllInRange(DbStructureSubmission.Columns.ATTEMPT_ID, attempts.map { it.id }.joinToString()).filter { it.status == Submission.Status.LOCAL }
 
-            val steps = stepRepository.getSteps(*attempts.map { it.step }.toLongArray(), primarySourceType = DataSourceType.CACHE).blockingGet()
-            val lessons = lessonRepository.getLessons(*steps.map { it.lesson }.toLongArray(), primarySourceType = DataSourceType.CACHE).blockingGet()
+            stepRepository
+                .getSteps(*attempts.map { it.step }.toLongArray(), primarySourceType = DataSourceType.CACHE)
+
+            val steps = stepRepository.getSteps(*attempts.mapToLongArray { it.step }, primarySourceType = DataSourceType.CACHE).blockingGet()
+            val lessons = lessonRepository.getLessons(*steps.mapToLongArray { it.lesson }, primarySourceType = DataSourceType.CACHE).blockingGet()
             val units = lessons.flatMap { unitRepository.getUnitsByLessonId(it.id, primarySourceType = DataSourceType.CACHE).blockingGet() }
-            val sections = sectionRepository.getSections(*units.map { it.section }.toLongArray(), primarySourceType = DataSourceType.CACHE).blockingGet()
+            val sections = sectionRepository.getSections(*units.mapToLongArray { it.section }, primarySourceType = DataSourceType.CACHE).blockingGet()
 
             val items = submissions.map { submission ->
                 val attempt = attempts.find { it.id == submission.attempt }
@@ -74,4 +81,60 @@ constructor(
             }
             attemptCacheItems.toList()
         }
+     */
+
+    fun fetchAttemptCacheItems(): Single<List<AttemptCacheItem>> =
+        Single.fromCallable {
+            val attempts = attemptDao.getAll()
+            val submissions = submissionDao.getAllInRange(DbStructureSubmission.Columns.ATTEMPT_ID, attempts.map { it.id }.joinToString()).filter { it.status == Submission.Status.LOCAL }
+            Pair(attempts, submissions)
+        }.flatMap { (attempts, submissions) ->
+            getSteps(attempts.mapToLongArray { it.step }, attempts, submissions)
+        }
+
+    private fun getSteps(ids: LongArray, attempts: List<Attempt>, submissions: List<Submission>): Single<List<AttemptCacheItem>> =
+        stepRepository
+            .getSteps(*ids, primarySourceType = DataSourceType.CACHE)
+            .flatMap { steps -> getLessons(steps.mapToLongArray { it.lesson }, attempts, submissions, steps) }
+
+    private fun getLessons(ids: LongArray, attempts: List<Attempt>, submissions: List<Submission>, steps: List<Step>): Single<List<AttemptCacheItem>> =
+        lessonRepository
+            .getLessons(*ids, primarySourceType = DataSourceType.CACHE)
+            .flatMap { lessons -> lessons
+                .toObservable()
+                .flatMapSingle { lesson -> unitRepository.getUnitsByLessonId(lesson.id, primarySourceType = DataSourceType.CACHE) }
+                .reduce(emptyList<Unit>()) { a, b ->  a + b }
+                .flatMap { units -> getSections(units.mapToLongArray { it.section }, attempts, submissions, steps, lessons, units) }
+            }
+
+    private fun getSections(ids: LongArray, attempts: List<Attempt>, submissions: List<Submission>, steps: List<Step>, lessons: List<Lesson>, units: List<Unit>): Single<List<AttemptCacheItem>> =
+        sectionRepository
+            .getSections(*ids, primarySourceType = DataSourceType.CACHE)
+            .map { sections ->
+                val items = submissions.map { submission ->
+                    val attempt = attempts.find { it.id == submission.attempt }
+                    val lessonId = steps.find { it.id == attempt?.step }?.lesson
+                    val lesson = lessons.find { it.id == lessonId }
+
+                    val unit = units.find { it.lesson == lessonId }
+                    val section = sections.find { it.id == unit?.section }
+                    AttemptCacheItem.SubmissionItem(section = section!!, unit = unit!!, lesson = lesson!!, submission = submission, time = attempt?.time!!)
+                }
+
+                val lessonItems = items.map { AttemptCacheItem.LessonItem(it.section, it.unit, it.lesson) }.distinct().sortedBy { it.lesson.id }
+                val sectionItems = lessonItems.map { AttemptCacheItem.SectionItem(it.section) }.distinct().sortedBy { it.section.id }
+
+                val attemptCacheItems = mutableListOf<AttemptCacheItem>()
+
+                sectionItems.forEach { sectionItem ->
+                    attemptCacheItems.add(sectionItem)
+                    val lessonsBySection = lessonItems.filter { it.section == sectionItem.section }
+                    lessonsBySection.forEach { lessonItem ->
+                        attemptCacheItems.add(lessonItem)
+                        val subs = items.filter { it.lesson == lessonItem.lesson }
+                        attemptCacheItems += subs
+                    }
+                }
+                attemptCacheItems
+            }
 }
