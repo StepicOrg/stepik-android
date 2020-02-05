1263d431cb62f3bbfc612f0c6aca9cecfc9fde27
Ruslan Davletshin <smartmorefeed@gmail.com>
2020-02-06 01:57:37 +0300

    add remote config defaults + sync with amplitude

--- app/src/main/java/org/stepic/droid/configuration/RemoteConfig.kt
+++ app/src/main/java/org/stepic/droid/configuration/RemoteConfig.kt
@@ -1,6 +1,8 @@
 package org.stepic.droid.configuration
 
 object RemoteConfig {
+    const val PREFIX = "remote_config_"
+
     const val MIN_DELAY_RATE_DIALOG_SEC = "min_delay_rate_dialog_sec"
     const val SHOW_STREAK_DIALOG_AFTER_LOGIN = "show_streak_dialog_after_login"
     const val SHOW_NOTIFICATIONS_BADGES = "show_notifications_badges"
