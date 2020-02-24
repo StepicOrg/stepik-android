b1dfdf17c1bcd23c9291117f9bceea566aaee349
Rostislav Smirnov <rostislavsmirnoff@gmail.com>
2020-02-24 16:49:22 +0300

    use enum for thread container data

--- app/src/main/java/org/stepik/android/view/step/ui/delegate/StepDiscussionsDelegate.kt
+++ app/src/main/java/org/stepik/android/view/step/ui/delegate/StepDiscussionsDelegate.kt
@@ -1,13 +1,12 @@
 package org.stepik.android.view.step.ui.delegate
 
 import android.view.View
-import androidx.annotation.DrawableRes
 import androidx.core.view.isVisible
 import kotlinx.android.synthetic.main.fragment_step.view.*
 import kotlinx.android.synthetic.main.view_step_discussion.view.*
-import org.stepic.droid.R
 import org.stepic.droid.ui.util.setCompoundDrawables
 import org.stepik.android.model.comments.DiscussionThread
+import org.stepik.android.view.comment.model.DiscussionThreadContainer
 
 class StepDiscussionsDelegate(
     containerView: View,
@@ -44,43 +43,31 @@ class StepDiscussionsDelegate(
             val discussionsCount = discussionThread?.discussionsCount ?: 0
 
             when (discussionThread?.thread) {
-                DiscussionThread.THREAD_DEFAULT -> {
-                    val containerText =
-                        when {
-                            discussionProxy == null ->
-                                containerView.context.getString(R.string.comment_disabled)
+                DiscussionThread.THREAD_DEFAULT ->
+                    setDiscussionThreadData(discussionProxy, discussionsCount, DiscussionThreadContainer.DEFAULT)
 
-                            discussionsCount > 0 ->
-                                containerView.context.getString(R.string.step_discussion_show, discussionsCount)
+                DiscussionThread.THREAD_SOLUTIONS ->
+                    setDiscussionThreadData(discussionProxy, discussionsCount, DiscussionThreadContainer.SOLUTIONS)
 
-                            else ->
-                                containerView.context.getString(R.string.step_discussion_write_first)
-                        }
-                    setDiscussionThreadData(containerText, R.drawable.ic_step_discussion, discussionProxy != null)
-                }
-                DiscussionThread.THREAD_SOLUTIONS -> {
-                    val containerText =
-                        when {
-                            discussionProxy == null ->
-                                containerView.context.getString(R.string.solution_disabled)
-
-                            discussionsCount > 0 ->
-                                containerView.context.getString(R.string.step_solutions_show, discussionsCount)
-
-                            else ->
-                                containerView.context.getString(R.string.step_solutions_write_first)
-                        }
-                    setDiscussionThreadData(containerText, R.drawable.ic_step_solutions, discussionProxy != null)
-                }
                 else ->
                     containerView.isVisible = false
             }
         }
 
-        private fun setDiscussionThreadData(containerText: String, @DrawableRes containerDrawableId: Int, isContainerEnabled: Boolean) {
-            stepDiscussions.text = containerText
-            stepDiscussions.setCompoundDrawables(start = if (isContainerEnabled) containerDrawableId else -1)
-            containerView.isEnabled = isContainerEnabled
+        private fun setDiscussionThreadData(discussionProxy: String?, discussionsCount: Int, discussionThreadContainer: DiscussionThreadContainer) {
+            stepDiscussions.text =
+                when {
+                    discussionProxy == null ->
+                        containerView.context.getString(discussionThreadContainer.disabledStringRes)
+
+                    discussionsCount > 0 ->
+                        containerView.context.getString(discussionThreadContainer.showStringRes, discussionsCount)
+
+                    else ->
+                        containerView.context.getString(discussionThreadContainer.writeFirstStringRes)
+                }
+            stepDiscussions.setCompoundDrawables(start = if (discussionProxy != null) discussionThreadContainer.containerDrawable else -1)
+            containerView.isEnabled = discussionProxy != null
             containerView.isVisible = true
         }
     }
