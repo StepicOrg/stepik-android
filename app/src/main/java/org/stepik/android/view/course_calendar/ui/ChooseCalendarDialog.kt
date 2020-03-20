6e58b66e61af8fc96d5dbf8a9412ce47bf2d9d0d
Ruslan Davletshin <smartmorefeed@gmail.com>
2020-03-20 22:50:06 +0300

    choose calendar dialog

--- app/src/main/java/org/stepik/android/view/course_calendar/ui/ChooseCalendarDialog.kt
+++ app/src/main/java/org/stepik/android/view/course_calendar/ui/ChooseCalendarDialog.kt
@@ -5,14 +5,13 @@ import android.app.Dialog
 import android.content.Intent
 import android.os.Bundle
 import android.os.Parcelable
-import android.widget.ArrayAdapter
 import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.DialogFragment
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import org.stepic.droid.R
 import org.stepik.android.domain.calendar.model.CalendarItem
 
 class ChooseCalendarDialog : DialogFragment() {
-
     companion object {
         private const val parcelableArrayListKey = "parcelable_list_key"
         const val KEY_CALENDAR_ITEM = "calendar_item"
@@ -32,23 +31,22 @@ class ChooseCalendarDialog : DialogFragment() {
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         val calendarItems = arguments?.getParcelableArrayList<CalendarItem>(parcelableArrayListKey) ?: arrayListOf()
         val ownerTitles: Array<String> = calendarItems.map { it.owner }.toTypedArray()
-        val adapter = ArrayAdapter<String>(requireContext(), R.layout.simple_list_item_single_choice, ownerTitles)
 
-        val builder = AlertDialog.Builder(requireContext())
-        builder
+        return MaterialAlertDialogBuilder(context)
             .setTitle(R.string.choose_calendar_title)
-            .setSingleChoiceItems(adapter, 0, null)
+            .setSingleChoiceItems(ownerTitles, 0, null)
             .setNegativeButton(R.string.cancel, null)
             .setPositiveButton(R.string.ok) { dialog, _ ->
                 dialog.dismiss()
                 val selectedPosition = (dialog as AlertDialog).listView.checkedItemPosition
                 val chosenCalendarItem = calendarItems[selectedPosition]
-                targetFragment?.onActivityResult(
+                targetFragment
+                    ?.onActivityResult(
                         CHOOSE_CALENDAR_REQUEST_CODE,
                         Activity.RESULT_OK,
                         Intent().putExtra(KEY_CALENDAR_ITEM, chosenCalendarItem as Parcelable)
-                )
+                    )
             }
-        return builder.create()
+            .create()
     }
 }
