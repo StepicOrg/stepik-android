17fdf13c57a28f949cc4f841ab9cab4ea02d85e9
Ruslan Davletshin <smartmorefeed@gmail.com>
2020-03-29 16:17:41 +0300

    use branch only in case of deeplink

--- app/src/main/java/org/stepic/droid/ui/activities/SplashActivity.kt
+++ app/src/main/java/org/stepic/droid/ui/activities/SplashActivity.kt
@@ -10,8 +10,6 @@ import android.os.Bundle
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 import io.branch.referral.Branch
-import io.branch.referral.BranchError
-import org.json.JSONObject
 import org.stepic.droid.R
 import org.stepic.droid.analytic.AmplitudeAnalytic
 import org.stepic.droid.analytic.Analytic
@@ -79,20 +77,26 @@ class SplashActivity : BackToExitActivityBase(), SplashView {
 
     override fun onStart() {
         super.onStart()
-        Branch
-            .sessionBuilder(this)
-            .withCallback { referringParams, error ->
-                if (error == null && referringParams != null && referringParams.has(BranchParams.FIELD_CAMPAIGN)) {
-                    analytics.reportAmplitudeEvent(AmplitudeAnalytic.Branch.LINK_OPENED, mapOf(
-                        AmplitudeAnalytic.Branch.PARAM_CAMPAIGN to referringParams[BranchParams.FIELD_CAMPAIGN],
-                        AmplitudeAnalytic.Branch.IS_FIRST_SESSION to referringParams.optBoolean(BranchParams.IS_FIRST_SESSION, false)
-                    ))
-                }
 
-                splashPresenter.onSplashCreated(referringParams)
-            }
-            .withData(intent?.data)
-            .init()
+        val uri = intent?.data
+        if (uri == null) {
+            splashPresenter.onSplashCreated()
+        } else {
+            Branch
+                .sessionBuilder(this)
+                .withCallback { referringParams, error ->
+                    if (error == null && referringParams != null && referringParams.has(BranchParams.FIELD_CAMPAIGN)) {
+                        analytics.reportAmplitudeEvent(AmplitudeAnalytic.Branch.LINK_OPENED, mapOf(
+                            AmplitudeAnalytic.Branch.PARAM_CAMPAIGN to referringParams[BranchParams.FIELD_CAMPAIGN],
+                            AmplitudeAnalytic.Branch.IS_FIRST_SESSION to referringParams.optBoolean(BranchParams.IS_FIRST_SESSION, false)
+                        ))
+                    }
+
+                    splashPresenter.onSplashCreated(referringParams)
+                }
+                .withData(uri)
+                .init()
+        }
     }
 
     override fun onDestroy() {
