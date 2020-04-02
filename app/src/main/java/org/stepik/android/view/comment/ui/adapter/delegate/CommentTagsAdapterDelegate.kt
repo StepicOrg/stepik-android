a065812713472020409258e00fe9cb9d6e0b8acf
Ruslan Davletshin <smartmorefeed@gmail.com>
2020-04-02 18:14:04 +0300

    comment tags items

--- app/src/main/java/org/stepik/android/view/comment/ui/adapter/delegate/CommentTagsAdapterDelegate.kt
+++ app/src/main/java/org/stepik/android/view/comment/ui/adapter/delegate/CommentTagsAdapterDelegate.kt
@@ -1,8 +1,10 @@
 package org.stepik.android.view.comment.ui.adapter.delegate
 
+import android.content.res.ColorStateList
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.core.content.ContextCompat
+import androidx.core.widget.TextViewCompat
 import org.stepic.droid.R
 import org.stepic.droid.ui.util.setCompoundDrawables
 import org.stepik.android.view.comment.model.CommentTag
@@ -20,10 +22,13 @@ class CommentTagsAdapterDelegate : AdapterDelegate<CommentTag, DelegateViewHolde
         private val root: TextView
     ) : DelegateViewHolder<CommentTag>(root) {
         override fun onBind(data: CommentTag) {
+            val textColor = ContextCompat.getColor(context, data.textColorRes)
+
             root.setText(data.textRes)
-            root.setTextColor(ContextCompat.getColor(context, data.textColorRes))
+            root.setTextColor(textColor)
             root.setBackgroundResource(data.backgroundRes)
             root.setCompoundDrawables(start = data.compoundDrawableRes)
+            TextViewCompat.setCompoundDrawableTintList(root, ColorStateList.valueOf(textColor))
         }
     }
 }
