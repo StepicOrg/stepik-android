ccc373c9e59cc26ce2eddfaf3b9082c41cc1ad1e
Rostislav Smirnov <rostislavsmirnoff@gmail.com>
2020-04-28 22:04:59 +0300

    fix tests

--- app/src/test/java/org/stepik/android/domain/course/model/CourseHeaderDataTest.kt
+++ app/src/test/java/org/stepik/android/domain/course/model/CourseHeaderDataTest.kt
@@ -4,7 +4,7 @@ import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
 import org.stepic.droid.testUtils.assertThatObjectParcelable
-import org.stepik.android.domain.user_courses.model.UserCourse
+import org.stepik.android.domain.user_courses.model.UserCourseHeader
 import org.stepik.android.model.Course
 import org.stepik.android.model.Progress
 
@@ -24,15 +24,7 @@ class CourseHeaderDataTest {
         val courseHeaderData = CourseHeaderData(
             courseId = 100,
             course = Course(id = 100),
-            userCourse = UserCourse(
-                id = 0,
-                user = 0,
-                course = 0,
-                isFavorite = false,
-                isPinned = false,
-                isArchived = false,
-                lastViewed = null
-            ),
+            userCourseHeader =  UserCourseHeader.Empty,
             title = "title",
             cover = "cover",
             stats = CourseStats(
