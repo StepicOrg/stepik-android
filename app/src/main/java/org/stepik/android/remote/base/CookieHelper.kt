e36a6f09df7daac25f03fadf36108919930de5ad
Ruslan Davletshin <smartmorefeed@gmail.com>
2020-05-12 23:53:59 +0300

    remove cookie manager from cookie helper

--- app/src/main/java/org/stepik/android/remote/base/CookieHelper.kt
+++ app/src/main/java/org/stepik/android/remote/base/CookieHelper.kt
@@ -1,13 +1,8 @@
 package org.stepik.android.remote.base
 
-import android.os.Build
-import android.os.Looper
-import android.webkit.CookieManager
-import io.reactivex.Completable
-import io.reactivex.Scheduler
 import okhttp3.Request
 import org.stepic.droid.configuration.Config
-import org.stepic.droid.di.qualifiers.BackgroundScheduler
+import org.stepic.droid.preferences.SharedPreferenceHelper
 import org.stepic.droid.util.AppConstants
 import org.stepik.android.remote.auth.service.EmptyAuthService
 import java.net.HttpCookie
@@ -19,12 +14,14 @@ import javax.inject.Inject
 class CookieHelper
 @Inject
 constructor(
+    private val sharedPreferenceHelper: SharedPreferenceHelper,
     private val emptyAuthService: EmptyAuthService,
-    private val cookieManager: CookieManager,
-    private val config: Config,
-    @BackgroundScheduler
-    private val backgroundScheduler: Scheduler
+    private val config: Config
 ) {
+    companion object {
+        private const val COOKIE_SEPARATOR = "|"
+        private const val COOKIE_HEADER_SEPARATOR = ";"
+    }
 
     private fun tryGetCsrfFromOnePair(keyValueCookie: String): String? =
         HttpCookie.parse(keyValueCookie).find { it.name == AppConstants.csrfTokenCookieName }?.value
@@ -39,33 +36,29 @@ constructor(
     }
 
     fun removeCookiesCompat() {
-        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
-            @Suppress("DEPRECATION")
-            cookieManager.removeAllCookie()
-        } else {
-            Completable.fromRunnable {
-                Looper.prepare()
-                cookieManager.removeAllCookies(null)
-                Looper.loop()
-            }.subscribeOn(backgroundScheduler).subscribe()
-        }
+        sharedPreferenceHelper.cookiesHeader = null
     }
 
     fun addCsrfTokenToRequest(request: Request): Request {
-        val cookies = cookieManager.getCookie(config.baseUrl)
-        return if (cookies == null) {
-            request
-        } else {
-            val csrftoken =
-                getCsrfTokenFromCookies(
-                    cookies
-                )
-            request.newBuilder()
-                .addHeader(AppConstants.refererHeaderName, config.baseUrl)
-                .addHeader(AppConstants.csrfTokenHeaderName, csrftoken)
-                .addHeader(AppConstants.cookieHeaderName, cookies)
-                .build()
-        }
+        val cookies = sharedPreferenceHelper
+            .cookiesHeader
+            ?.split(COOKIE_SEPARATOR)
+            ?.flatMap { cookie ->
+                HttpCookie.parse(cookie).filter { !it.hasExpired() }
+            }
+            ?: return request
+
+        val csrftoken = cookies.find { it.name == AppConstants.csrfTokenCookieName }
+            ?.value
+            ?: ""
+
+        val header = cookies.joinToString(separator = COOKIE_HEADER_SEPARATOR) { it.toString() }
+
+        return request.newBuilder()
+            .addHeader(AppConstants.refererHeaderName, config.baseUrl)
+            .addHeader(AppConstants.csrfTokenHeaderName, csrftoken)
+            .addHeader(AppConstants.cookieHeaderName, header)
+            .build()
     }
 
     fun getCookiesForBaseUrl(): List<HttpCookie>? {
@@ -88,11 +81,7 @@ constructor(
         val lang = Locale.getDefault().language
         val response = emptyAuthService.getStepicForFun(lang).execute()
 
-        val setCookieHeaders = response.headers().values(AppConstants.setCookieHeaderName)
-        for (value in setCookieHeaders) {
-            if (value != null) {
-                cookieManager.setCookie(config.baseUrl, value) // set-cookie is not empty
-            }
-        }
+        sharedPreferenceHelper.cookiesHeader =
+            response.headers().values(AppConstants.setCookieHeaderName).joinToString(separator = COOKIE_SEPARATOR)
     }
 }
