1906ee3fb67b8c8c6cb6d45fe185661188d88876
Ruslan Davletshin <smartmorefeed@gmail.com>
2020-05-19 20:20:43 +0300

    add course list analytic data

---
+++ app/src/main/java/org/stepik/android/domain/course/analytic/CourseCardSeenAnalyticEvent.kt
@@ -0,0 +1,22 @@
+package org.stepik.android.domain.course.analytic
+
+import org.stepik.android.domain.base.analytic.AnalyticEvent
+
+class CourseCardSeenAnalyticEvent(
+    courseId: Long,
+    source: CourseSourceAnalyticData
+) : AnalyticEvent {
+    companion object {
+        private const val PARAM_COURSE = "course"
+        private const val PARAM_SOURCE = "source"
+    }
+
+    override val name: String =
+        "Course card seen"
+
+    override val params: Map<String, Any> =
+        mapOf(
+            PARAM_COURSE to courseId,
+            PARAM_SOURCE to source.name
+        ) + source.params
+}
---
+++ app/src/main/java/org/stepik/android/domain/course/analytic/CourseSourceAnalyticData.kt
@@ -0,0 +1,73 @@
+package org.stepik.android.domain.course.analytic
+
+import com.google.gson.Gson
+import org.stepik.android.domain.base.analytic.AnalyticEvent
+import org.stepik.android.domain.course_list.model.CourseListQuery
+
+sealed class CourseSourceAnalyticData : AnalyticEvent {
+    class Search(query: String): CourseSourceAnalyticData() {
+        companion object {
+            private const val PARAM_QUERY = "query"
+        }
+
+        override val name: String = "search"
+
+        override val params: Map<String, Any> =
+            mapOf(PARAM_QUERY to query)
+    }
+
+    class Collection(collectionId: Long): CourseSourceAnalyticData() {
+        companion object {
+            private const val PARAM_COLLECTION = "collection"
+        }
+
+        override val name: String = "collection"
+
+        override val params: Map<String, Any> =
+            mapOf(PARAM_COLLECTION to collectionId)
+    }
+
+    class Query(query: CourseListQuery): CourseSourceAnalyticData() {
+        companion object {
+            private const val PARAM_QUERY = "query"
+        }
+
+        override val name: String = "query"
+
+        override val params: Map<String, Any> =
+            mapOf(PARAM_QUERY to Gson().toJson(query))
+    }
+
+    class Profile(profileId: Long): CourseSourceAnalyticData() {
+        companion object {
+            private const val PARAM_PROFILE = "profile"
+        }
+
+        override val name: String = "profile"
+
+        override val params: Map<String, Any> =
+            mapOf(PARAM_PROFILE to profileId)
+    }
+
+    class Story(storyId: Long): CourseSourceAnalyticData() {
+        companion object {
+            private const val PARAM_STORY = "story"
+        }
+
+        override val name: String = "story"
+
+        override val params: Map<String, Any> =
+            mapOf(PARAM_STORY to storyId)
+    }
+
+    class DeepLink(url: String): CourseSourceAnalyticData() {
+        companion object {
+            private const val PARAM_URL = "url"
+        }
+
+        override val name: String = "deeplink"
+
+        override val params: Map<String, Any> =
+            mapOf(PARAM_URL to url)
+    }
+}
