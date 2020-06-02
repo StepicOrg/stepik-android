2c4450aa560e6bd3622005e0224b597282c02c4e
Rostislav Smirnov <rostislavsmirnoff@gmail.com>
2020-06-02 14:47:13 +0300

    Deserialize blanks feedback

---
+++ model/src/main/java/org/stepik/android/model/feedback/BlanksFeedback.kt
@@ -0,0 +1,9 @@
+package org.stepik.android.model.feedback
+
+import com.google.gson.annotations.SerializedName
+import java.io.Serializable
+
+data class BlanksFeedback(
+    @SerializedName("blanks_feedback")
+    val blanksFeedback: List<String>? = null
+) : Feedback, Serializable
