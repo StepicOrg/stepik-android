f7ba3b7ba07b6cb4bce91782416582ee5aeb6d1b
Rostislav Smirnov <rostislavsmirnoff@gmail.com>
2020-08-11 23:42:30 +0300

    Schedule notifications between 12 and 21

--- app/src/main/java/org/stepik/android/domain/purchase_notification/interactor/PurchaseReminderInteractor.kt
+++ app/src/main/java/org/stepik/android/domain/purchase_notification/interactor/PurchaseReminderInteractor.kt
@@ -2,10 +2,12 @@ package org.stepik.android.domain.purchase_notification.interactor
 
 import io.reactivex.Completable
 import org.stepic.droid.analytic.experiments.CoursePurchaseReminderSplitTest
+import org.stepic.droid.util.AppConstants
 import org.stepic.droid.util.DateTimeHelper
 import org.stepik.android.data.purchase_notification.model.PurchaseNotificationScheduled
 import org.stepik.android.domain.purchase_notification.repository.PurchaseNotificationRepository
 import org.stepik.android.view.purchase_notification.notification.PurchaseNotificationDelegate
+import java.util.Calendar
 import javax.inject.Inject
 
 class PurchaseReminderInteractor
@@ -33,6 +35,24 @@ constructor(
             }
             .doOnComplete { purchaseNotificationDelegate.schedulePurchaseNotification() }
 
-    private fun calculateScheduleOffset(timeStamp: Long): Long =
-        timeStamp + (coursePurchaseReminderSplitTest.currentGroup.notificationDelayHours * MILLIS_IN_1_HOUR)
+    private fun calculateScheduleOffset(timeStamp: Long): Long {
+        val scheduledTime = timeStamp + (coursePurchaseReminderSplitTest.currentGroup.notificationDelayHours * MILLIS_IN_1_HOUR)
+        val calendar = Calendar.getInstance()
+        calendar.timeInMillis = scheduledTime
+
+        val scheduledHour = calendar.get(Calendar.HOUR_OF_DAY)
+        calendar.set(Calendar.HOUR_OF_DAY, 12)
+        calendar.set(Calendar.MINUTE, 0)
+        calendar.set(Calendar.MILLISECOND, 0)
+        val nowAt12 = calendar.timeInMillis
+
+        return when {
+            scheduledHour < 12 ->
+                nowAt12
+            scheduledHour >= 21 ->
+                nowAt12 + AppConstants.MILLIS_IN_24HOURS
+            else ->
+                scheduledTime
+        }
+    }
 }
