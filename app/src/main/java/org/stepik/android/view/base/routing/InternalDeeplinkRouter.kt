58a06b6267374c80d0c339cbae5bb49bf0e04f86
Rostislav Smirnov <rostislavsmirnoff@gmail.com>
2020-08-18 14:05:27 +0300

    Show error if no activity found

--- app/src/main/java/org/stepik/android/view/base/routing/InternalDeeplinkRouter.kt
+++ app/src/main/java/org/stepik/android/view/base/routing/InternalDeeplinkRouter.kt
@@ -4,7 +4,9 @@ import android.content.ActivityNotFoundException
 import android.content.Context
 import android.content.Intent
 import android.net.Uri
+import android.widget.Toast
 import org.stepic.droid.BuildConfig
+import org.stepic.droid.R
 
 object InternalDeeplinkRouter {
     private const val PARAM_MOBILE_INTERNAL_DEEPLINK = "mobile_internal_deeplink"
@@ -19,7 +21,13 @@ object InternalDeeplinkRouter {
             intent.`package` = BuildConfig.APPLICATION_ID
             context.startActivity(intent)
         } catch (e: ActivityNotFoundException) {
-            context.startActivity(Intent(Intent.ACTION_VIEW, uri))
+            val intent = Intent(Intent.ACTION_VIEW, uri)
+            if (intent.resolveActivity(context.packageManager) != null) {
+                context.startActivity(intent)
+            } else {
+                val errorMessage = context.resources.getString(R.string.internal_deeplink_error, uri.path)
+                Toast.makeText(context, errorMessage, Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
