5a262122cf1fa3c441a110c6e7be351ac21d56de
Rostislav Smirnov <rostislavsmirnoff@gmail.com>
2020-08-30 18:54:21 +0300

    Pass source type

--- app/src/main/java/org/stepik/android/domain/course/interactor/CourseStatsInteractor.kt
+++ app/src/main/java/org/stepik/android/domain/course/interactor/CourseStatsInteractor.kt
@@ -95,7 +95,7 @@ constructor(
 
             resolveEnrollmentState ->
                 coursePaymentsRepository
-                    .getCoursePaymentsByCourseId(course.id, coursePaymentStatus = CoursePayment.Status.SUCCESS)
+                    .getCoursePaymentsByCourseId(course.id, coursePaymentStatus = CoursePayment.Status.SUCCESS, primarySourceType = sourceType)
                     .flatMap { payments ->
                         if (payments.isEmpty()) {
                             if (inAppPurchaseSplitTest.currentGroup.isInAppPurchaseActive) {
