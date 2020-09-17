97cee59b2f7eef20a888e12f5f72ab37540dc4c6
Ruslan Davletshin <smartmorefeed@gmail.com>
2020-09-17 16:28:46 +0300

    save search query

--- app/src/main/java/org/stepik/android/data/search/repository/SearchRepositoryImpl.kt
+++ app/src/main/java/org/stepik/android/data/search/repository/SearchRepositoryImpl.kt
@@ -1,7 +1,9 @@
 package org.stepik.android.data.search.repository
 
+import io.reactivex.Completable
 import io.reactivex.Single
 import org.stepic.droid.model.SearchQuery
+import org.stepik.android.data.search.source.SearchCacheDataSource
 import org.stepik.android.data.search.source.SearchRemoteDataSource
 import org.stepik.android.domain.search.repository.SearchRepository
 import javax.inject.Inject
@@ -9,8 +11,12 @@ import javax.inject.Inject
 class SearchRepositoryImpl
 @Inject
 constructor(
-    private val searchRemoteDataSource: SearchRemoteDataSource
+    private val searchRemoteDataSource: SearchRemoteDataSource,
+    private val searchCacheDataSource: SearchCacheDataSource
 ) : SearchRepository {
+    override fun saveSearchQuery(query: String): Completable =
+        searchCacheDataSource.saveSearchQuery(query)
+
     override fun getSearchQueries(query: String): Single<List<SearchQuery>> =
         searchRemoteDataSource.getSearchQueries(query)
 }
--- app/src/main/java/org/stepik/android/domain/course_list/interactor/CourseListSearchInteractor.kt
+++ app/src/main/java/org/stepik/android/domain/course_list/interactor/CourseListSearchInteractor.kt
@@ -1,14 +1,17 @@
 package org.stepik.android.domain.course_list.interactor
 
+import io.reactivex.Completable
 import io.reactivex.Observable
 import io.reactivex.Single
 import org.stepic.droid.preferences.SharedPreferenceHelper
 import org.stepic.droid.util.PagedList
+import org.stepic.droid.util.then
 import ru.nobird.android.core.model.mapToLongArray
 import org.stepik.android.domain.base.DataSourceType
 import org.stepik.android.domain.course.analytic.CourseViewSource
 import org.stepik.android.domain.course.model.SourceTypeComposition
 import org.stepik.android.domain.course_list.model.CourseListItem
+import org.stepik.android.domain.search.repository.SearchRepository
 import org.stepik.android.domain.search_result.model.SearchResultQuery
 import org.stepik.android.domain.search_result.repository.SearchResultRepository
 import org.stepik.android.model.SearchResult
@@ -19,9 +22,11 @@ class CourseListSearchInteractor
 constructor(
     private val searchResultRepository: SearchResultRepository,
     private val sharedPreferenceHelper: SharedPreferenceHelper,
-    private val courseListInteractor: CourseListInteractor
+    private val courseListInteractor: CourseListInteractor,
+    private val searchRepository: SearchRepository
 ) {
     fun getCoursesBySearch(searchResultQuery: SearchResultQuery): Observable<Pair<PagedList<CourseListItem.Data>, DataSourceType>> =
+        addSearchQuery(searchResultQuery) then
         searchResultRepository
             .getSearchResults(searchResultQuery.copy(lang = sharedPreferenceHelper.languageForFeatured))
             .flatMapObservable { searchResult ->
@@ -33,6 +38,13 @@ constructor(
                     )
                     .toObservable()
             }
+
+    private fun addSearchQuery(searchResultQuery: SearchResultQuery): Completable =
+        if (searchResultQuery.query.isNullOrEmpty()) {
+            Completable.complete()
+        } else {
+            searchRepository.saveSearchQuery(searchResultQuery.query)
+        }
 
     fun getCourseListItems(
         vararg courseId: Long,
