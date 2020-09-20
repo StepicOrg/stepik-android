871ea8b02dda18f89db53c7c53c79b49672882dc
Ruslan Davletshin <smartmorefeed@gmail.com>
2020-09-20 22:09:59 +0300

    add reducer

--- app/src/main/java/org/stepik/android/presentation/step_quiz_review/StepQuizReviewView.kt
+++ app/src/main/java/org/stepik/android/presentation/step_quiz_review/StepQuizReviewView.kt
@@ -1,13 +1,64 @@
 package org.stepik.android.presentation.step_quiz_review
 
+import org.stepik.android.domain.review_instruction.model.ReviewInstruction
+import org.stepik.android.domain.review_session.model.ReviewSession
+import org.stepik.android.model.Progress
+import org.stepik.android.model.Step
+import org.stepik.android.model.Submission
+import org.stepik.android.model.attempts.Attempt
+import org.stepik.android.presentation.step_quiz.StepQuizView
+
 interface StepQuizReviewView {
     sealed class State {
         object Idle : State()
-        object Loading : State()
+        data class Loading(val step: Step) : State()
+        object Error : State()
 
-        class Content() : State()
+        data class SubmissionNotMade(val quizState: StepQuizView.State, val instruction: ReviewInstruction) : State()
+        data class SubmissionNotSelected(val quizState: StepQuizView.State.AttemptLoaded, val instruction: ReviewInstruction) : State()
+        data class SubmissionSelectedLoading(val quizState: StepQuizView.State.AttemptLoaded, val instruction: ReviewInstruction) : State()
+        data class SubmissionSelected(val quizState: StepQuizView.State.AttemptLoaded, val session: ReviewSession, val instruction: ReviewInstruction) : State()
+        data class Completed(val quizState: StepQuizView.State.AttemptLoaded, val progress: Progress) : State()
     }
 
-    fun setState(state: State)
-    fun showNetworkError()
+    sealed class Message {
+        /**
+         * Initialization
+         */
+        data class InitWithStep(val step: Step, val forceUpdate: Boolean = false) : Message()
+        data class FetchReviewSessionSuccess(
+            val session: ReviewSession,
+            val instruction: ReviewInstruction,
+            val submission: Submission,
+            val attempt: Attempt
+        ) : Message()
+        data class FetchStepQuizStateSuccess(
+            val quizState: StepQuizView.State,
+            val instruction: ReviewInstruction
+        ) : Message()
+        object InitialFetchError : Message()
+
+        /**
+         * Submission creation or changing
+         */
+        object SolveAgain : Message() // solve again
+        data class ChangeSubmission(val submission: Submission, val attempt: Attempt) : Message() // change solution from existing
+
+        /**
+         * Submitting submission to review
+         */
+        object SelectCurrentSubmission : Message() // selects current solution
+        object SelectSubmissionError : Message() // error during solution selecting
+        data class SessionCreated(val reviewSession: ReviewSession) : Message() // solution selected and session created
+    }
+
+    sealed class Action {
+        data class FetchStepQuizState(val step: Step) : Action() // if there is no review session
+        data class FetchReviewSession(val instructionId: Long, val sessionId: Long) : Action()
+        data class CreateSessionWithSubmission(val submissionId: Long) : Action() // select solution
+        object ShowNetworkError : Action() // error
+    }
+
+    fun render(state: State)
+    fun onAction(action: Action)
 }
---
+++ app/src/main/java/org/stepik/android/presentation/step_quiz_review/reducer/StepQuizReviewReducer.kt
@@ -0,0 +1,127 @@
+package org.stepik.android.presentation.step_quiz_review.reducer
+
+import org.stepik.android.domain.step_quiz.model.StepQuizRestrictions
+import org.stepik.android.model.DiscountingPolicyType
+import org.stepik.android.model.Submission
+import org.stepik.android.model.attempts.Attempt
+import org.stepik.android.presentation.step_quiz.StepQuizView
+import org.stepik.android.presentation.step_quiz_review.StepQuizReviewView.State
+import org.stepik.android.presentation.step_quiz_review.StepQuizReviewView.Action
+import org.stepik.android.presentation.step_quiz_review.StepQuizReviewView.Message
+import ru.nobird.android.core.model.safeCast
+import javax.inject.Inject
+
+class StepQuizReviewReducer
+@Inject
+constructor() : StateReducer<State, Message, Action> {
+    override fun reduce(
+        state: State,
+        message: Message
+    ): Pair<State, Set<Action>> =
+        when (message) {
+            is Message.InitWithStep ->
+                if (state is State.Idle ||
+                    state is State.Error && message.forceUpdate) {
+                    val sessionId = message.step.session
+                    val action =
+                        if (sessionId != null && sessionId > 0) {
+                            Action.FetchReviewSession(message.step.instruction ?: -1, sessionId)
+                        } else {
+                            Action.FetchStepQuizState(message.step)
+                        }
+
+                    State.Loading(message.step) to setOf(action)
+                } else {
+                    null
+                }
+
+            is Message.FetchReviewSessionSuccess ->
+                when (state) {
+                    is State.Loading ->
+                        State.SubmissionSelected(
+                            quizState = createAttemptLoadedState(message.attempt, message.submission),
+                            session = message.session,
+                            instruction = message.instruction
+                        ) to emptySet()
+
+                    else -> null
+                }
+
+            is Message.FetchStepQuizStateSuccess ->
+                when (state) {
+                    is State.Loading ->
+                        State.SubmissionNotMade(
+                            quizState = message.quizState,
+                            instruction = message.instruction
+                        ) to emptySet()
+                    else -> null
+                }
+
+            is Message.InitialFetchError ->
+                when (state) {
+                    is State.Loading ->
+                        State.Error to emptySet()
+                    else -> null
+                }
+
+            is Message.SolveAgain ->
+                when (state) {
+                    is State.SubmissionNotSelected ->
+                        State.SubmissionNotMade(state.quizState, state.instruction) to emptySet()
+
+                    else -> null
+                }
+
+            is Message.ChangeSubmission ->
+                when (state) {
+                    is State.SubmissionNotSelected -> {
+                        val stepQuizViewState =
+                            state.quizState.copy(attempt = message.attempt, submissionState = StepQuizView.SubmissionState.Loaded(message.submission))
+
+                        State.SubmissionNotSelected(stepQuizViewState, state.instruction) to emptySet()
+                    }
+
+                    else -> null
+                }
+
+            is Message.SelectCurrentSubmission ->
+                when (state) {
+                    is State.SubmissionNotSelected -> {
+                        val submissionId = state.quizState.submissionState.safeCast<StepQuizView.SubmissionState.Loaded>()
+                            ?.submission
+                            ?.id
+
+                        if (submissionId != null) {
+                            State.SubmissionSelectedLoading(state.quizState, state.instruction) to setOf(Action.CreateSessionWithSubmission(submissionId))
+                        } else {
+                            null
+                        }
+                    }
+
+                    else -> null
+                }
+
+            is Message.SelectSubmissionError ->
+                when (state) {
+                    is State.SubmissionSelectedLoading ->
+                        State.SubmissionNotSelected(state.quizState, state.instruction) to setOf(Action.ShowNetworkError)
+
+                    else -> null
+                }
+
+            is Message.SessionCreated ->
+                when (state) {
+                    is State.SubmissionSelectedLoading ->
+                        State.SubmissionSelected(state.quizState, message.reviewSession, state.instruction) to emptySet()
+
+                    else -> null
+                }
+        } ?: state to emptySet()
+
+    private fun createAttemptLoadedState(attempt: Attempt, submission: Submission, restrictions: StepQuizRestrictions? = null): StepQuizView.State.AttemptLoaded =
+        StepQuizView.State.AttemptLoaded(
+            attempt = attempt,
+            submissionState = StepQuizView.SubmissionState.Loaded(submission),
+            restrictions = restrictions ?: StepQuizRestrictions(1, 1, DiscountingPolicyType.NoDiscount)
+        )
+}
