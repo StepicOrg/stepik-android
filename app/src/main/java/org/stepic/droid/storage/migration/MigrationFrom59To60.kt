116be0be1ae0d7185978ee4697d52154758921ec
Ruslan Davletshin <smartmorefeed@gmail.com>
2020-09-24 19:53:54 +0300

    add migration for step table

--- app/src/main/java/org/stepic/droid/storage/migration/MigrationFrom59To60.kt
+++ app/src/main/java/org/stepic/droid/storage/migration/MigrationFrom59To60.kt
@@ -2,10 +2,12 @@ package org.stepic.droid.storage.migration
 
 import androidx.room.migration.Migration
 import androidx.sqlite.db.SupportSQLiteDatabase
-import org.stepik.android.cache.course_payments.structure.DbStructureCoursePayments
+import org.stepik.android.cache.step.structure.DbStructureStep
 
 object MigrationFrom59To60 : Migration(59, 60) {
     override fun migrate(db: SupportSQLiteDatabase) {
-        // todo migrate Step table
+        db.execSQL("ALTER TABLE ${DbStructureStep.TABLE_NAME} ADD COLUMN ${DbStructureStep.Column.SESSION} LONG")
+        db.execSQL("ALTER TABLE ${DbStructureStep.TABLE_NAME} ADD COLUMN ${DbStructureStep.Column.INSTRUCTION} LONG")
+        db.execSQL("ALTER TABLE ${DbStructureStep.TABLE_NAME} ADD COLUMN ${DbStructureStep.Column.INSTRUCTION_TYPE} TEXT")
     }
 }
