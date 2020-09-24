a4f0790dc34a0a09e9ad80d54d97dcecce5e65ad
Ruslan Davletshin <smartmorefeed@gmail.com>
2020-09-24 21:29:44 +0300

    add attempts & submissions to session response

--- app/src/main/java/org/stepik/android/remote/review_session/model/ReviewSessionResponse.kt
+++ app/src/main/java/org/stepik/android/remote/review_session/model/ReviewSessionResponse.kt
@@ -3,16 +3,21 @@ package org.stepik.android.remote.review_session.model
 import org.stepik.android.domain.review_session.model.ReviewSession
 import com.google.gson.annotations.SerializedName
 import org.stepik.android.model.Meta
+import org.stepik.android.model.Submission
+import org.stepik.android.model.attempts.Attempt
 import org.stepik.android.remote.base.model.MetaResponse
 
 class ReviewSessionResponse(
     @SerializedName("meta")
     override val meta: Meta,
     @SerializedName("review-sessions")
-    val reviewSessions: List<ReviewSession>
+    val reviewSessions: List<ReviewSession>,
 
-//    @SerializedName("attempts")
+    @SerializedName("attempts")
+    val attempts: List<Attempt>,
+    @SerializedName("submissions")
+    val submissions: List<Submission>
+
 //    @SerializedName("reviews")
 //    @SerializedName("rubric-scores")
-//    @SerializedName("submissions")
 ) : MetaResponse
