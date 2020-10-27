8ffa7451837cf0e5f34d0586b1f2843e39ead519
Rostislav Smirnov <rostislavsmirnoff@gmail.com>
2020-10-27 18:45:50 +0300

    Autoincrement id; Add course id uniqueness

--- app/src/main/java/org/stepik/android/domain/visited_courses/model/VisitedCourse.kt
+++ app/src/main/java/org/stepik/android/domain/visited_courses/model/VisitedCourse.kt
@@ -1,14 +1,16 @@
 package org.stepik.android.domain.visited_courses.model
 
 import androidx.room.Entity
+import androidx.room.Index
 import androidx.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
 
-@Entity
+@Entity(indices = [Index(value = ["course"], unique = true)])
 data class VisitedCourse(
-    @PrimaryKey
-    @SerializedName("id")
-    val id: Long,
     @SerializedName("course")
     val course: Long
-)
+) {
+    @PrimaryKey
+    @SerializedName("id")
+    var id: Long = 0
+}
