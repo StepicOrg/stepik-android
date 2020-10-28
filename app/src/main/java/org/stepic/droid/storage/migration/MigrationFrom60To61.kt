b0f79ead3c43dc523f46a7544b73ab5a27148fca
Rostislav Smirnov <rostislavsmirnoff@gmail.com>
2020-10-28 22:05:22 +0300

    Fix trigger

--- app/src/main/java/org/stepic/droid/storage/migration/MigrationFrom60To61.kt
+++ app/src/main/java/org/stepic/droid/storage/migration/MigrationFrom60To61.kt
@@ -7,18 +7,18 @@ object MigrationFrom60To61 : Migration(60, 61) {
     private const val TRIGGER_NAME = "visited_courses_limiter"
     private const val TABLE_SIZE_LIMIT = 20
     override fun migrate(db: SupportSQLiteDatabase) {
-        val trigger = """
-            CREATE TRIGGER IF NOT EXISTS $TRIGGER_NAME
-            AFTER INSERT ON VisitedCourse
-            BEGIN
-            DELETE FROM VisitedCourse
-            WHERE id
-            IN
-            (SELECT * FROM VisitedCourse ORDER BY id DESC
-            LIMIT -1 OFFSET $TABLE_SIZE_LIMIT);
-            END;
-        """.trimIndent()
+        val trigger = "CREATE TRIGGER IF NOT EXISTS $TRIGGER_NAME " +
+           "AFTER INSERT ON VisitedCourse " +
+           "BEGIN " +
+           "DELETE FROM VisitedCourse " +
+           "WHERE id " +
+           "IN" +
+           "(SELECT id " +
+           "FROM VisitedCourse " +
+           "ORDER BY id DESC " +
+           "LIMIT -1 OFFSET $TABLE_SIZE_LIMIT);" +
+           "END;"
         db.execSQL("CREATE TABLE IF NOT EXISTS `VisitedCourse` (`id` INTEGER NOT NULL, `course` INTEGER NOT NULL, PRIMARY KEY(`course`))")
-//        db.execSQL(trigger)
+        db.execSQL(trigger)
     }
 }
