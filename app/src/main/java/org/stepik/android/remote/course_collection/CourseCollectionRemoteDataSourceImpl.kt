d382e067800c7a2a67e9af61d63528be2d14e48c
Ruslan Davletshin <smartmorefeed@gmail.com>
2020-11-17 16:45:50 +0300

    fix empty ids request

--- app/src/main/java/org/stepik/android/remote/course_collection/CourseCollectionRemoteDataSourceImpl.kt
+++ app/src/main/java/org/stepik/android/remote/course_collection/CourseCollectionRemoteDataSourceImpl.kt
@@ -1,9 +1,11 @@
 package org.stepik.android.remote.course_collection
 
 import io.reactivex.Single
+import io.reactivex.functions.Function
 import org.stepik.android.data.course_collection.source.CourseCollectionRemoteDataSource
 import org.stepik.android.domain.course_collection.model.CourseCollectionQuery
 import org.stepik.android.model.CourseCollection
+import org.stepik.android.remote.base.chunkedSingleMap
 import org.stepik.android.remote.course_collection.model.CourseCollectionsResponse
 import org.stepik.android.remote.course_collection.service.CourseCollectionService
 import javax.inject.Inject
@@ -13,13 +15,19 @@ class CourseCollectionRemoteDataSourceImpl
 constructor(
     private val courseCollectionService: CourseCollectionService
 ) : CourseCollectionRemoteDataSource {
+    private val mapper =
+        Function<CourseCollectionsResponse, List<CourseCollection>>(CourseCollectionsResponse::courseCollections)
+
     override fun getCourseCollections(ids: List<Long>): Single<List<CourseCollection>> =
-        courseCollectionService
-            .getCourseCollections(ids)
-            .map(CourseCollectionsResponse::courseCollections)
+        ids.chunkedSingleMap {
+            courseCollectionService
+                .getCourseCollections(it)
+                .map(mapper)
+        }
+
 
     override fun getCourseCollections(query: CourseCollectionQuery): Single<List<CourseCollection>> =
         courseCollectionService
             .getCourseCollections(query.toMap())
-            .map(CourseCollectionsResponse::courseCollections)
+            .map(mapper)
 }
