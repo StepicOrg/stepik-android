2ab01dab2d2c69857f3c0c7ec4d0c47d15720fb6
Ruslan Davletshin <smartmorefeed@gmail.com>
2020-11-20 13:32:02 +0300

    add presentation redux-rx

--- app/src/main/java/org/stepik/android/presentation/step_quiz_review/dispatcher/StepQuizReviewActionDispatcher.kt
+++ app/src/main/java/org/stepik/android/presentation/step_quiz_review/dispatcher/StepQuizReviewActionDispatcher.kt
@@ -3,7 +3,6 @@ package org.stepik.android.presentation.step_quiz_review.dispatcher
 import com.jakewharton.rxrelay2.BehaviorRelay
 import io.reactivex.Scheduler
 import io.reactivex.Single
-import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.rxkotlin.Singles
 import io.reactivex.rxkotlin.plusAssign
 import io.reactivex.rxkotlin.subscribeBy
@@ -16,7 +15,7 @@ import org.stepik.android.domain.step_quiz_review.interactor.StepQuizReviewInter
 import org.stepik.android.presentation.step_quiz.StepQuizFeature
 import org.stepik.android.presentation.step_quiz.dispatcher.StepQuizActionDispatcher
 import org.stepik.android.presentation.step_quiz_review.StepQuizReviewFeature
-import ru.nobird.android.presentation.redux.dispatcher.ActionDispatcher
+import ru.nobird.android.presentation.redux.dispatcher.RxActionDispatcher
 import javax.inject.Inject
 
 class StepQuizReviewActionDispatcher
@@ -33,19 +32,12 @@ constructor(
     private val backgroundScheduler: Scheduler,
     @MainScheduler
     private val mainScheduler: Scheduler
-) : ActionDispatcher<StepQuizReviewFeature.Action, StepQuizReviewFeature.Message> {
-    private val compositeDisposable = CompositeDisposable()
-    private var messageListener: ((StepQuizReviewFeature.Message) -> Unit)? = null
-
+) : RxActionDispatcher<StepQuizReviewFeature.Action, StepQuizReviewFeature.Message>() {
     init {
         compositeDisposable += stepWrapperRxRelay
             .subscribeOn(backgroundScheduler)
             .observeOn(mainScheduler)
-            .subscribeBy(onNext = { messageListener?.invoke(StepQuizReviewFeature.Message.InitWithStep(it, lessonData)) })
-    }
-
-    override fun setListener(listener: (message: StepQuizReviewFeature.Message) -> Unit) {
-        messageListener = listener
+            .subscribeBy(onNext = { onNewMessage(StepQuizReviewFeature.Message.InitWithStep(it, lessonData)) })
     }
 
     override fun handleAction(action: StepQuizReviewFeature.Action) {
@@ -60,9 +52,9 @@ constructor(
                     .observeOn(mainScheduler)
                     .subscribeBy(
                         onSuccess = { (quizState, progress) ->
-                            messageListener?.invoke(StepQuizReviewFeature.Message.FetchStepQuizStateSuccess(quizState, progress.firstOrNull()))
+                            onNewMessage(StepQuizReviewFeature.Message.FetchStepQuizStateSuccess(quizState, progress.firstOrNull()))
                         },
-                        onError = { messageListener?.invoke(StepQuizReviewFeature.Message.InitialFetchError) }
+                        onError = { onNewMessage(StepQuizReviewFeature.Message.InitialFetchError) }
                     )
             }
 
@@ -73,9 +65,9 @@ constructor(
                     .subscribeOn(backgroundScheduler)
                     .subscribeBy(
                         onSuccess = { (instruction, sessionData, progress) ->
-                            messageListener?.invoke(StepQuizReviewFeature.Message.FetchReviewSessionSuccess(sessionData, instruction, progress.firstOrNull()))
+                            onNewMessage(StepQuizReviewFeature.Message.FetchReviewSessionSuccess(sessionData, instruction, progress.firstOrNull()))
                         },
-                        onError = { messageListener?.invoke(StepQuizReviewFeature.Message.InitialFetchError) }
+                        onError = { onNewMessage(StepQuizReviewFeature.Message.InitialFetchError) }
                     )
             }
 
@@ -86,9 +78,9 @@ constructor(
                     .subscribeOn(backgroundScheduler)
                     .subscribeBy(
                         onSuccess = { (session, instruction) ->
-                            messageListener?.invoke(StepQuizReviewFeature.Message.SessionCreated(session, instruction))
+                            onNewMessage(StepQuizReviewFeature.Message.SessionCreated(session, instruction))
                         },
-                        onError = { messageListener?.invoke(StepQuizReviewFeature.Message.CreateSessionError) }
+                        onError = { onNewMessage(StepQuizReviewFeature.Message.CreateSessionError) }
                     )
             }
 
@@ -99,9 +91,9 @@ constructor(
                     .subscribeOn(backgroundScheduler)
                     .subscribeBy(
                         onSuccess = { review ->
-                            messageListener?.invoke(StepQuizReviewFeature.Message.ReviewCreated(review.id))
+                            onNewMessage(StepQuizReviewFeature.Message.ReviewCreated(review.id))
                         },
-                        onError = { messageListener?.invoke(StepQuizReviewFeature.Message.StartReviewError) }
+                        onError = { onNewMessage(StepQuizReviewFeature.Message.StartReviewError) }
                     )
             }
         }
@@ -120,8 +112,4 @@ constructor(
                         StepQuizFeature.State.AttemptLoaded(attempt, submissionState, stepRestrictions)
                     }
             }
-
-    override fun cancel() {
-        compositeDisposable.clear()
-    }
 }
