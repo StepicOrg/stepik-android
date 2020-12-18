a2b9d96745841829b581742c870f08e03e947add
Ruslan Davletshin <smartmorefeed@gmail.com>
2020-12-18 18:22:52 +0300

    add color schemes

--- app/src/main/java/org/stepik/android/view/catalog_block/ui/adapter/delegate/SimpleCourseListDefaultAdapterDelegate.kt
+++ app/src/main/java/org/stepik/android/view/catalog_block/ui/adapter/delegate/SimpleCourseListDefaultAdapterDelegate.kt
@@ -2,6 +2,8 @@ package org.stepik.android.view.catalog_block.ui.adapter.delegate
 
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.content.res.AppCompatResources
+import androidx.core.view.ViewCompat
 import kotlinx.android.extensions.LayoutContainer
 import kotlinx.android.synthetic.main.item_simple_course_list_default.*
 import org.stepic.droid.R
@@ -23,6 +25,14 @@ class SimpleCourseListDefaultAdapterDelegate(
     private inner class ViewHolder(
         override val containerView: View
     ) : DelegateViewHolder<StandardCatalogBlockContentItem>(containerView), LayoutContainer {
+        private val colorSchemes =
+            listOf(
+                R.color.color_overlay_green,
+                R.color.color_overlay_yellow,
+                R.color.color_overlay_blue,
+                R.color.color_overlay_violet
+            ).map { AppCompatResources.getColorStateList(context, it) }
+
         init {
             containerView.setOnClickListener { onCourseListClicked(itemData ?: return@setOnClickListener) }
         }
@@ -31,6 +41,13 @@ class SimpleCourseListDefaultAdapterDelegate(
             simpleCourseListTitle.text = data.title
             simpleCourseListCount.text =
                 courseCountMapper.mapCourseCountToString(context, data.coursesCount)
+
+            val colorList = colorSchemes[adapterPosition % colorSchemes.size]
+
+            simpleCourseListTitle.setTextColor(colorList)
+            simpleCourseListCount.setTextColor(colorList)
+
+            ViewCompat.setBackgroundTintList(itemView, colorList)
         }
     }
 }
