faa9c6f91214f4a549c78d8d5fd49ef7f39adaf1
Ruslan Davletshin <smartmorefeed@gmail.com>
2021-03-02 20:43:35 +0300

    APPS-3228: recreate LessonActivity on deeplink

--- app/src/main/java/org/stepik/android/view/lesson/ui/activity/LessonActivity.kt
+++ app/src/main/java/org/stepik/android/view/lesson/ui/activity/LessonActivity.kt
@@ -269,9 +269,16 @@ class LessonActivity : FragmentActivityBase(), LessonView,
     override fun onNewIntent(intent: Intent?) {
         super.onNewIntent(intent)
         if (intent != null) {
-            if (intent.getBooleanExtra(EXTRA_AUTOPLAY_MOVE_NEXT, false)) {
-                lessonPager.post { (stepsAdapter.activeFragments[lessonPager.currentItem] as? NextMoveable)?.moveNext(true) }
-                intent.removeExtra(EXTRA_AUTOPLAY_MOVE_NEXT)
+            when {
+                intent.getBooleanExtra(EXTRA_AUTOPLAY_MOVE_NEXT, false) -> {
+                    lessonPager.post { (stepsAdapter.activeFragments[lessonPager.currentItem] as? NextMoveable)?.moveNext(true) }
+                    intent.removeExtra(EXTRA_AUTOPLAY_MOVE_NEXT)
+                }
+
+                intent.data != null -> {
+                    finish()
+                    startActivity(intent)
+                }
             }
         }
     }
