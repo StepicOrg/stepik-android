86a45b269e6428bf56e9ae2d33d4453ab0909daa
Rostislav Smirnov <rostislavsmirnoff@gmail.com>
2021-03-10 12:19:14 +0300

    Update migration wrappers

--- app/src/sharedTest/java/org/stepik/android/migration_wrapper/MigrationWrappers.kt
+++ app/src/sharedTest/java/org/stepik/android/migration_wrapper/MigrationWrappers.kt
@@ -1,8 +1,9 @@
 package org.stepik.android.migration_wrapper
 
 import org.stepic.droid.storage.migration.MigrationFrom62To63
+import org.stepic.droid.storage.migration.MigrationFrom63To64
+import org.stepic.droid.storage.migration.MigrationFrom64To65
 import org.stepic.droid.storage.migration.Migrations
-import org.stepik.android.cache.base.database.AppDatabase
 
 object MigrationWrappers {
     private const val LAST_TESTED_DATABASE_VERSION = 63
@@ -13,5 +14,9 @@ object MigrationWrappers {
             .map { object : MigrationWrapper(it) {} } as List<MigrationWrapper>
 
 
-    val allMigration = oldMigrations + MigrationWrapperFrom62To63(MigrationFrom62To63)
+    val allMigration = oldMigrations + MigrationWrapperFrom62To63(MigrationFrom62To63) +
+            listOf(
+                object : MigrationWrapper(MigrationFrom63To64) {},
+                object : MigrationWrapper(MigrationFrom64To65) {}
+            )
 }
