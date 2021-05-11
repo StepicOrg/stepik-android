02dc4817772e43566c0798019e14bb9baff37a7e
Rostislav Smirnov <rostislavsmirnoff@gmail.com>
2021-05-11 19:18:05 +0300

    Add bottom sheet dialog

---
+++ app/src/main/java/org/stepik/android/view/lesson/ui/dialog/LessonDemoCompleteBottomSheetDialogFragment.kt
@@ -0,0 +1,33 @@
+package org.stepik.android.view.lesson.ui.dialog
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomsheet.BottomSheetDialog
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import org.stepic.droid.R
+
+class LessonDemoCompleteBottomSheetDialogFragment : BottomSheetDialogFragment() {
+    companion object {
+        const val TAG = "LessonDemoCompleteBottomSheetDialog"
+
+        fun newInstance(): DialogFragment =
+            LessonDemoCompleteBottomSheetDialogFragment()
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setStyle(DialogFragment.STYLE_NO_TITLE, R.style.CustomBottomSheetDialog)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? =
+        inflater.inflate(R.layout.bottom_sheet_dialog_lesson_demo_complete, container, false)
+
+    override fun onStart() {
+        super.onStart()
+        (dialog as? BottomSheetDialog)?.behavior?.state = BottomSheetBehavior.STATE_EXPANDED
+    }
+}
