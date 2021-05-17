4529fa245d85129fe6786f2a8ba10fca0e64c6fe
Rostislav Smirnov <rostislavsmirnoff@gmail.com>
2021-05-17 20:14:16 +0300

    Update naming

--- app/src/main/java/org/stepik/android/view/lesson/ui/dialog/LessonDemoCompleteBottomSheetDialogFragment.kt
+++ app/src/main/java/org/stepik/android/view/lesson/ui/dialog/LessonDemoCompleteBottomSheetDialogFragment.kt
@@ -20,7 +20,7 @@ class LessonDemoCompleteBottomSheetDialogFragment : BottomSheetDialogFragment() 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setStyle(DialogFragment.STYLE_NO_TITLE, R.style.CustomBottomSheetDialog)
+        setStyle(DialogFragment.STYLE_NO_TITLE, R.style.TopCornersRoundedBottomSheetDialog)
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? =
