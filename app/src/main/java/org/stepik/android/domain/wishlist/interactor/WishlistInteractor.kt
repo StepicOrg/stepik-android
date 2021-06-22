0163be4206937fc81d24d2af65dec4fc4426a908
Rostislav Smirnov <rostislavsmirnoff@gmail.com>
2021-06-22 23:10:38 +0300

    Move business logic to interactor

--- app/src/main/java/org/stepik/android/domain/wishlist/interactor/WishlistInteractor.kt
+++ app/src/main/java/org/stepik/android/domain/wishlist/interactor/WishlistInteractor.kt
@@ -1,9 +1,13 @@
 package org.stepik.android.domain.wishlist.interactor
 
 import io.reactivex.Single
+import io.reactivex.rxkotlin.Singles.zip
 import io.reactivex.subjects.PublishSubject
 import org.stepik.android.domain.wishlist.model.WishlistEntity
 import org.stepik.android.domain.base.DataSourceType
+import org.stepik.android.domain.course_list.interactor.CourseListUserInteractor
+import org.stepik.android.domain.course_list.model.UserCourseQuery
+import org.stepik.android.domain.user_courses.model.UserCourse
 import org.stepik.android.domain.wishlist.model.WishlistOperationData
 import org.stepik.android.domain.wishlist.repository.WishlistRepository
 import org.stepik.android.view.injection.course_list.WishlistOperationBus
@@ -13,10 +17,23 @@ class WishlistInteractor
 @Inject
 constructor(
     private val wishlistRepository: WishlistRepository,
+    private val courseListUserInteractor: CourseListUserInteractor,
 
     @WishlistOperationBus
     private val wishlistOperationPublisher: PublishSubject<WishlistOperationData>
 ) {
+    fun getWishlistSyncedWithUserCourses(dataSourceType: DataSourceType = DataSourceType.REMOTE): Single<WishlistEntity> =
+        zip(
+            courseListUserInteractor.getAllUserCourses(userCourseQuery = UserCourseQuery(page = 1, isArchived = false), sourceType = dataSourceType),
+            getWishlist(dataSourceType = dataSourceType)
+        ) { userCourses, wishlistEntity ->
+            val enrolledCourseIds = userCourses.map(UserCourse::course).toSet()
+            val wishlistEntityCourses = wishlistEntity.courses.toSet()
+            val updatedWishlistCourses = (wishlistEntityCourses - enrolledCourseIds).toList()
+            val updatedWishlistEntity = wishlistEntity.copy(courses = updatedWishlistCourses)
+            updatedWishlistEntity
+        }
+
     fun getWishlist(dataSourceType: DataSourceType = DataSourceType.CACHE): Single<WishlistEntity> =
         wishlistRepository
             .getWishlistRecord(dataSourceType)
--- app/src/main/java/org/stepik/android/presentation/wishlist/dispatcher/WishlistActionDispatcher.kt
+++ app/src/main/java/org/stepik/android/presentation/wishlist/dispatcher/WishlistActionDispatcher.kt
@@ -2,15 +2,11 @@ package org.stepik.android.presentation.wishlist.dispatcher
 
 import io.reactivex.Observable
 import io.reactivex.Scheduler
-import io.reactivex.rxkotlin.Singles.zip
 import io.reactivex.rxkotlin.plusAssign
 import io.reactivex.rxkotlin.subscribeBy
 import org.stepic.droid.di.qualifiers.BackgroundScheduler
 import org.stepic.droid.di.qualifiers.MainScheduler
 import org.stepik.android.domain.base.DataSourceType
-import org.stepik.android.domain.course_list.interactor.CourseListUserInteractor
-import org.stepik.android.domain.course_list.model.UserCourseQuery
-import org.stepik.android.domain.user_courses.model.UserCourse
 import org.stepik.android.domain.wishlist.interactor.WishlistInteractor
 import org.stepik.android.domain.wishlist.model.WishlistOperationData
 import org.stepik.android.presentation.wishlist.WishlistFeature
@@ -23,7 +19,6 @@ class WishlistActionDispatcher
 @Inject
 constructor(
     private val wishlistInteractor: WishlistInteractor,
-    private val courseListUserInteractor: CourseListUserInteractor,
     @WishlistOperationBus
     private val wishlistOperationObservable: Observable<WishlistOperationData>,
     @BackgroundScheduler
@@ -43,21 +38,11 @@ constructor(
     override fun handleAction(action: WishlistFeature.Action) {
         when (action) {
             is WishlistFeature.Action.FetchWishList -> {
-                compositeDisposable += zip(
-                    courseListUserInteractor.getAllUserCourses(userCourseQuery = UserCourseQuery(page = 1, isArchived = false), sourceType = DataSourceType.REMOTE),
-                    wishlistInteractor.getWishlist(dataSourceType = DataSourceType.REMOTE)
-                )
+                compositeDisposable += wishlistInteractor.getWishlistSyncedWithUserCourses(dataSourceType = DataSourceType.REMOTE)
                 .subscribeOn(backgroundScheduler)
                 .observeOn(mainScheduler)
                     .subscribeBy(
-                        onSuccess = { (userCourses, wishlistEntity) ->
-                            val enrolledCourseIds = userCourses.map(UserCourse::course).toSet()
-                            val wishlistEntityCourses = wishlistEntity.courses.toSet()
-                            val updatedWishlistCourses = (wishlistEntityCourses - enrolledCourseIds).toList()
-                            val updatedWishlistEntity = wishlistEntity.copy(courses = updatedWishlistCourses)
-
-                            onNewMessage(WishlistFeature.Message.FetchWishlistSuccess(updatedWishlistEntity))
-                        },
+                        onSuccess = { onNewMessage(WishlistFeature.Message.FetchWishlistSuccess(it)) },
                         onError = { onNewMessage(WishlistFeature.Message.FetchWishListError) }
                     )
             }
