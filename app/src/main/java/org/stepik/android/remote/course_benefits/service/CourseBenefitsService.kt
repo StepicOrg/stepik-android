2c94679a7f2a1af9f31bcd615dd8c7bd647e18cc
Rostislav Smirnov <rostislavsmirnoff@gmail.com>
2021-06-28 15:17:42 +0300

    Add missing GET annotation

--- app/src/main/java/org/stepik/android/remote/course_benefits/service/CourseBenefitsService.kt
+++ app/src/main/java/org/stepik/android/remote/course_benefits/service/CourseBenefitsService.kt
@@ -2,7 +2,9 @@ package org.stepik.android.remote.course_benefits.service
 
 import io.reactivex.Single
 import org.stepik.android.remote.course_benefits.model.CourseBenefitsResponse
+import retrofit2.http.GET
 
 interface CourseBenefitsService {
+    @GET("/api/course-benefits")
     fun getCourseBenefits(): Single<CourseBenefitsResponse>
 }
