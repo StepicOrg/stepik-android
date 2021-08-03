8e69e91e9fa3828e0d0085926d8fa2e9fd89d4ab
Rostislav Smirnov <rostislavsmirnoff@gmail.com>
2021-08-03 18:59:25 +0300

    Setup view state delegate and update layout

--- app/src/main/java/org/stepik/android/view/debug/ui/fragment/DebugFragment.kt
+++ app/src/main/java/org/stepik/android/view/debug/ui/fragment/DebugFragment.kt
@@ -2,19 +2,24 @@ package org.stepik.android.view.debug.ui.fragment
 
 import android.os.Bundle
 import android.view.View
+import androidx.appcompat.widget.AppCompatTextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import kotlinx.android.synthetic.main.fragment_debug.*
+import kotlinx.android.synthetic.main.progress_bar_on_empty_screen.*
 import kotlinx.android.synthetic.main.view_centered_toolbar.*
 import org.stepic.droid.R
 import org.stepic.droid.base.App
+import org.stepic.droid.util.copyTextToClipboard
 import org.stepik.android.presentation.debug.DebugFeature
 import org.stepik.android.presentation.debug.DebugViewModel
+import org.stepik.android.view.ui.delegate.ViewStateDelegate
 import ru.nobird.android.presentation.redux.container.ReduxView
 import ru.nobird.android.view.redux.ui.extension.reduxViewModel
-import timber.log.Timber
 import javax.inject.Inject
 
-class DebugFragment : Fragment(R.layout.fragment_debug), ReduxView<DebugFeature.State, DebugFeature.Action.ViewAction> {
+class DebugFragment : Fragment(R.layout.fragment_debug),
+    ReduxView<DebugFeature.State, DebugFeature.Action.ViewAction> {
     companion object {
         const val TAG = "DebugFragment"
 
@@ -27,15 +32,27 @@ class DebugFragment : Fragment(R.layout.fragment_debug), ReduxView<DebugFeature.
 
     private val debugViewModel: DebugViewModel by reduxViewModel(this) { viewModelFactory }
 
+    private val viewStateDelegate = ViewStateDelegate<DebugFeature.State>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         injectComponent()
-        debugViewModel.onNewMessage(DebugFeature.Message.InitMessage())
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         centeredToolbarTitle.setText(R.string.debug_toolbar_title)
+        initViewStateDelegate()
+        debugViewModel.onNewMessage(DebugFeature.Message.InitMessage())
+
+        debugFcmTokenValue.setOnLongClickListener {
+            val textToCopy = (it as AppCompatTextView).text.toString()
+            requireContext().copyTextToClipboard(
+                textToCopy = textToCopy,
+                toastMessage = getString(R.string.copied_to_clipboard_toast)
+            )
+            true
+        }
     }
 
     private fun injectComponent() {
@@ -45,11 +62,21 @@ class DebugFragment : Fragment(R.layout.fragment_debug), ReduxView<DebugFeature.
             .inject(this)
     }
 
+    private fun initViewStateDelegate() {
+        viewStateDelegate.addState<DebugFeature.State.Idle>()
+        viewStateDelegate.addState<DebugFeature.State.Loading>(loadProgressbarOnEmptyScreen)
+        viewStateDelegate.addState<DebugFeature.State.Error>()
+        viewStateDelegate.addState<DebugFeature.State.Content>(debugContent)
+    }
+
     override fun onAction(action: DebugFeature.Action.ViewAction) {
         // no op
     }
 
     override fun render(state: DebugFeature.State) {
-        Timber.d("State: $state")
+        viewStateDelegate.switchState(state)
+        if (state is DebugFeature.State.Content) {
+            debugFcmTokenValue.text = state.fcmToken
+        }
     }
 }
