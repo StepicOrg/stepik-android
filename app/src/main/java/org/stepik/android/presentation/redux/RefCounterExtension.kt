afac0dd539869da80fbaf8dcb0918531e8122ebd
Rostislav Smirnov <rostislavsmirnoff@gmail.com>
2021-09-08 13:58:46 +0300

    Use synchronize

--- app/src/main/java/org/stepik/android/presentation/redux/RefCounterExtension.kt
+++ app/src/main/java/org/stepik/android/presentation/redux/RefCounterExtension.kt
@@ -1,30 +1,23 @@
 package org.stepik.android.presentation.redux
 
 import ru.nobird.android.presentation.redux.feature.Feature
-import java.util.concurrent.atomic.AtomicInteger
 
 fun <State, Message, Action> Feature<State, Message, Action>.wrapWithRefCounter(): Feature<State, Message, Action> =
     object : Feature<State, Message, Action> by this {
-        private val refCounter = AtomicInteger(0)
-        override fun addStateListener(listener: (state: State) -> Unit) {
-            var isSuccessful = false
-            while (!isSuccessful) {
-                val refCounterValue = refCounter.get()
-                val newValue = refCounterValue + 1
-                isSuccessful = refCounter.compareAndSet(refCounterValue, newValue)
+        private var refCounter = 0
+        override fun  addStateListener(listener: (state: State) -> Unit) {
+            synchronized(this) {
+                refCounter++
+                this@wrapWithRefCounter.addStateListener(listener)
             }
-            this@wrapWithRefCounter.addStateListener(listener)
         }
 
         override fun cancel() {
-            var isSuccessful = false
-            while (!isSuccessful) {
-                val refCounterValue = refCounter.get()
-                val newValue = refCounterValue - 1
-                isSuccessful = refCounter.compareAndSet(refCounterValue, newValue)
-            }
-            if (refCounter.get() == 0) {
-                this@wrapWithRefCounter.cancel()
+            synchronized(this) {
+                refCounter--
+                if (refCounter == 0) {
+                    this@wrapWithRefCounter.cancel()
+                }
             }
         }
     }
