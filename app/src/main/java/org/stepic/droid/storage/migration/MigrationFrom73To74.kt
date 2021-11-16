635c1f73602393ab07c7e511b987c6bb3e204d85
Rostislav Smirnov <rostislavsmirnoff@gmail.com>
2021-11-16 17:05:11 +0300

    Fix MobileTier table creation

--- app/src/main/java/org/stepic/droid/storage/migration/MigrationFrom73To74.kt
+++ app/src/main/java/org/stepic/droid/storage/migration/MigrationFrom73To74.kt
@@ -5,7 +5,7 @@ import androidx.sqlite.db.SupportSQLiteDatabase
 
 object MigrationFrom73To74 : Migration(73, 74) {
     override fun migrate(db: SupportSQLiteDatabase) {
-        db.execSQL("CREATE TABLE IF NOT EXISTS `MobileTier` (`id` TEXT NOT NULL, `course` INTEGER NOT NULL, `priceTier` , `promoTier` TEXT, PRIMARY KEY(`course`))")
+        db.execSQL("CREATE TABLE IF NOT EXISTS `MobileTier` (`id` TEXT NOT NULL, `course` INTEGER NOT NULL, `priceTier` TEXT, `promoTier` TEXT, PRIMARY KEY(`course`))")
         db.execSQL("CREATE TABLE IF NOT EXISTS `LightSku` (`id` TEXT NOT NULL, `price` TEXT NOT NULL, PRIMARY KEY(`id`))")
     }
 }
