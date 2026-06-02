a184b006e307f4c8a57c6361704ce1ea83914e17
Aleksandr Zhukov <alexandr.zhukov@hyperskill.org>
2026-06-02 12:19:11 +0200

    Fix SocialAuthConsentBottomSheetDialogFragment.kt check box colors

--- app/src/main/java/org/stepik/android/view/auth/ui/dialog/SocialAuthConsentBottomSheetDialogFragment.kt
+++ app/src/main/java/org/stepik/android/view/auth/ui/dialog/SocialAuthConsentBottomSheetDialogFragment.kt
@@ -1,5 +1,6 @@
 package org.stepik.android.view.auth.ui.dialog
 
+import android.content.res.ColorStateList
 import android.os.Bundle
 import android.text.method.LinkMovementMethod
 import android.view.LayoutInflater
@@ -7,12 +8,14 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.core.text.HtmlCompat
 import androidx.core.view.isVisible
+import androidx.core.widget.CompoundButtonCompat
 import androidx.fragment.app.DialogFragment
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import kotlinx.android.synthetic.main.bottom_sheet_dialog_social_auth_consent.*
 import org.stepic.droid.R
+import org.stepic.droid.util.resolveColorAttribute
 import org.stepic.droid.util.stripUnderlinesFromLinks
 import org.stepik.android.domain.auth.mapper.SocialConsentMapper
 import org.stepik.android.domain.auth.mapper.SocialConsentResult
@@ -36,6 +39,18 @@ class SocialAuthConsentBottomSheetDialogFragment :
 
     private var socialNetwork: SocialNetwork by argument()
     private var isMarketingEnabled: Boolean by argument()
+    private val requiredConsentDefaultTint by lazy {
+        createRequiredConsentTint(
+            checkedColor = requireContext().resolveColorAttribute(R.attr.colorSecondary),
+            uncheckedColor = requireContext().resolveColorAttribute(R.attr.colorOnSurface)
+        )
+    }
+    private val requiredConsentErrorTint by lazy {
+        createRequiredConsentTint(
+            checkedColor = requireContext().resolveColorAttribute(R.attr.colorSecondary),
+            uncheckedColor = requireContext().resolveColorAttribute(R.attr.colorError)
+        )
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -65,8 +80,9 @@ class SocialAuthConsentBottomSheetDialogFragment :
         }
 
         requiredConsentCheckBox.setOnCheckedChangeListener { _, _ ->
-            consentErrorText.isVisible = false
+            clearRequiredConsentError()
         }
+        clearRequiredConsentError()
 
         continueButton.setOnClickListener {
             val currentState = RegistrationConsentState(
@@ -83,10 +99,38 @@ class SocialAuthConsentBottomSheetDialogFragment :
                 )
                 dismiss()
             } else {
-                consentErrorText.isVisible = true
+                showRequiredConsentError()
             }
         }
     }
+
+    private fun showRequiredConsentError() {
+        consentErrorText.isVisible = true
+        CompoundButtonCompat.setButtonTintList(
+            requiredConsentCheckBox,
+            requiredConsentErrorTint
+        )
+    }
+
+    private fun clearRequiredConsentError() {
+        consentErrorText.isVisible = false
+        CompoundButtonCompat.setButtonTintList(
+            requiredConsentCheckBox,
+            requiredConsentDefaultTint
+        )
+    }
+
+    private fun createRequiredConsentTint(checkedColor: Int, uncheckedColor: Int): ColorStateList =
+        ColorStateList(
+            arrayOf(
+                intArrayOf(android.R.attr.state_checked),
+                intArrayOf(-android.R.attr.state_checked)
+            ),
+            intArrayOf(
+                checkedColor,
+                uncheckedColor
+            )
+        )
 
     interface Callback {
         fun onSocialConsentConfirmed(socialNetwork: SocialNetwork, marketingConsent: PendingSocialMarketingConsent)
