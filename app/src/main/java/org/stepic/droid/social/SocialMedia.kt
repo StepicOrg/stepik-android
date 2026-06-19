9d4354f85e92f500a9cf3f28b583d4d96102e42e
Aleksandr Zhukov <alexandr.zhukov@hyperskill.org>
2026-06-19 14:31:57 +0200

    Hide Facebook and Instagram items from the About screen

--- app/src/main/java/org/stepic/droid/social/SocialMedia.kt
+++ app/src/main/java/org/stepic/droid/social/SocialMedia.kt
@@ -9,7 +9,5 @@ enum class SocialMedia(
     @DrawableRes
     val drawable: Int
 ) {
-    VK("https://vk.com/rustepik", R.drawable.ic_login_social_vk),
-    FACEBOOK("https://www.facebook.com/rustepik/", R.drawable.ic_login_social_fb),
-    INSTAGRAM("https://www.instagram.com/stepik.education/", R.drawable.ic_social_instagram)
+    VK("https://vk.com/rustepik", R.drawable.ic_login_social_vk)
 }
